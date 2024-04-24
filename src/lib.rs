2ca04d07d0b9ea6e9e3a20b664a2db676e83d450
philsippl <philsippl@gmail.com>
2024-04-24 15:34:05 +0000

    up

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,53 @@
-use std::{
-    ffi::c_void,
-    sync::Arc,
-};
+use std::{ffi::c_void, str::FromStr, sync::Arc};
 
 use cudarc::{
-    cublas::{result::gemm_ex, sys, CudaBlas},
-    driver::{
-        CudaDevice, CudaFunction, CudaSlice, DevicePtr, DevicePtrMut,
-    },
-    nvrtc::compile_ptx,
+    cublas::{result::gemm_ex, sys, CudaBlas}, driver::{CudaDevice, CudaFunction, CudaSlice, CudaStream, DevicePtr, DevicePtrMut, LaunchAsync, LaunchConfig}, nccl::Id, nvrtc::compile_ptx
 };
+use once_cell::sync::Lazy;
 use rayon::prelude::*;
+
+static COMM_ID: Lazy<Vec<Id>> = Lazy::new(|| {
+    (0..CudaDevice::count().unwrap())
+        .map(|_| Id::new().unwrap())
+        .collect::<Vec<_>>()
+});
 
 pub(crate) const P: u16 = (1 << 14) - 3;
 const PTX_SRC: &str = include_str!("matmul.cu");
 const IRIS_CODE_LENGTH: usize = 12800;
 const QUERY_LENGTH: usize = 31 * 10;
 const FUNCTION_NAME: &str = "matmul_f14";
+
+struct IdWrapper(Id);
+
+impl FromStr for IdWrapper {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let bytes = hex::decode(s)
+            .unwrap()
+            .iter()
+            .map(|&c| c as i8)
+            .collect::<Vec<_>>();
+
+        let mut id = [0i8; 128];
+        id.copy_from_slice(&bytes);
+
+        Ok(IdWrapper(Id::uninit(id)))
+    }
+}
+
+impl ToString for IdWrapper {
+    fn to_string(&self) -> String {
+        hex::encode(
+            self.0
+                .internal()
+                .iter()
+                .map(|&c| c as u8)
+                .collect::<Vec<_>>(),
+        )
+    }
+}
 
 pub fn gemm(
     handle: &CudaBlas,
@@ -59,11 +90,13 @@ pub fn gemm(
 }
 
 pub struct IrisDB {
+    peer_id: usize,
     db_length: usize,
     limbs: usize,
     blass: Vec<CudaBlas>,
     devs: Vec<Arc<CudaDevice>>,
     kernels: Vec<CudaFunction>,
+    streams: Vec<CudaStream>,
     db1: Vec<CudaSlice<u8>>,
     db0: Vec<CudaSlice<u8>>,
     db1_sums: Vec<CudaSlice<u32>>,
@@ -76,28 +109,30 @@ pub struct IrisDB {
 }
 
 impl IrisDB {
-    pub fn init(db_entries: &[u16]) -> Self {
-        // TOOD: replace with a MAX_DB_SIZE to allow for insertions
+    pub fn init(peer_id: usize, db_entries: &[u16], peer_url: Option<String>) -> Self {
+        // TODO: replace with a MAX_DB_SIZE to allow for insertions
         let db_length = db_entries.len() / IRIS_CODE_LENGTH;
         let limbs = 2;
         let ptx = compile_ptx(PTX_SRC).unwrap();
 
         // Start all devices
-        let (devs, (blass, kernels)): (Vec<Arc<CudaDevice>>, (Vec<CudaBlas>, Vec<CudaFunction>)) =
-            (0..CudaDevice::count().unwrap() as usize)
-                .map(|i| {
-                    let dev = CudaDevice::new(i).unwrap();
-                    let blas = CudaBlas::new(dev.clone()).unwrap();
-                    let stream = dev.fork_default_stream().unwrap();
-                    unsafe {
-                        blas.set_stream(Some(&stream)).unwrap();
-                    }
-                    dev.load_ptx(ptx.clone(), FUNCTION_NAME, &[FUNCTION_NAME])
-                        .unwrap();
-                    let function = dev.get_func(FUNCTION_NAME, FUNCTION_NAME).unwrap();
-                    (dev, (blas, function))
-                })
-                .unzip();
+        let (devs, (blass, (kernels, streams))): (
+            Vec<Arc<CudaDevice>>,
+            (Vec<CudaBlas>, (Vec<CudaFunction>, Vec<CudaStream>)),
+        ) = (0..CudaDevice::count().unwrap() as usize)
+            .map(|i| {
+                let dev = CudaDevice::new(i).unwrap();
+                let blas = CudaBlas::new(dev.clone()).unwrap();
+                let stream = dev.fork_default_stream().unwrap();
+                unsafe {
+                    blas.set_stream(Some(&stream)).unwrap();
+                }
+                dev.load_ptx(ptx.clone(), FUNCTION_NAME, &[FUNCTION_NAME])
+                    .unwrap();
+                let function = dev.get_func(FUNCTION_NAME, FUNCTION_NAME).unwrap();
+                (dev, (blas, (function, stream)))
+            })
+            .unzip();
 
         let mut a1_host = db_entries
             .iter()
@@ -177,12 +212,25 @@ impl IrisDB {
             })
             .unzip();
 
+        if peer_url.is_some() {
+            for i in 0..CudaDevice::count().unwrap() as usize {
+                let id = if peer_id == 0 {
+                    COMM_ID[i]
+                } else {
+                    let res = reqwest::blocking::get(format!("http://{}/{}", peer_url.clone().unwrap(), i)).unwrap();
+                    IdWrapper::from_str(&res.text().unwrap()).unwrap().0
+                };
+            }
+        }
+
         Self {
+            peer_id,
             db_length,
             limbs,
             blass,
             devs,
             kernels,
+            streams,
             db1,
             db0,
             db1_sums,
@@ -211,7 +259,7 @@ impl IrisDB {
         result.to_vec()
     }
 
-    pub fn dot(&mut self, preprocessed_query: &Vec<Vec<u8>>, results_host: *mut u16) {
+    pub fn dot(&mut self, preprocessed_query: &Vec<Vec<u8>>) {
         let b_dev: Vec<Vec<CudaSlice<u8>>> = (0..CudaDevice::count().unwrap() as usize)
             .map(|idx| {
                 preprocessed_query
@@ -220,6 +268,15 @@ impl IrisDB {
                     .collect::<Vec<_>>()
             })
             .collect::<Vec<_>>();
+
+            let num_elements = self.db_length * QUERY_LENGTH;
+            let threads_per_block = 256;
+            let blocks_per_grid = (num_elements + threads_per_block - 1) / threads_per_block;
+            let cfg = LaunchConfig {
+                block_dim: (threads_per_block as u32, 1, 1),
+                grid_dim: (blocks_per_grid as u32, 1, 1),
+                shared_mem_bytes: 0,
+            };
 
         for idx in 0..self.devs.len() {
             // TODO: check this is actually async
@@ -264,7 +321,7 @@ impl IrisDB {
                         &mut self.intermediate_results[idx],
                         0,
                         0,
-                        (self.db_length * QUERY_LENGTH * 4 * (i*2+j)) as u64,
+                        (self.db_length * QUERY_LENGTH * 4 * (i * 2 + j)) as u64,
                         self.db_length,
                         QUERY_LENGTH,
                         IRIS_CODE_LENGTH,
@@ -274,10 +331,30 @@ impl IrisDB {
                 }
             }
 
-            // TODO: reduce
-
-            // TODO: nccl broadcast
-
+            unsafe {
+                self.kernels[idx].clone().launch_on_stream(
+                    &self.streams[idx],
+                    cfg,
+                    (
+                        &self.intermediate_results[idx],
+                        &mut self.results[idx],
+                        &self.db0_sums[idx],
+                        &self.db1_sums[idx],
+                        &self.query0_sums[idx],
+                        &self.query1_sums[idx],
+                        self.db_length as u64,
+                        (self.db_length * QUERY_LENGTH) as u64,
+                        IRIS_CODE_LENGTH as u64,
+                        P,
+                    ),
+                ).unwrap();
+            }
         }
+    }
+
+    pub fn exchange() {
+
+
+
     }
 }
