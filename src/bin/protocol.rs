afc043a864ea754e60229e39e4eb106ab2a514c0
philsippl <philsippl@gmail.com>
2024-05-02 10:56:23 +0000

    lower size for testing

--- src/bin/protocol.rs
+++ src/bin/protocol.rs
@@ -11,7 +11,7 @@ use gpu_iris_mpc::{
 use rand::{rngs::StdRng, Rng, SeedableRng};
 use tokio::time;
 
-const DB_SIZE: usize = 8*125_000;
+const DB_SIZE: usize = 8*10_000;
 const QUERIES: usize = 930;
 const RNG_SEED: u64 = 1337;
 
