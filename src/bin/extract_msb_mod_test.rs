91cc32a579e87e0bd289a36d22f9708563fe26ee
Roman Walch <walch@taceo.io>
2024-05-08 11:49:52 +0200

    .

--- src/bin/extract_msb_mod_test.rs
+++ src/bin/extract_msb_mod_test.rs
@@ -73,9 +73,9 @@ fn to_gpu(a: &[u16], b: &[u16], devices: &[Arc<CudaDevice>]) -> Vec<ChunkShare<u
 }
 
 fn pack_with_device_padding(bits: Vec<bool>) -> Vec<u64> {
-    assert!(bits.len() % (CHUNK_SIZE * 64) == 0);
+    assert!(bits.len() % INPUTS_PER_GPU_SIZE == 0);
     let mut res = vec![];
-    for devices in bits.chunks_exact(CHUNK_SIZE * 64) {
+    for devices in bits.chunks_exact(INPUTS_PER_GPU_SIZE) {
         for bits in devices.chunks(64) {
             let mut r = 0;
             for (i, bit) in bits.iter().enumerate() {
