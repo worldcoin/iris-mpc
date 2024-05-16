28d0ffacc0fb8c2fe92a917833a784d8438efcf1
Daniel Kales <11509575+dkales@users.noreply.github.com>
2024-05-16 11:01:29 +0200

    remove more sync

--- src/threshold/protocol.rs
+++ src/threshold/protocol.rs
@@ -858,9 +858,6 @@ impl Circuits {
             self.send(&res.b, self.prev_id, idx);
         }
         result::group_end().unwrap();
-        for dev in self.devs.iter() {
-            dev.synchronize().unwrap();
-        }
         self.send_recv_time += now.elapsed();
 
         res
