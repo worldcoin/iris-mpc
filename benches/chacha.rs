e2003f59964fb51df945cb1ad1b4169ef889ba0f
Daniel Kales <11509575+dkales@users.noreply.github.com>
2024-05-17 11:35:21 +0200

    remove chacha two

--- benches/chacha.rs
+++ benches/chacha.rs
@@ -1,6 +1,6 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use cudarc::driver::CudaDevice;
-use gpu_iris_mpc::rng::{aes::AesCudaRng, chacha::ChaChaCudaRng, chacha_field, chacha_two};
+use gpu_iris_mpc::rng::{aes::AesCudaRng, chacha::ChaChaCudaRng, chacha_field};
 
 pub fn criterion_benchmark_chacha12_field_runner(c: &mut Criterion, buf_size: usize) {
     let mut chacha =
@@ -20,41 +20,6 @@ pub fn criterion_benchmark_chacha12_field_runner(c: &mut Criterion, buf_size: us
     group.bench_function("without copy to host", move |b| {
         b.iter(|| {
             chacha.fill_rng_no_host_copy();
-        })
-    });
-    group.finish();
-}
-pub fn criterion_benchmark_chacha12_two_runner(c: &mut Criterion, buf_size: usize) {
-    let mut group = c.benchmark_group(format!(
-        "ChaCha12 TWO (buf_size = {}MB)",
-        buf_size * 4 / (1024 * 1024)
-    ));
-
-    group.throughput(criterion::Throughput::Bytes(
-        (buf_size * std::mem::size_of::<u32>()) as u64,
-    ));
-    let mut chacha = chacha_two::ChaChaCudaRng::init(buf_size);
-    group.bench_function("with copy to host (interleaved)", move |b| {
-        b.iter(|| {
-            chacha.fill_rng(0);
-        })
-    });
-    let mut chacha = chacha_two::ChaChaCudaRng::init(buf_size);
-    group.bench_function("without copy to host (interleaved)", move |b| {
-        b.iter(|| {
-            chacha.fill_rng_no_host_copy(0);
-        })
-    });
-    let mut chacha = chacha_two::ChaChaCudaRng::init(buf_size);
-    group.bench_function("with copy to host (seq)", move |b| {
-        b.iter(|| {
-            chacha.fill_rng(1);
-        })
-    });
-    let mut chacha = chacha_two::ChaChaCudaRng::init(buf_size);
-    group.bench_function("without copy to host (seq)", move |b| {
-        b.iter(|| {
-            chacha.fill_rng_no_host_copy(1);
         })
     });
     group.finish();
@@ -122,13 +87,6 @@ pub fn criterion_benchmark_chacha12_field(c: &mut Criterion) {
     }
 }
 
-pub fn criterion_benchmark_chacha12_two(c: &mut Criterion) {
-    for log_buf_size in 20..=30 {
-        let buf_size = (1usize << log_buf_size) / 4;
-        criterion_benchmark_chacha12_two_runner(c, buf_size);
-    }
-}
-
 pub fn criterion_benchmark_aes(c: &mut Criterion) {
     for log_buf_size in 20..=30 {
         let buf_size = (1usize << log_buf_size) / 4;
@@ -138,6 +96,6 @@ pub fn criterion_benchmark_aes(c: &mut Criterion) {
 criterion_group!(
     name = rng_benches;
     config = Criterion::default();
-    targets = criterion_benchmark_chacha12, criterion_benchmark_chacha12_two, criterion_benchmark_chacha12_field, criterion_benchmark_aes
+    targets = criterion_benchmark_chacha12, criterion_benchmark_chacha12_field, criterion_benchmark_aes
 );
 criterion_main!(rng_benches);
--- src/rng/mod.rs
+++ src/rng/mod.rs
@@ -1,4 +1,3 @@
 pub mod aes;
 pub mod chacha;
 pub mod chacha_field;
-pub mod chacha_two;
