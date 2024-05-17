629f2a6efebba46f1296ed37ff250015bbb0c3a8
philsippl <philsippl@gmail.com>
2024-05-17 12:19:07 +0000

    rotations

--- src/setup/iris_db/shamir_iris.rs
+++ src/setup/iris_db/shamir_iris.rs
@@ -28,6 +28,8 @@ impl Default for ShamirIris {
 }
 
 impl ShamirIris {
+    const COLS: usize = 200;
+
     fn share_bit<R: Rng>(code: bool, mask: bool, rng: &mut R) -> ([u16; 3], [u16; 3]) {
         // code needs to be encoded before sharing
         let val = (code & mask) as u32;
@@ -59,17 +61,36 @@ impl ShamirIris {
         result
     }
 
-    pub fn rotate(&self) -> Vec<ShamirIris> {
+    pub fn all_rotations(&self) -> Vec<ShamirIris> {
         let mut reference = self.clone();
         let mut result = vec![];
-        reference.code.rotate_left(15*4);
-        reference.mask.rotate_left(15*4);
+        reference.rotate_left(16*4);
         for _ in 0..31 {
-            let mut rotated = reference.clone();
-            rotated.code.rotate_right(4);
-            rotated.mask.rotate_right(4);
-            result.push(rotated);
+            reference.rotate_right(4);
+            result.push(reference.clone());
         }
         result
     }
+
+    pub fn rotate_right(&mut self, by: usize) {
+        self.code
+            .chunks_exact_mut(Self::COLS * 4)
+            .for_each(|chunk| chunk.rotate_right(by));
+
+        self.mask
+            .chunks_exact_mut(Self::COLS * 4)
+            .for_each(|chunk| chunk.rotate_right(by));
+    }
+    
+    pub fn rotate_left(&mut self, by: usize) {
+        self.code
+            .chunks_exact_mut(Self::COLS * 4)
+            .for_each(|chunk| chunk.rotate_left(by));
+
+        self.mask
+            .chunks_exact_mut(Self::COLS * 4)
+            .for_each(|chunk| chunk.rotate_left(by));
+    }
+
+    
 }
