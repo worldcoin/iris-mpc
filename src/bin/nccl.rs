1b7d838d426d3097406be4d2b96516a8f9018ccd
philsippl <philsippl@gmail.com>
2024-05-21 16:31:22 +0000

    up

--- src/bin/nccl.rs
+++ src/bin/nccl.rs
@@ -55,7 +55,7 @@ impl ToString for IdWrapper {
     }
 }
 
-const DUMMY_DATA_LEN: usize = 5 * (1 << 30);
+const DUMMY_DATA_LEN: usize = 300 * (1 << 20);
 
 async fn root(Path(device_id): Path<String>) -> String {
     let device_id: usize = device_id.parse().unwrap();
