fed0f72d85805f465511f1bf6bb51dae0f664588
Roman Walch <walch@taceo.io>
2024-05-21 10:31:59 +0200

    a fix

--- src/threshold/protocol.rs
+++ src/threshold/protocol.rs
@@ -950,14 +950,14 @@ impl Circuits {
                     .alloc::<u32>(self.chunk_size * 2 * 64)
                     .unwrap()
             };
-            self.rngs[idx].fill_their_rng_into(&mut rand_ca.slice_mut(..));
+            self.rngs[idx].fill_my_rng_into(&mut rand_ca.slice_mut(..));
             // SAFETY: Only unsafe because memory is not initialized. But, we fill afterwards.
             let mut rand_cb = unsafe {
                 self.devs[idx]
                     .alloc::<u32>(self.chunk_size * 2 * 64)
                     .unwrap()
             };
-            self.rngs[idx].fill_my_rng_into(&mut rand_cb.slice_mut(..));
+            self.rngs[idx].fill_their_rng_into(&mut rand_cb.slice_mut(..));
             // SAFETY: Only unsafe because memory is not initialized. But, we fill afterwards.
             let mut rand_wa1 = unsafe {
                 self.devs[idx]
