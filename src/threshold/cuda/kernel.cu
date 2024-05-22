ffb553bd553929b14cb1bd9412f95819ddd1e358
Roman Walch <walch@taceo.io>
2024-05-22 10:59:07 +0200

    .

--- src/threshold/cuda/kernel.cu
+++ src/threshold/cuda/kernel.cu
@@ -7,8 +7,8 @@
 #define B_BITS 16
 #define B ((u32)(1ULL << B_BITS))
 #define A ((U16)((1. - 2. * MATCH_THRESHOLD_RATIO) * (double)B))
-#define P ((u16)((1ULL << 16) - 17))
-#define P2K ((u32)(U32(P) << B_BITS))
+#define P ((U16)((1ULL << 16) - 17))
+#define P2K ((U32)(U32(P) << B_BITS))
 
 ////////////////////////////////////////////////////////////////////////////////
 // Basic Blocks (not parallelized)
