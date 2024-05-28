39901489f64a2179d98d04e86cf056aac1425555
philsippl <philsippl>
2024-05-28 13:00:52 +0000

    dbg

--- src/kernel.cu
+++ src/kernel.cu
@@ -62,8 +62,8 @@ extern "C" __global__ void dedupAndAppend(unsigned int *matchResultsSelf, unsign
         int oldIdx = (deviceIdx * queryLength + idx) * (2 * ROTATIONS + 1) + ROTATIONS;
 
         // write back result
-        queriesSumNew1[row] = queriesSum1[oldIdx];
-        queriesSumNew2[row] = queriesSum2[oldIdx];
+        // queriesSumNew1[row] = queriesSum1[oldIdx];
+        // queriesSumNew2[row] = queriesSum2[oldIdx];
         // for (int i = 0; i < IRIS_CODE_LENGTH; i++)
         // {
         //     queriesNew1[row * IRIS_CODE_LENGTH + i] = queries1[oldIdx * IRIS_CODE_LENGTH + i];
