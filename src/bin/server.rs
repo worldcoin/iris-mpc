45031d501b4bbf74b00a110b88129695ca90897b
philsippl <philsippl@gmail.com>
2024-06-01 15:26:47 +0000

    up

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -516,7 +516,6 @@ async fn main() -> eyre::Result<()> {
             let mut host_results = vec![];
             // Step 1: Fetch the uniqueness results for each query for each device
             for i in 0..tmp_devs.len() {
-                println!("fetching results {}", i);
                 tmp_devs[i].bind_to_thread().unwrap();
                 host_results.push(vec![u32::MAX; QUERIES / ROTATIONS]);
 
