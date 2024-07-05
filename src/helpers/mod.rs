11df181c854a82f6f06f32ca850d8c0fd7a5b822
Daniel Kales <11509575+dkales@users.noreply.github.com>
2024-07-05 10:38:17 +0200

    feat: add safety comments to stream copy functions

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -5,13 +5,13 @@ use cudarc::driver::{
 };
 use std::sync::Arc;
 
+pub mod aws;
 pub mod aws_sigv4;
 pub mod id_wrapper;
 pub mod kms_dh;
 pub mod mmap;
 pub mod sqs;
 pub mod task_monitor;
-pub mod aws;
 
 pub fn device_ptrs<T>(slice: &[CudaSlice<T>]) -> Vec<CUdeviceptr> {
     slice.iter().map(|s| *s.device_ptr()).collect()
@@ -40,10 +40,13 @@ pub fn dtoh_on_stream_sync<T: Default + Clone, U: DevicePtr<T>>(
 ) -> Result<Vec<T>, DriverError> {
     device.bind_to_thread()?;
     let mut buf = vec![T::default(); input.len()];
+    // SAFETY: for the below async copy call, the `buf` needs to not be moved or
+    // dropped until the copy is complete therefore, we synchronize the stream
+    // before returning the buffer or wrapping it in `Ok`
     unsafe {
         memcpy_dtoh_async(&mut buf, *input.device_ptr(), stream.stream)?;
+        stream::synchronize(stream.stream).unwrap()
     }
-    unsafe { stream::synchronize(stream.stream).unwrap() }
 
     Ok(buf)
 }
@@ -54,10 +57,16 @@ pub fn htod_on_stream_sync<T: DeviceRepr>(
     stream: &CudaStream,
 ) -> Result<CudaSlice<T>, DriverError> {
     device.bind_to_thread()?;
-    let mut buf = unsafe { device.alloc(input.len()) }?;
-    unsafe {
+    // SAFETY: for the below async copy call, we need to allocate a buffer of the
+    // input size. We can use alloc, which uses uninitialized memory, since we
+    // are going to copy the input into it immediately afterwards.
+    // The synchronization is done to ensure that the input slice is valid for the
+    // duration of the otherwise async copy
+    let buf = unsafe {
+        let mut buf = device.alloc(input.len())?;
         memcpy_htod_async(*buf.device_ptr_mut(), input, stream.stream)?;
-    }
-    unsafe { stream::synchronize(stream.stream).unwrap() }
+        stream::synchronize(stream.stream).unwrap();
+        buf
+    };
     Ok(buf)
 }
