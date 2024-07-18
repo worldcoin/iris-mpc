be90e909f9d42df11d153740c31f100fb2eb3577
wojciechsromek <wojciech.sromek@toolsforhumanity.com>
2024-07-18 15:03:11 +0200

    chore: Rename KMS IDs to ARNs

--- src/helpers/kms_dh.rs
+++ src/helpers/kms_dh.rs
@@ -1,16 +1,19 @@
 use aws_sdk_kms::{types::KeyAgreementAlgorithmSpec, Client};
 
 /// Derive a shared secret from two KMS keys
-pub async fn derive_shared_secret(own_key_id: &str, other_key_id: &str) -> eyre::Result<[u8; 32]> {
+pub async fn derive_shared_secret(
+    own_key_arn: &str,
+    other_key_arn: &str,
+) -> eyre::Result<[u8; 32]> {
     let shared_config = aws_config::from_env().load().await;
 
     let client = Client::new(&shared_config);
-    let other_public_key = client.get_public_key().key_id(other_key_id).send().await?;
+    let other_public_key = client.get_public_key().key_id(other_key_arn).send().await?;
     let public_key = other_public_key.public_key.unwrap();
 
     let res = client
         .derive_shared_secret()
-        .key_id(own_key_id)
+        .key_id(own_key_arn)
         .public_key(public_key)
         .key_agreement_algorithm(KeyAgreementAlgorithmSpec::Ecdh)
         .send()
