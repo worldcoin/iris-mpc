ae3c26ae56936d1f92a5e6d5a55c11adc2ed8a16
philsippl <philsippl@gmail.com>
2024-07-18 13:43:49 +0000

    increase db size again

--- src/server/actor.rs
+++ src/server/actor.rs
@@ -63,9 +63,9 @@ impl ServerActorHandle {
     }
 }
 
-const DB_SIZE: usize = 2 * 1_000;
-const DB_BUFFER: usize = 2 * 1_000;
-const DB_CHUNK_SIZE: usize = 512;
+const DB_SIZE: usize = 8 * 1_000;
+const DB_BUFFER: usize = 8 * 1_000;
+const DB_CHUNK_SIZE: usize = 1000;
 const N_QUERIES: usize = 64;
 const QUERIES: usize = ROTATIONS * N_QUERIES;
 
