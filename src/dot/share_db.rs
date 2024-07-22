74ec12e10290b4a5ccdc781df202a4473f4251aa
philsippl <philsippl@gmail.com>
2024-07-22 06:11:12 +0000

    up

--- src/dot/share_db.rs
+++ src/dot/share_db.rs
@@ -315,7 +315,7 @@ impl ShareDB {
                     Ok(())
                 }));
             } else {
-                thread::sleep(Duration::from_secs(5));
+                thread::sleep(Duration::from_secs(10));
             }
 
             for i in 0..n_devices {
