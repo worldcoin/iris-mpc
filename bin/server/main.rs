4073078e8e0edb4a403c482ae527e2a8fec9d862
philsippl <philsippl@gmail.com>
2024-07-22 06:07:16 +0000

    up

--- bin/server/main.rs
+++ bin/server/main.rs
@@ -41,7 +41,7 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 const REGION: &str = "eu-north-1";
 const DB_SIZE: usize = 8 * 250_000;
 const N_QUERIES: usize = 64;
-const N_BATCHES: usize = 100;
+const N_BATCHES: usize = 10;
 const RNG_SEED: u64 = 42;
 /// The number of batches before a stream is re-used.
 
