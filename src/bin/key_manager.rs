0d05cf09938bb39fbd47eed421633bf932693f87
Carlo Mazzaferro <carlo.mazzaferro@toolsforhumanity.com>
2024-07-31 15:40:18 +0200

    clippy + fmt

--- src/bin/key_manager.rs
+++ src/bin/key_manager.rs
@@ -1,14 +1,11 @@
+use aws_sdk_s3::{config::Region, Client, Error};
+use base64::{engine::general_purpose::STANDARD, Engine};
 use clap::Parser;
-use sodiumoxide::crypto::box_::{PublicKey, SecretKey, Seed, curve25519xsalsa20poly1305};
-use aws_sdk_s3::{Client, Error, config::Region};
 use rand::{rngs::StdRng, Rng, SeedableRng};
-use base64::{engine::general_purpose::STANDARD, DecodeError, Engine};
-
-
+use sodiumoxide::crypto::box_::{curve25519xsalsa20poly1305, PublicKey, SecretKey, Seed};
 
 const PUBLIC_KEY_S3_KEY_NAME_PREFIX: &str = "public-key";
 const REGION: &str = "eu-north-1";
-
 
 #[derive(Debug, Parser)]
 struct Opt {
@@ -24,7 +21,6 @@ struct Opt {
     #[arg(short, long, env)]
     dry_run: Option<bool>,
 }
-
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
@@ -43,17 +39,15 @@ async fn main() -> eyre::Result<()> {
         StdRng::from_entropy()
     };
 
-
     let mut seedbuf = [0u8; 32];
     rng.fill(&mut seedbuf);
     let pk_seed = Seed(seedbuf);
-
 
     let region_provider = Region::new(REGION);
     let shared_config = aws_config::from_env().region(region_provider).load().await;
     let client = Client::new(&shared_config);
     let bucket_key_name = format!("{}-{}", PUBLIC_KEY_S3_KEY_NAME_PREFIX, node_id);
-    
+
     let (public_key, _) = generate_key_pairs(pk_seed);
     let pub_key_str = STANDARD.encode(public_key);
 
@@ -63,18 +57,24 @@ async fn main() -> eyre::Result<()> {
         return Ok(());
     }
     upload_public_key_to_s3(
-        &client, 
-        public_key_s3_bucket_name.as_str(), 
-        bucket_key_name.as_str(), 
-        pub_key_str.as_str()
-    ).await?;
+        &client,
+        public_key_s3_bucket_name.as_str(),
+        bucket_key_name.as_str(),
+        pub_key_str.as_str(),
+    )
+    .await?;
 
     println!("File uploaded successfully!");
 
     Ok(())
 }
 
-async fn upload_public_key_to_s3(client: &Client, bucket: &str, key: &str, content: &str) -> Result<(), Error> {
+async fn upload_public_key_to_s3(
+    client: &Client,
+    bucket: &str,
+    key: &str,
+    content: &str,
+) -> Result<(), Error> {
     client
         .put_object()
         .bucket(bucket)
@@ -86,21 +86,25 @@ async fn upload_public_key_to_s3(client: &Client, bucket: &str, key: &str, conte
     Ok(())
 }
 
-
 fn generate_key_pairs(seed: Seed) -> (PublicKey, SecretKey) {
     // Generate an ephemeral secret (private key)
     let (public_key, private_key) = curve25519xsalsa20poly1305::keypair_from_seed(&seed);
-    
 
     (public_key, private_key)
 }
-
-
 
 // tests
 #[cfg(test)]
 mod test {
     use super::*;
+
+    pub fn get_public_key(user_pub_key: &str) -> PublicKey {
+        let user_pubkey = STANDARD.decode(user_pub_key.as_bytes()).unwrap();
+        match PublicKey::from_slice(&user_pubkey) {
+            Some(key) => key,
+            None => panic!("Invalid public key"),
+        }
+    }
 
     #[test]
     fn test_encode_pk_to_pem() {
@@ -109,15 +113,4 @@ mod test {
         let decoded_pub_key = get_public_key(&pub_key_str);
         assert_eq!(public_key, decoded_pub_key);
     }
-
 }
-
-
-pub fn get_public_key(user_pub_key: &str,) -> PublicKey {
-    let user_pubkey = STANDARD.decode(user_pub_key.as_bytes()).unwrap();
-    match PublicKey::from_slice(&user_pubkey) {
-        Some(key) => key,
-        None => panic!("Invalid public key"),
-    }
-}
-
