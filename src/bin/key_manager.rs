8d91ca9a6daf3f8be9acee6b9176e358888ee30b
Carlo Mazzaferro <carlo.mazzaferro@toolsforhumanity.com>
2024-07-31 16:22:47 +0200

    Secrets manager private key storage

--- src/bin/key_manager.rs
+++ src/bin/key_manager.rs
@@ -1,10 +1,13 @@
-use aws_sdk_s3::{config::Region, Client, Error};
+use aws_sdk_s3::{Client as S3Client, config::Region as S3Region, Error as S3Error};
+use aws_sdk_secretsmanager::{Client as SecretsManagerClient, Error as SecretsManagerError};
 use base64::{engine::general_purpose::STANDARD, Engine};
 use clap::Parser;
 use rand::{rngs::StdRng, Rng, SeedableRng};
 use sodiumoxide::crypto::box_::{curve25519xsalsa20poly1305, PublicKey, SecretKey, Seed};
 
 const PUBLIC_KEY_S3_KEY_NAME_PREFIX: &str = "public-key";
+const PRIVATE_KEY_SM_KEY_NAME_PREFIX: &str = "private-key";
+const PRIVATE_KEY_KMS_KEY_ID: &str = "private-key-kms-key-id";
 const REGION: &str = "eu-north-1";
 
 #[derive(Debug, Parser)]
@@ -43,38 +46,76 @@ async fn main() -> eyre::Result<()> {
     rng.fill(&mut seedbuf);
     let pk_seed = Seed(seedbuf);
 
-    let region_provider = Region::new(REGION);
+    let region_provider = S3Region::new(REGION);
     let shared_config = aws_config::from_env().region(region_provider).load().await;
-    let client = Client::new(&shared_config);
-    let bucket_key_name = format!("{}-{}", PUBLIC_KEY_S3_KEY_NAME_PREFIX, node_id);
+    
+    let s3_client = S3Client::new(&shared_config);
+    let sm_client = SecretsManagerClient::new(&shared_config);
 
-    let (public_key, _) = generate_key_pairs(pk_seed);
+    let (public_key, private_key) = generate_key_pairs(pk_seed);
     let pub_key_str = STANDARD.encode(public_key);
+    let priv_key_str = STANDARD.encode(private_key.clone());
+    let bucket_key_name = format!("{}-{}", PUBLIC_KEY_S3_KEY_NAME_PREFIX, node_id);
+    let secret_key_name: String = format!("{}-{}", PRIVATE_KEY_SM_KEY_NAME_PREFIX, node_id);
 
     if dry_run.unwrap_or(false) {
         println!("Dry run enabled, skipping upload of public key to S3");
         println!("Public key: {}", pub_key_str);
+        println!("Private key: {}", priv_key_str);
+
+        let user_pubkey = STANDARD.decode(pub_key_str.as_bytes()).unwrap();
+        let decoded_pub_key = PublicKey::from_slice(&user_pubkey).unwrap();
+    
+        assert_eq!(public_key, decoded_pub_key);
+
+        let user_privkey = STANDARD.decode(priv_key_str.as_bytes()).unwrap();
+        let decoded_priv_key = SecretKey::from_slice(&user_privkey).unwrap();
+
+        assert_eq!(private_key, decoded_priv_key);
+
         return Ok(());
     }
     upload_public_key_to_s3(
-        &client,
+        &s3_client,
         public_key_s3_bucket_name.as_str(),
         bucket_key_name.as_str(),
         pub_key_str.as_str(),
     )
     .await?;
 
+    upload_private_key_to_asm(
+        &sm_client,
+        secret_key_name.as_str(),
+        priv_key_str.as_str(),
+    ).await?;
+
     println!("File uploaded successfully!");
 
     Ok(())
 }
 
+
+async fn upload_private_key_to_asm(
+    client: &SecretsManagerClient,
+    secret_name: &str,
+    content: &str,
+) -> Result<(), SecretsManagerError> {
+    client
+        .create_secret()
+        .secret_string(content)
+        .name(secret_name)
+        .kms_key_id(PRIVATE_KEY_KMS_KEY_ID)
+        .send()
+        .await?;
+    Ok(())
+}
+
 async fn upload_public_key_to_s3(
-    client: &Client,
+    client: &S3Client,
     bucket: &str,
     key: &str,
     content: &str,
-) -> Result<(), Error> {
+) -> Result<(), S3Error> {
     client
         .put_object()
         .bucket(bucket)
