5989b4684b38645af254b52a9a76b109035d1775
philsippl <philsippl@gmail.com>
2024-08-06 16:38:48 +0000

    up

--- src/server/actor.rs
+++ src/server/actor.rs
@@ -581,32 +581,34 @@ impl ServerActor {
             forget_vec!(mask_dots);
             // ---- END PHASE 2 ----
 
-            // Prefetch the next chunk
-            if chunk_size[0] == DB_CHUNK_SIZE {
-                for i in 0..self.device_manager.device_count() {
-                    self.device_manager.device(i).bind_to_thread().unwrap();
-                    for ptr in &[
-                        self.code_db_slices.code_gr.limb_0[i],
-                        self.code_db_slices.code_gr.limb_1[i],
-                        self.mask_db_slices.code_gr.limb_0[i],
-                        self.mask_db_slices.code_gr.limb_1[i],
-                    ] {
-                        unsafe {
-                            mem_prefetch_async(
-                                ptr + (DB_CHUNK_SIZE * (db_chunk_idx + 1) * IRIS_CODE_LENGTH)
-                                    as u64,
-                                DB_CHUNK_SIZE * IRIS_CODE_LENGTH,
-                                CUmemLocation_st {
-                                    type_: CUmemLocationType::CU_MEM_LOCATION_TYPE_DEVICE,
-                                    id:    i as i32,
-                                },
-                                self.streams[(db_chunk_idx + 1) % 2][i].stream,
-                            )
-                            .unwrap();
-                        }
-                    }
-                }
-            }
+            // self.device_manager.await_streams(request_streams);
+
+            // // Prefetch the next chunk
+            // if chunk_size[0] == DB_CHUNK_SIZE {
+            //     for i in 0..self.device_manager.device_count() {
+            //         self.device_manager.device(i).bind_to_thread().unwrap();
+            //         for ptr in &[
+            //             self.code_db_slices.code_gr.limb_0[i],
+            //             self.code_db_slices.code_gr.limb_1[i],
+            //             self.mask_db_slices.code_gr.limb_0[i],
+            //             self.mask_db_slices.code_gr.limb_1[i],
+            //         ] {
+            //             unsafe {
+            //                 mem_prefetch_async(
+            //                     ptr + (DB_CHUNK_SIZE * (db_chunk_idx + 1) *
+            // IRIS_CODE_LENGTH)                         as u64,
+            //                     DB_CHUNK_SIZE * IRIS_CODE_LENGTH,
+            //                     CUmemLocation_st {
+            //                         type_:
+            // CUmemLocationType::CU_MEM_LOCATION_TYPE_DEVICE,                  
+            // id:    i as i32,                     },
+            //                     self.streams[(db_chunk_idx + 1) % 2][i].stream,
+            //                 )
+            //                 .unwrap();
+            //             }
+            //         }
+            //     }
+            // }
 
             // Update events for synchronization
             current_dot_event = next_dot_event;
