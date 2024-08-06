81aba092f6b136b3ec66d481f6e5011e8aa311f1
philsippl <philsippl@gmail.com>
2024-08-06 15:29:59 +0000

    up

--- src/server/actor.rs
+++ src/server/actor.rs
@@ -65,7 +65,7 @@ impl ServerActorHandle {
 }
 
 const DB_SIZE: usize = 1 << 24;
-const DB_BUFFER: usize = 8 * 1_000;
+const DB_BUFFER: usize = 8 * (1 << 16);
 const DB_CHUNK_SIZE: usize = 1 << 16;
 const N_QUERIES: usize = 64;
 const QUERIES: usize = ROTATIONS * N_QUERIES;
