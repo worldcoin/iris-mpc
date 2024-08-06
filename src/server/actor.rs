0c00d1175184f86636ebda76fcca97a45bd31d0a
philsippl <philsippl@gmail.com>
2024-08-06 15:17:09 +0000

    up

--- src/server/actor.rs
+++ src/server/actor.rs
@@ -11,7 +11,11 @@ use crate::{
 };
 use cudarc::{
     cublas::CudaBlas,
-    driver::{result, CudaDevice, CudaSlice, CudaStream, DevicePtr},
+    driver::{
+        result::{self, mem_prefetch_async},
+        sys::{CUmemLocationType, CUmemLocation_st},
+        CudaDevice, CudaSlice, CudaStream, DevicePtr,
+    },
     nccl::Comm,
 };
 use futures::{Future, FutureExt};
@@ -428,6 +432,7 @@ impl ServerActor {
         let now = Instant::now();
         tracing::debug!(party_id = self.party_id, "Start DB deduplication");
         let mut db_chunk_idx = 0;
+        let mut mem_offsets = vec![0; self.device_manager.device_count()];
         loop {
             tracing::debug!(
                 party_id = self.party_id,
@@ -452,6 +457,31 @@ impl ServerActor {
                 .iter()
                 .map(|s| s.div_ceil(4) * 4)
                 .collect::<Vec<_>>();
+
+            // Prefetch the next chunk already
+            for i in 0..self.device_manager.device_count() {
+                self.device_manager.device(i).bind_to_thread().unwrap();
+                for ptr in &[
+                    self.code_db_slices.code_gr.limb_0[i],
+                    self.code_db_slices.code_gr.limb_1[i],
+                    self.mask_db_slices.code_gr.limb_0[i],
+                    self.mask_db_slices.code_gr.limb_1[i],
+                ] {
+                    unsafe {
+                        mem_prefetch_async(
+                            ptr + DB_CHUNK_SIZE as u64
+                                + mem_offsets[i] as u64 * IRIS_CODE_LENGTH as u64,
+                            DB_CHUNK_SIZE,
+                            CUmemLocation_st {
+                                type_: CUmemLocationType::CU_MEM_LOCATION_TYPE_DEVICE,
+                                id:    i as i32,
+                            },
+                            request_streams[i].stream,
+                        )
+                        .unwrap();
+                    }
+                }
+            }
 
             // First stream doesn't need to wait
             if db_chunk_idx == 0 {
@@ -587,6 +617,12 @@ impl ServerActor {
 
             // Increment chunk index
             db_chunk_idx += 1;
+
+            mem_offsets = mem_offsets
+                .iter()
+                .zip(chunk_size.iter())
+                .map(|(offset, chunk_size)| offset + chunk_size)
+                .collect();
 
             tracing::debug!(
                 party_id = self.party_id,
