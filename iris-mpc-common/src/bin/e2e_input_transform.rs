3010c2359ee51c9677a15be150b59fd3d0b39c5c
Ertugrul Aypek <ertugrul.aypek@toolsforhumanity.com>
2024-08-22 16:13:16 +0200

    refactor the replicated section into a function and improve logs

--- iris-mpc-common/src/bin/e2e_input_transform.rs
+++ iris-mpc-common/src/bin/e2e_input_transform.rs
@@ -37,43 +37,25 @@ impl Signup {
         let iris_code_right = IrisCodeArray::from_base64(&self.iris_code_right).unwrap();
         let mask_code_right = IrisCodeArray::from_base64(&self.mask_code_right).unwrap();
 
-        let mut empty_string_array: [String; 3] = Default::default();
-
         // Encode iris code shares left
         let shares_iris_code_left =
             GaloisRingIrisCodeShare::encode_iris_code(&iris_code_left, &mask_code_left, &mut rng);
-        for i in 0..3 {
-            empty_string_array[i] = general_purpose::STANDARD
-                .encode(bytemuck::cast_slice(&shares_iris_code_left[i].coefs));
-        }
-        self.iris_code_shares_left = Some(empty_string_array.clone());
+        self.iris_code_shares_left = Some(get_share_strings(&shares_iris_code_left));
 
         // Encode iris code shares right
         let shares_iris_code_right =
             GaloisRingIrisCodeShare::encode_iris_code(&iris_code_right, &mask_code_right, &mut rng);
-        for i in 0..3 {
-            empty_string_array[i] = general_purpose::STANDARD
-                .encode(bytemuck::cast_slice(&shares_iris_code_right[i].coefs));
-        }
-        self.iris_code_shares_right = Some(empty_string_array.clone());
+        self.iris_code_shares_right = Some(get_share_strings(&shares_iris_code_right));
 
         // Encode mask code shares left
         let shares_mask_code_left =
             GaloisRingIrisCodeShare::encode_mask_code(&mask_code_left, &mut rng);
-        for i in 0..3 {
-            empty_string_array[i] = general_purpose::STANDARD
-                .encode(bytemuck::cast_slice(&shares_mask_code_left[i].coefs));
-        }
-        self.mask_code_shares_left = Some(empty_string_array.clone());
+        self.mask_code_shares_left = Some(get_share_strings(&shares_mask_code_left));
 
         // Encode mask code shares right
         let shares_mask_code_right =
             GaloisRingIrisCodeShare::encode_mask_code(&mask_code_right, &mut rng);
-        for i in 0..3 {
-            empty_string_array[i] = general_purpose::STANDARD
-                .encode(bytemuck::cast_slice(&shares_mask_code_right[i].coefs));
-        }
-        self.mask_code_shares_right = Some(empty_string_array.clone());
+        self.mask_code_shares_right = Some(get_share_strings(&shares_mask_code_right));
     }
 }
 
@@ -84,6 +66,15 @@ struct Match {
     distance_right:     f64,
     manipulation_type:  String,
     normalization_used: bool,
+}
+
+fn get_share_strings(shares_iris_code_left: &[GaloisRingIrisCodeShare; 3]) -> [String; 3] {
+    let mut shares: [String; 3] = Default::default();
+    for i in 0..3 {
+        shares[i] =
+            general_purpose::STANDARD.encode(bytemuck::cast_slice(&shares_iris_code_left[i].coefs));
+    }
+    return shares;
 }
 
 fn read_json_file(file_path: &str) -> serde_json::Result<Vec<Signup>> {
@@ -104,14 +95,17 @@ fn write_json_file(file_path: &str, signups: &Vec<Signup>) -> std::io::Result<()
 }
 
 fn main() {
-    let mut signups = read_json_file("./iris-mpc-common/src/bin/data/ss_e2e.json").unwrap();
+    let input_file = "./iris-mpc-common/src/bin/data/ss_e2e.json";
+    let output_file = "./iris-mpc-common/src/bin/data/generated_ss_e2e_shares.json";
+    let mut signups = read_json_file(input_file).unwrap();
     for signup in &mut signups {
+        println!("Calculating shares for signup {:?}", signup.signup_id);
         signup.add_shares();
-        println!("{:?}", signup.mask_code_shares_right);
     }
     write_json_file(
         "./iris-mpc-common/src/bin/data/generated_ss_e2e_shares.json",
         &signups,
     )
     .unwrap();
+    println!("Share calculation completed. Results written to {:?}", output_file);
 }
