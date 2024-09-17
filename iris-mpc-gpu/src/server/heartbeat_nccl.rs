a46588bc91c83bb5bb49da251e1d0796dc1934cf
Wojciech Sromek <157375010+wojciechsromek@users.noreply.github.com>
2024-09-17 09:13:53 +0200

    fix: Increase heartbeat timeout (#388)

--- iris-mpc-gpu/src/server/heartbeat_nccl.rs
+++ iris-mpc-gpu/src/server/heartbeat_nccl.rs
@@ -7,7 +7,7 @@ use tokio::{
     task::{spawn_blocking, JoinHandle},
     time::timeout,
 };
-const HEARBEAT_INTERVAL: Duration = Duration::from_secs(5);
+const HEARBEAT_INTERVAL: Duration = Duration::from_secs(60);
 
 pub async fn start_heartbeat(
     party_id: usize,
