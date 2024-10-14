88e9e1a97b1908e7d408c04e942fba5dc3b81976
Carlo Mazzaferro <carlo.mazzaferro@toolsforhumanity.com>
2024-10-14 12:03:59 +0200

    fix reset script for new staging

--- scripts/purge_stage_dbs.sh
+++ scripts/purge_stage_dbs.sh
@@ -31,11 +31,11 @@ get_aws_secret() {
 SECRET_NAME="stage/iris-mpc/rds-master-password"
 REGION="eu-north-1"
 
-MPC_1_DATABASE_URL=$(get_aws_secret "$SECRET_NAME" "$REGION" "DATABASE_URL" "worldcoin-smpcv2-1")
-MPC_2_DATABASE_URL=$(get_aws_secret "$SECRET_NAME" "$REGION" "DATABASE_URL" "worldcoin-smpcv2-2")
-MPC_3_DATABASE_URL=$(get_aws_secret "$SECRET_NAME" "$REGION" "DATABASE_URL" "worldcoin-smpcv2-3")
+MPC_1_DATABASE_URL=$(get_aws_secret "$SECRET_NAME" "$REGION" "DATABASE_URL" "worldcoin-smpcv-io-0")
+MPC_2_DATABASE_URL=$(get_aws_secret "$SECRET_NAME" "$REGION" "DATABASE_URL" "worldcoin-smpcv-io-1")
+MPC_3_DATABASE_URL=$(get_aws_secret "$SECRET_NAME" "$REGION" "DATABASE_URL" "worldcoin-smpcv-io-2")
 
-kubectx arn:aws:eks:eu-north-1:767397983205:cluster/mpc1-stage || kubectx smpcv2-stage-1
+kubectx arn:aws:eks:eu-north-1:024848486749:cluster/smpcv2-0-stage || kubectx smpc-io-stage-0
 kubens iris-mpc
 kubectl apply -f db-cleaner-helper-pod.yaml
 echo "Waiting 10s for db-cleaner pod to be ready..."
@@ -45,7 +45,7 @@ kubectl exec -it db-cleaner -- bash -c "psql -H $MPC_1_DATABASE_URL -c 'SET sear
 kubectl exec -it db-cleaner -- bash -c "psql -H $MPC_1_DATABASE_URL -c 'SET search_path TO \"SMPC_stage_0\"; TRUNCATE results;'"
 kubectl delete pod --force db-cleaner
 
-kubectx arn:aws:eks:eu-north-1:381492197851:cluster/mpc2-stage || kubectx smpcv2-stage-2
+kubectx arn:aws:eks:eu-north-1:024848486818:cluster/smpcv2-1-stage || kubectx smpc-io-stage-1
 kubens iris-mpc
 kubectl apply -f db-cleaner-helper-pod.yaml
 echo "Waiting 10s for db-cleaner pod to be ready..."
@@ -55,7 +55,7 @@ kubectl exec -it db-cleaner -- bash -c "psql -H $MPC_2_DATABASE_URL -c 'SET sear
 kubectl exec -it db-cleaner -- bash -c "psql -H $MPC_2_DATABASE_URL -c 'SET search_path TO \"SMPC_stage_1\"; TRUNCATE results;'"
 kubectl delete pod --force db-cleaner
 
-kubectx arn:aws:eks:eu-north-1:590184084615:cluster/mpc3-stage || kubectx smpcv2-stage-3
+kubectx arn:aws:eks:eu-north-1:024848486770:cluster/smpcv2-2-stage || kubectx smpc-io-stage-2
 kubens iris-mpc
 kubectl apply -f db-cleaner-helper-pod.yaml
 echo "Waiting 10s for db-cleaner pod to be ready..."
