7928038529aeae4456c0a396a7c8ea90e903386f
philsippl <philsippl@gmail.com>
2024-11-02 18:56:12 +0100

    up

--- iris-mpc-gpu/src/server/actor.rs
+++ iris-mpc-gpu/src/server/actor.rs
@@ -492,14 +492,70 @@ impl ServerActor {
                 && batch_size == batch.metadata.len()
                 && batch_size == batch.store_right.code.len()
                 && batch_size == batch.store_right.mask.len()
-                && batch_size * ROTATIONS * IRIS_CODE_LENGTH == batch.query_left.code.len()
-                && batch_size * ROTATIONS * MASK_CODE_LENGTH == batch.query_left.mask.len()
-                && batch_size * ROTATIONS * IRIS_CODE_LENGTH == batch.query_right.code.len()
-                && batch_size * ROTATIONS * MASK_CODE_LENGTH == batch.query_right.mask.len()
-                && batch_size * ROTATIONS * IRIS_CODE_LENGTH == batch.db_left.code.len()
-                && batch_size * ROTATIONS * MASK_CODE_LENGTH == batch.db_left.mask.len()
-                && batch_size * ROTATIONS * IRIS_CODE_LENGTH == batch.db_right.code.len()
-                && batch_size * ROTATIONS * MASK_CODE_LENGTH == batch.db_right.mask.len(),
+                && batch_size * ROTATIONS * IRIS_CODE_LENGTH
+                    == batch
+                        .query_left
+                        .code
+                        .iter()
+                        .flatten()
+                        .collect::<Vec<_>>()
+                        .len()
+                && batch_size * ROTATIONS * MASK_CODE_LENGTH * 2
+                    == batch
+                        .query_left
+                        .mask
+                        .iter()
+                        .flatten()
+                        .collect::<Vec<_>>()
+                        .len()
+                && batch_size * ROTATIONS * IRIS_CODE_LENGTH * 2
+                    == batch
+                        .query_right
+                        .code
+                        .iter()
+                        .flatten()
+                        .collect::<Vec<_>>()
+                        .len()
+                && batch_size * ROTATIONS * MASK_CODE_LENGTH * 2
+                    == batch
+                        .query_right
+                        .mask
+                        .iter()
+                        .flatten()
+                        .collect::<Vec<_>>()
+                        .len()
+                && batch_size * ROTATIONS * IRIS_CODE_LENGTH * 2
+                    == batch
+                        .db_left
+                        .code
+                        .iter()
+                        .flatten()
+                        .collect::<Vec<_>>()
+                        .len()
+                && batch_size * ROTATIONS * MASK_CODE_LENGTH * 2
+                    == batch
+                        .db_left
+                        .mask
+                        .iter()
+                        .flatten()
+                        .collect::<Vec<_>>()
+                        .len()
+                && batch_size * ROTATIONS * IRIS_CODE_LENGTH * 2
+                    == batch
+                        .db_right
+                        .code
+                        .iter()
+                        .flatten()
+                        .collect::<Vec<_>>()
+                        .len()
+                && batch_size * ROTATIONS * MASK_CODE_LENGTH * 2
+                    == batch
+                        .db_right
+                        .mask
+                        .iter()
+                        .flatten()
+                        .collect::<Vec<_>>()
+                        .len(),
             "Query batch sizes mismatch"
         );
 
