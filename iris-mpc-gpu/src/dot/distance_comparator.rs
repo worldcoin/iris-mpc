1de0b2e430552beb9fc52e0ec6282d6dcc4e0a7a
philsippl <philsippl@gmail.com>
2024-11-23 19:16:32 +0100

    fix

--- iris-mpc-gpu/src/dot/distance_comparator.rs
+++ iris-mpc-gpu/src/dot/distance_comparator.rs
@@ -289,7 +289,7 @@ impl DistanceComparator {
             );
         }
 
-        const BATCH_MATCH_IDX: u32 = u32::MAX - (self.query_length / ROTATIONS); // batch matches have an index of u32::MAX - index
+        let batch_match_idx: u32 = u32::MAX - (self.query_length / ROTATIONS); // batch matches have an index of u32::MAX - index
         let mut matches_per_query = vec![vec![]; match_counters[0].len()];
         let n_devices = self.device_manager.device_count();
         for i in 0..self.device_manager.device_count() {
@@ -299,13 +299,13 @@ impl DistanceComparator {
                 let ids = results[i][offset..offset + min(len, ALL_MATCHES_LEN)]
                     .iter()
                     .map(|&idx| {
-                        if idx > BATCH_MATCH_IDX {
+                        if idx > batch_match_idx {
                             idx
                         } else {
                             idx * n_devices as u32 + i as u32
                         }
                     })
-                    .filter(|&idx| idx < BATCH_MATCH_IDX || i == 0) // take all normal matches, but only batch matches from device 0
+                    .filter(|&idx| idx < batch_match_idx || i == 0) // take all normal matches, but only batch matches from device 0
                     .collect::<Vec<_>>();
                 matches_per_query[j].extend_from_slice(&ids);
                 offset += ALL_MATCHES_LEN;
