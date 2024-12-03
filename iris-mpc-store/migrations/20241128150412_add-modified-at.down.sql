d538f21e814f29326950aa4eaea085cee20bca87
philsippl <philsippl@gmail.com>
2024-12-03 09:21:20 -0500

    up

--- iris-mpc-store/migrations/20241128150412_add-modified-at.down.sql
+++ iris-mpc-store/migrations/20241128150412_add-modified-at.down.sql
@@ -1,3 +1,3 @@
-DROP TRIGGER IF EXISTS set_last_modified_at ON mpc_store;
+DROP TRIGGER IF EXISTS set_last_modified_at ON irises;
 DROP FUNCTION IF EXISTS update_last_modified_at();
-ALTER TABLE mpc_store DROP COLUMN last_modified_at;
+ALTER TABLE irises DROP COLUMN last_modified_at;
