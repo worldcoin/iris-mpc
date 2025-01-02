e6b75d752b8171b6d2c80871bcc4899f1c27e68d
philsippl <philsippl@gmail.com>
2025-01-02 23:12:37 +0100

    revert dbg

--- iris-mpc-gpu/src/server/actor.rs
+++ iris-mpc-gpu/src/server/actor.rs
@@ -72,7 +72,7 @@ impl ServerActorHandle {
     }
 }
 
-const DB_CHUNK_SIZE: usize = 1 << 6;
+const DB_CHUNK_SIZE: usize = 1 << 15;
 const KDF_SALT: &str = "111a1a93518f670e9bb0c2c68888e2beb9406d4c4ed571dc77b801e676ae3091"; // Random 32 byte salt
 const SUPERMATCH_THRESHOLD: usize = 4_000;
 
