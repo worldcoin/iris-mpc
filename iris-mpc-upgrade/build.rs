16ae90aac997dc56dbd26caca306ccbcdb9bdd78
Carlo Mazzaferro <carlo.mazzaferro@gmail.com>
2025-01-09 09:26:10 -0800

    values for outstanding stage reshare servers (#894)
    * values for outstanding stage reshare servers
    
    * wrong party id
    
    * remove unneded

--- iris-mpc-upgrade/build.rs
+++ iris-mpc-upgrade/build.rs
@@ -1,6 +1,5 @@
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=proto/reshare.proto");
     tonic_build::configure()
         .out_dir("src/proto/")
         .compile_protos(
