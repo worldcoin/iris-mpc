ef5c0d649e5c27ea45b04a351a655ed4c4995f73
Carlo Mazzaferro <carlo.mazzaferro@gmail.com>
2025-01-09 06:45:56 -0800

    use fixed chacha seeds for e2e tests (#882)

--- iris-mpc/src/bin/server.rs
+++ iris-mpc/src/bin/server.rs
@@ -9,7 +9,7 @@ use clap::Parser;
 use eyre::{eyre, Context};
 use futures::{stream::select_all, StreamExt, TryStreamExt};
 use iris_mpc_common::{
-    config::{json_wrapper::JsonStrWrapper, Config, Opt},
+    config::{Config, Opt},
     galois_engine::degree4::{GaloisRingIrisCodeShare, GaloisRingTrimmedMaskCodeShare},
     helpers::{
         aws::{
@@ -528,35 +528,41 @@ fn initialize_tracing(config: &Config) -> eyre::Result<TracingShutdownHandle> {
     }
 }
 
-async fn initialize_chacha_seeds(
-    kms_key_arns: &JsonStrWrapper<Vec<String>>,
-    party_id: usize,
-) -> eyre::Result<([u32; 8], [u32; 8])> {
+async fn initialize_chacha_seeds(config: Config) -> eyre::Result<([u32; 8], [u32; 8])> {
     // Init RNGs
-    let own_key_arn = kms_key_arns
+    let own_key_arn = config
+        .kms_key_arns
         .0
-        .get(party_id)
+        .get(config.party_id)
         .expect("Expected value not found in kms_key_arns");
-    let dh_pairs = match party_id {
+    let dh_pairs = match config.party_id {
         0 => (1usize, 2usize),
         1 => (2usize, 0usize),
         2 => (0usize, 1usize),
         _ => unimplemented!(),
     };
 
-    let dh_pair_0: &str = kms_key_arns
+    let dh_pair_0: &str = config
+        .kms_key_arns
         .0
         .get(dh_pairs.0)
         .expect("Expected value not found in kms_key_arns");
-    let dh_pair_1: &str = kms_key_arns
+    let dh_pair_1: &str = config
+        .kms_key_arns
         .0
         .get(dh_pairs.1)
         .expect("Expected value not found in kms_key_arns");
 
-    let chacha_seeds = (
-        bytemuck::cast(derive_shared_secret(own_key_arn, dh_pair_0).await?),
-        bytemuck::cast(derive_shared_secret(own_key_arn, dh_pair_1).await?),
-    );
+    // To be used only for e2e testing where we use localstack. There's a bug in
+    // localstack's implementation of `derive_shared_secret`. See: https://github.com/localstack/localstack/pull/12071
+    let chacha_seeds: ([u32; 8], [u32; 8]) = if config.fixed_shared_secrets {
+        ([0u32; 8], [0u32; 8])
+    } else {
+        (
+            bytemuck::cast(derive_shared_secret(own_key_arn, dh_pair_0).await?),
+            bytemuck::cast(derive_shared_secret(own_key_arn, dh_pair_1).await?),
+        )
+    };
 
     Ok(chacha_seeds)
 }
@@ -695,7 +701,7 @@ async fn server_main(config: Config) -> eyre::Result<()> {
 
     let party_id = config.party_id;
     tracing::info!("Deriving shared secrets");
-    let chacha_seeds = initialize_chacha_seeds(&config.kms_key_arns, party_id).await?;
+    let chacha_seeds = initialize_chacha_seeds(config.clone()).await?;
 
     let uniqueness_result_attributes = create_message_type_attribute_map(UNIQUENESS_MESSAGE_TYPE);
     let identity_deletion_result_attributes =
