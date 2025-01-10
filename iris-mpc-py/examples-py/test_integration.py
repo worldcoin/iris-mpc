9bce4fd8629e536ef0ad7f49d464e2e0e9ec84e1
Bryan Gillespie <bryan@inversed.tech>
2025-01-10 07:25:54 -0700

    Modify Python integration test to store data files in ignored directory

--- iris-mpc-py/examples-py/test_integration.py
+++ iris-mpc-py/examples-py/test_integration.py
@@ -1,4 +1,10 @@
+import os
 from iris_mpc_py import PyIrisCode, PyPlaintextStore, PyGraphStore, PyHnswSearcher
+
+vector_filename = "./data/vector.ndjson"
+graph_filename = "./data/graph1.dat"
+if not os.path.exists("./data/"):
+    os.makedirs("./data/")
 
 print("Generating 100k uniform random iris codes...")
 vector_init = PyPlaintextStore()
@@ -9,13 +15,13 @@ for i in range(1,100000):
 
 # write vector store to file
 print("Writing vector store to file...")
-vector_init.write_to_ndjson("vector.ndjson")
+vector_init.write_to_ndjson(vector_filename)
 
 print("Generating HNSW graphs for 10k imported iris codes...")
 hnsw = PyHnswSearcher(32, 64, 32)
 vector1 = PyPlaintextStore()
 graph1 = PyGraphStore()
-hnsw.fill_from_ndjson_file("vector.ndjson", vector1, graph1, 10000)
+hnsw.fill_from_ndjson_file(vector_filename, vector1, graph1, 10000)
 
 print("Imported length:", vector1.len())
 
@@ -27,11 +33,11 @@ print("Search for random query iris code:", hnsw.search(query, vector1, graph1))
 
 # write graph store to file
 print("Writing graph store to file...")
-graph1.write_to_bin("graph1.dat")
+graph1.write_to_bin(graph_filename)
 
 # read HNSW graphs from disk
 print("Reading vector and graph stores from file...")
-vector2 = PyPlaintextStore.read_from_ndjson("vector.ndjson", 10000)
-graph2 = PyGraphStore.read_from_bin("graph1.dat")
+vector2 = PyPlaintextStore.read_from_ndjson(vector_filename, 10000)
+graph2 = PyGraphStore.read_from_bin(graph_filename)
 
 print("Search for random query iris code:", hnsw.search(query, vector2, graph2))
