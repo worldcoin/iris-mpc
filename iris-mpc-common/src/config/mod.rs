2e573a6c62e7d838053b56959039b0f3e624e978
Ertugrul Aypek <ertugrul.aypek@toolsforhumanity.com>
2025-01-27 00:22:37 +0100

    try 10% page lock chunk

--- iris-mpc-common/src/config/mod.rs
+++ iris-mpc-common/src/config/mod.rs
@@ -121,12 +121,6 @@ pub struct Config {
     #[serde(default)]
     pub load_chunks_static_ips: usize,
 
-    #[serde(default)]
-    pub test_load_into_memory: bool,
-
-    #[serde(default)]
-    pub page_lock_at_beginning: bool,
-
     /// Percentage of the chunk size to page lock at each iteration
     /// Must be a positive integer between [1-100]
     /// The first memory chunk will be page-locked before starting db & s3
