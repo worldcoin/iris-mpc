3b15eb422510c61b50d377d9e50889c002e7e563
Aurélien Nicolas <info@nau.re>
2025-01-27 14:49:28 +0100

    aurel/complex-request: Generalize to left/right stores

--- iris-mpc-cpu/src/execution/hawk_main.rs
+++ iris-mpc-cpu/src/execution/hawk_main.rs
@@ -53,20 +53,27 @@ pub struct HawkActor {
 
     // ---- My state ----
     // TODO: Persistence.
-    iris_store:  SharedIrisesRef,
-    graph_store: GraphRef,
+    iris_store:  [SharedIrisesRef; 2],
+    graph_store: [GraphRef; 2],
 
     // ---- My network setup ----
     networking:   GrpcNetworking,
     own_identity: Identity,
 }
 
+#[derive(Clone, Copy, Debug)]
+pub enum StoreId {
+    Left,
+    Right,
+}
+
 type GraphRef = Arc<RwLock<GraphMem<Aby3Store>>>;
 
 /// HawkSession is a unit of parallelism when operating on the HawkActor.
 pub struct HawkSession {
-    aby3_store: Aby3Store,
-    shared_rng: AesRng,
+    aby3_store:  Aby3Store,
+    graph_store: GraphRef,
+    shared_rng:  AesRng,
 }
 
 type HawkSessionRef = Arc<RwLock<HawkSession>>;
@@ -145,10 +152,13 @@ impl HawkActor {
             .into_iter()
             .collect::<Result<()>>()?;
 
+        let iris_store = [(); 2].map(|_| SharedIrisesRef::default());
+        let graph_store = [(); 2].map(|_| Arc::new(RwLock::new(GraphMem::<Aby3Store>::new())));
+
         Ok(HawkActor {
             search_params,
-            iris_store: SharedIrisesRef::default(),
-            graph_store: Arc::new(RwLock::new(GraphMem::<Aby3Store>::new())),
+            iris_store,
+            graph_store,
             role_assignments: Arc::new(role_assignments),
             consensus: Consensus::default(),
             networking,
@@ -156,12 +166,24 @@ impl HawkActor {
         })
     }
 
-    pub async fn new_session(&mut self) -> Result<HawkSession> {
-        let session_id = self.consensus.next_session_id();
-        self.create_session(session_id).await
+    fn iris_store(&self, store_id: StoreId) -> SharedIrisesRef {
+        self.iris_store[store_id as usize].clone()
     }
 
-    async fn create_session(&self, session_id: SessionId) -> Result<HawkSession> {
+    fn graph_store(&self, store_id: StoreId) -> GraphRef {
+        self.graph_store[store_id as usize].clone()
+    }
+
+    pub async fn new_session(&mut self, store_id: StoreId) -> Result<HawkSession> {
+        let session_id = self.consensus.next_session_id();
+        self.create_session(store_id, session_id).await
+    }
+
+    async fn create_session(
+        &self,
+        store_id: StoreId,
+        session_id: SessionId,
+    ) -> Result<HawkSession> {
         // TODO: cleanup of dropped sessions.
         self.networking.create_session(session_id).await?;
 
@@ -185,7 +207,7 @@ impl HawkActor {
 
         let aby3_store = Aby3Store {
             session,
-            storage: self.iris_store.clone(),
+            storage: self.iris_store(store_id),
             owner: self.own_identity.clone(),
         };
 
@@ -194,6 +216,7 @@ impl HawkActor {
 
         Ok(HawkSession {
             aby3_store,
+            graph_store: self.graph_store(store_id),
             shared_rng,
         })
     }
@@ -229,13 +252,13 @@ impl HawkActor {
             let tasks = izip!(chunk, sessions)
                 .map(|(iris, session)| {
                     let search_params = Arc::clone(&self.search_params);
-                    let graph_store = Arc::clone(&self.graph_store);
                     let session = Arc::clone(session);
                     let iris = iris.clone();
 
                     tokio::spawn(async move {
-                        let graph_store = graph_store.read().await;
                         let mut session = session.write().await;
+                        let graph_store = Arc::clone(&session.graph_store);
+                        let graph_store = graph_store.read().await;
                         Self::search_to_insert_one(&search_params, &graph_store, &mut session, iris)
                             .await
                     })
@@ -299,7 +322,7 @@ impl HawkActor {
         insert_plan: InsertPlan,
     ) -> Result<ConnectPlan> {
         let inserted = session.aby3_store.insert(&insert_plan.query).await;
-        let mut graph_store = self.graph_store.write().await;
+        let mut graph_store = session.graph_store.write().await;
 
         let connect_plan = self
             .search_params
@@ -335,32 +358,34 @@ pub struct HawkHandle {
 
 impl HawkHandle {
     pub async fn new(mut hawk_actor: HawkActor, request_parallelism: usize) -> Result<Self> {
-        let mut sessions = vec![];
-        for _ in 0..request_parallelism {
-            let session = hawk_actor.new_session().await?;
-            sessions.push(Arc::new(RwLock::new(session)));
-        }
+        let sessions = [
+            Self::new_sessions(&mut hawk_actor, request_parallelism, StoreId::Left).await?,
+            Self::new_sessions(&mut hawk_actor, request_parallelism, StoreId::Right).await?,
+        ];
 
         let (tx, mut rx) = tokio::sync::mpsc::channel::<HawkJob>(1);
 
         // ---- Request Handler ----
         tokio::spawn(async move {
             while let Some(job) = rx.recv().await {
+                // TODO: both eyes.
+                let sessions = &sessions[0];
+
                 // TODO: obtain rotated and mirrored versions.
                 let rotated = job.request.clone();
 
                 // Search for nearest neighbors.
-                let _search_results = hawk_actor.search(&sessions, rotated).await.unwrap();
+                let _search_results = hawk_actor.search(sessions, rotated).await.unwrap();
 
                 // TODO: Compare with threshold.
                 let to_insert = job.request;
 
                 // Insert into the database.
                 let plans = hawk_actor
-                    .search_to_insert(&sessions, to_insert)
+                    .search_to_insert(sessions, to_insert)
                     .await
                     .unwrap();
-                let connect_plans = hawk_actor.insert(&sessions, plans).await.unwrap();
+                let connect_plans = hawk_actor.insert(sessions, plans).await.unwrap();
 
                 println!("🎉 Inserted items into the database");
 
@@ -369,6 +394,19 @@ impl HawkHandle {
         });
 
         Ok(Self { job_queue: tx })
+    }
+
+    async fn new_sessions(
+        hawk_actor: &mut HawkActor,
+        request_parallelism: usize,
+        store_id: StoreId,
+    ) -> Result<Vec<HawkSessionRef>> {
+        let mut sessions = vec![];
+        for _ in 0..request_parallelism {
+            let session = hawk_actor.new_session(store_id).await?;
+            sessions.push(Arc::new(RwLock::new(session)));
+        }
+        Ok(sessions)
     }
 
     pub async fn submit(&self, request: HawkRequest) -> Result<HawkResult> {
