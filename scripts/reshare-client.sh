c67aa45a1816a6636fa57fc8bdd6d7bd4da4dd6b
Bartosz Jasinski <32450522+jazzbee@users.noreply.github.com>
2025-02-21 15:47:01 +0100

    feat: don't print command to avoid print secrets (#1051)
    * feat: don't print command to avoid print secrets
    
    * fix: typo

--- scripts/reshare-client.sh
+++ scripts/reshare-client.sh
@@ -137,7 +137,7 @@ COMMAND="reshare-client \
     --ca-root-file-path $CLIENT_TLS_CERT_PATH"
 
 # Display or execute command
-echo "Generated command:"
-echo "$COMMAND"
+#echo "Generated command:"
+#echo "$COMMAND"
 
 exec $COMMAND
