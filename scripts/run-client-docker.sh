c44063f5c1800608737430414d52625fd3dc46fd
Aurel <aurel@inversed.tech>
2025-02-27 15:58:19 +0100

    aurel/ci-client-request: endpoint localstack

--- scripts/run-client-docker.sh
+++ scripts/run-client-docker.sh
@@ -3,7 +3,7 @@ set -e
 
 export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
-export AWS_ENDPOINT_URL="http://localhost:4566"
+export AWS_ENDPOINT_URL="http://localstack:4566"
 export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
 export AWS_REGION=us-east-1
