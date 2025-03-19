4e5538cd3810bc301063980736977342440c7471
Danielle Nagar @ TFH <danielle.nagar@toolsforhumanity.com>
2025-03-19 15:34:47 +0100

    Fix purge stage dbs script (#1154)
    * Update purge stage dbs script
    
    * Update scripts/purge_stage/db-cleaner-helper-pod-ampc-hnsw.yaml
    
    Co-authored-by: semgrep-code-worldcoin[bot] <176410932+semgrep-code-worldcoin[bot]@users.noreply.github.com>
    
    * Update scripts/purge_stage/db-cleaner-helper-pod-iris-mpc.yaml
    
    Co-authored-by: semgrep-code-worldcoin[bot] <176410932+semgrep-code-worldcoin[bot]@users.noreply.github.com>
    
    * Fix comments
    
    ---------
    
    Co-authored-by: semgrep-code-worldcoin[bot] <176410932+semgrep-code-worldcoin[bot]@users.noreply.github.com>

--- scripts/purge_stage/delete_mongodb_cpu_collections.sh
+++ scripts/purge_stage/delete_mongodb_cpu_collections.sh
@@ -5,7 +5,7 @@ set -e
 CONNECTION_STRING="$1"
 
 DATABASE="iris"
-COLLECTIONS="mpcv2.cpu.results mpcv2.cpu.results.partial mpcv2.cpu.results.deletion mpcv2.cpu.results.anonymized_statistics"
+COLLECTIONS="mpcv2.hnsw.results mpcv2.hnsw.results.partial mpcv2.hnsw.results.deletion mpcv2.hnsw.results.anonymized_statistics mpcv2.hnsw.reauth.results mpcv2.hnsw.reauth.results.partial"
 
 for COLLECTION in $COLLECTIONS; do
     echo "Deleting collection $COLLECTION from database $DATABASE..."
--- scripts/purge_stage/purge_stage_dbs.sh
+++ scripts/purge_stage/purge_stage_dbs.sh
@@ -14,10 +14,18 @@ CLEANUP_TYPE=$1
 # Set the appropriate secret name based on cleanup type
 if [ "$CLEANUP_TYPE" == "cpu" ]; then
   SECRET_NAME="stage/hnsw-mpc/rds-aurora-master-password"
-  echo "Using GPU secret: $SECRET_NAME"
-elif [ "$CLEANUP_TYPE" == "gpu" ]; then
-  SECRET_NAME="stage/iris-mpc/rds-aurora-master-password"
+  CLUSTER_NAME="ampc-hnsw"
+  NAMESPACE="ampc-hnsw"
   echo "Using CPU secret: $SECRET_NAME"
+  echo "Using CPU cluster name: $CLUSTER_NAME"
+  echo "Using CPU namespace: $NAMESPACE"
+  elif [ "$CLEANUP_TYPE" == "gpu" ]; then
+  SECRET_NAME="stage/iris-mpc/rds-aurora-master-password"
+  CLUSTER_NAME="smpcv2"
+  NAMESPACE="iris-mpc"
+  echo "Using GPU secret: $SECRET_NAME" 
+  echo "Using GPU cluster name: $CLUSTER_NAME"
+  echo "Using GPU namespace: $NAMESPACE"
 else
   printf "\nUnknown cleanup type: %s\n" "$CLEANUP_TYPE"
   printf "Available cleanup types: gpu, cpu\n"
@@ -55,35 +63,44 @@ get_aws_secret() {
 clean_mpc_database() {
   local DATABASE_URL=$1
   local PARTY_ID=$2
-  local CLUSTER=$3
+  local ACCOUNT_ID=$3
+  local CLUSTER_NAME=$4
+  local NAMESPACE=$5
 
-  if [ -z "$DATABASE_URL" ] || [ -z "$PARTY_ID" ] || [ -z "$CLUSTER" ]; then
-    echo "Database URL, party ID, and cluster are required"
+
+  if [ -z "$DATABASE_URL" ] || [ -z "$PARTY_ID" ] || [ -z "$ACCOUNT_ID" ] || [ -z "$CLUSTER_NAME" ] || [-z "$NAMESPACE" ]; then
+    echo "Database URL, party ID, account ID, cluster name and namespace are required"
     exit 1
   fi
 
-  echo "Cleaning database for $PARTY_ID..."
+  CLUSTER="arn:aws:eks:eu-north-1:$ACCOUNT_ID:cluster/$CLUSTER_NAME-$PARTY_ID-stage"
+  echo "Cleaning database for $PARTY_ID with cluster name $CLUSTER_NAME"
   
   # Switch to the appropriate Kubernetes context
-  kubectx $CLUSTER || kubectx smpc-io-stage-$PARTY_ID
-  kubens iris-mpc
+  kubectx $CLUSTER
+  kubens $NAMESPACE
   
   # Create and use a temporary pod for database operations
-  kubectl apply -f db-cleaner-helper-pod.yaml
+  kubectl apply -f db-cleaner-helper-pod-$NAMESPACE.yaml
   echo "Waiting 10s for db-cleaner pod to be ready..."
   sleep 10
+
+  # Remove the last segment after the last slash
+  DATABASE_URL_WITH_CORRECT_NAME=$(echo "$DATABASE_URL" | sed 's|/[^/]*$||')
+
+  echo "Cleaning Database for URL: $DATABASE_URL_WITH_CORRECT_NAME"
   
   # Execute database cleanup commands
-  kubectl exec -it db-cleaner -- bash -c "psql -H $DATABASE_URL -c 'SET search_path TO \"SMPC_stage_$PARTY_ID\"; TRUNCATE irises RESTART IDENTITY;'"
-  kubectl exec -it db-cleaner -- bash -c "psql -H $DATABASE_URL -c 'SET search_path TO \"SMPC_stage_$PARTY_ID\"; TRUNCATE sync RESTART IDENTITY;'"
-  kubectl exec -it db-cleaner -- bash -c "psql -H $DATABASE_URL -c 'SET search_path TO \"SMPC_stage_$PARTY_ID\"; TRUNCATE results RESTART IDENTITY;'"
-  kubectl exec -it db-cleaner -- bash -c "psql -H $DATABASE_URL -c 'SET search_path TO \"SMPC_stage_$PARTY_ID\"; TRUNCATE modifications RESTART IDENTITY;'"
-  kubectl exec -it db-cleaner -- bash -c "psql -H $DATABASE_URL -c 'SET search_path TO \"SMPC_stage_$PARTY_ID\"; TRUNCATE hawk_graph_entry RESTART IDENTITY;'"
-  kubectl exec -it db-cleaner -- bash -c "psql -H $DATABASE_URL -c 'SET search_path TO \"SMPC_stage_$PARTY_ID\"; TRUNCATE hawk_graph_links RESTART IDENTITY;'"
+  kubectl exec -it db-cleaner -- bash -c "psql -H $DATABASE_URL_WITH_CORRECT_NAME -c 'SET search_path TO \"SMPC_stage_$PARTY_ID\"; TRUNCATE irises RESTART IDENTITY;'"
+  kubectl exec -it db-cleaner -- bash -c "psql -H $DATABASE_URL_WITH_CORRECT_NAME -c 'SET search_path TO \"SMPC_stage_$PARTY_ID\"; TRUNCATE sync RESTART IDENTITY;'"
+  kubectl exec -it db-cleaner -- bash -c "psql -H $DATABASE_URL_WITH_CORRECT_NAME -c 'SET search_path TO \"SMPC_stage_$PARTY_ID\"; TRUNCATE results RESTART IDENTITY;'"
+  kubectl exec -it db-cleaner -- bash -c "psql -H $DATABASE_URL_WITH_CORRECT_NAME -c 'SET search_path TO \"SMPC_stage_$PARTY_ID\"; TRUNCATE modifications RESTART IDENTITY;'"
+  kubectl exec -it db-cleaner -- bash -c "psql -H $DATABASE_URL_WITH_CORRECT_NAME -c 'SET search_path TO \"SMPC_stage_$PARTY_ID\"; TRUNCATE hawk_graph_entry RESTART IDENTITY;'"
+  kubectl exec -it db-cleaner -- bash -c "psql -H $DATABASE_URL_WITH_CORRECT_NAME -c 'SET search_path TO \"SMPC_stage_$PARTY_ID\"; TRUNCATE hawk_graph_links RESTART IDENTITY;'"
   
   # Clean up and restart deployment
   kubectl delete pod --force db-cleaner
-  kubectl rollout restart deployment iris-mpc -n iris-mpc
+  kubectl rollout restart deployment $NAMESPACE -n $NAMESPACE
   
   echo "Cleanup completed for $PARTY_ID"
 }
@@ -104,8 +121,8 @@ if [ -z "$MPC_0_DATABASE_URL" ] || [ -z "$MPC_1_DATABASE_URL" ] || [ -z "$MPC_2_
   exit 1
 fi
 
-clean_mpc_database "$MPC_0_DATABASE_URL" "0" arn:aws:eks:eu-north-1:$MPC_0_STAGE_ACCOUNT_ID:cluster/smpcv2-0-stage
-clean_mpc_database "$MPC_1_DATABASE_URL" "1" arn:aws:eks:eu-north-1:$MPC_1_STAGE_ACCOUNT_ID:cluster/smpcv2-1-stage
-clean_mpc_database "$MPC_2_DATABASE_URL" "2" arn:aws:eks:eu-north-1:$MPC_2_STAGE_ACCOUNT_ID:cluster/smpcv2-2-stage
+clean_mpc_database "$MPC_0_DATABASE_URL" "0" $MPC_0_STAGE_ACCOUNT_ID $CLUSTER_NAME $NAMESPACE
+clean_mpc_database "$MPC_1_DATABASE_URL" "1" $MPC_1_STAGE_ACCOUNT_ID $CLUSTER_NAME $NAMESPACE
+clean_mpc_database "$MPC_2_DATABASE_URL" "2" $MPC_2_STAGE_ACCOUNT_ID $CLUSTER_NAME $NAMESPACE
 
 echo "All database cleanup operations completed successfully."
