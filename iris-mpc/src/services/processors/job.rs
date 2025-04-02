26b1dff7fd910d7a5477836195f924805c1c19ed
leonanos8 <nanomail4@gmail.com>
2025-04-02 12:46:51 +0200

    fix iris-mpc cpu compilation error

--- iris-mpc/src/services/processors/job.rs
+++ iris-mpc/src/services/processors/job.rs
@@ -55,6 +55,7 @@ pub async fn process_job_result(
         reauth_or_rule_used,
         modifications,
         actor_data: hawk_mutation,
+        full_face_mirror_attack_detected,
         ..
     } = job_result;
     let now = Instant::now();
