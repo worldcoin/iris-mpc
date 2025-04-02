92c9b4e54d630449233c498ca99935cfba08f230
Carlo Mazzaferro <carlo.mazzaferro@toolsforhumanity.com>
2025-04-02 13:54:41 +0200

    fmt

--- iris-mpc-common/src/helpers/mod.rs
+++ iris-mpc-common/src/helpers/mod.rs
@@ -2,6 +2,7 @@
 pub mod aws;
 #[cfg(feature = "helpers")]
 pub mod aws_sigv4;
+pub mod batch_sync;
 #[cfg(feature = "helpers")]
 pub mod inmemory_store;
 #[cfg(feature = "helpers")]
@@ -23,5 +24,4 @@ pub mod statistics;
 pub mod sync;
 #[cfg(feature = "helpers")]
 pub mod task_monitor;
-pub mod batch_sync;
 pub mod utils;
