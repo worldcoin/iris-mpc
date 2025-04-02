b50e158a025c515bec78b1d49d7928e7d54a70f1
leonanos8 <nanomail4@gmail.com>
2025-04-02 11:59:48 +0200

    add new boolean vector field to track mirror attacks in the ServerJobResult

--- iris-mpc-common/src/job.rs
+++ iris-mpc-common/src/job.rs
@@ -96,7 +96,6 @@ pub struct ServerJobResult<A = ()> {
     // Boolean array to indicate if the query was unique which includes the matches that were not
     // entered into the DB
     pub matches_with_skip_persistence: Vec<bool>,
-
     // For each query, the serial ids to which the query matched to
     pub match_ids: Vec<Vec<u32>>,
     // For each query, the serial ids to which the query partially matched to
@@ -129,8 +128,10 @@ pub struct ServerJobResult<A = ()> {
     // Keeping track of updates & deletions for sync mechanism. Mapping: Serial id -> Modification
     // Used for roll forward in the case of needing to r-run mutations
     pub modifications: HashMap<u32, Modification>,
-    /// Actor-specific data (e.g. graph mutations).
+    // Actor-specific data (e.g. graph mutations).
     pub actor_data: A,
+    // Boolean array to indicate if the query is a full face mirror attack attempt.
+    pub full_face_mirror_attack_detected: Vec<bool>,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Serialize, Deserialize, Default)]
