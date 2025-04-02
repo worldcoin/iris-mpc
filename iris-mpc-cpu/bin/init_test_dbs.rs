b05648b348fcb4435b8f179f68afff75feebd20b
Bryan Gillespie <bryan@inversed.tech>
2025-04-02 16:15:01 -0600

    Add support for two iris sides

--- iris-mpc-cpu/bin/init_test_dbs.rs
+++ iris-mpc-cpu/bin/init_test_dbs.rs
@@ -2,14 +2,17 @@ use std::{error::Error, fs::File, io::BufReader, path::PathBuf};
 
 use aes_prng::AesRng;
 use clap::Parser;
+use iris_mpc_common::iris_db::iris::IrisCode;
 use iris_mpc_cpu::{
+    execution::hawk_main::{BothEyes, STORE_IDS},
     hawkers::plaintext_store::PlaintextStore,
     hnsw::{GraphMem, HnswParams, HnswSearcher},
     protocol::shared_iris::GaloisRingSharedIris,
     py_bindings::{limited_iterator, plaintext_store::Base64IrisCode},
 };
-use rand::SeedableRng;
+use rand::{RngCore, SeedableRng};
 use serde_json::Deserializer;
+use tokio::{sync::mpsc, task::JoinSet};
 use tracing::info;
 
 #[allow(non_snake_case)]
@@ -21,10 +24,7 @@ struct Args {
     #[clap(short = 'n')]
     database_size: Option<usize>,
 
-    // TODO replace with support for serial ranges so external driver script can
-    // handle checkpoints
-    #[clap(long("checkpoints"), value_delimiter = ',')]
-    checkpoints: Vec<usize>,
+    // TODO support for processing specific index ranges from file
 
     // TODO database parameters
 
@@ -69,90 +69,109 @@ async fn main() -> Result<(), Box<dyn Error>> {
     if let Some(q) = layer_probability {
         params.layer_probability = q
     }
-    let searcher = HnswSearcher { params };
 
     let mut hnsw_rng = AesRng::seed_from_u64(args.hnsw_prng_seed);
     let mut aby3_rng = AesRng::seed_from_u64(args.aby3_prng_seed);
 
     // TODO establish connections with databases
-
     // TODO read existing graph and vector stores from database
-    let mut vector = PlaintextStore::new();
-    let mut graph = GraphMem::new();
 
     info!(
         "Opening NDJSON file of plaintext iris codes: {:?}",
         args.iris_codes_file
     );
 
-    let file = File::open(args.iris_codes_file.as_path()).unwrap();
-    let reader = BufReader::new(file);
-    let stream = Deserializer::from_reader(reader).into_iter::<Base64IrisCode>();
-    let stream = limited_iterator(stream, args.database_size);
+    let (tx_l, rx_l) = mpsc::channel::<IrisCode>(256);
+    let (tx_r, rx_r) = mpsc::channel::<IrisCode>(256);
 
-    info!("Building in-memory plaintext vector store and HNSW graph");
+    let receivers: BothEyes<_> = [rx_l, rx_r];
 
-    // Iterate over deserialized objects
-    let mut counter = 0usize;
+    let mut jobs = JoinSet::new();
 
-    for json_pt in stream {
-        let raw_query = (&json_pt.unwrap()).into();
-        let query = vector.prepare_query(raw_query);
-        searcher
-            .insert(&mut vector, &mut graph, &query, &mut hnsw_rng)
-            .await;
+    tokio::task::spawn_blocking(move || {
+        let file = File::open(args.iris_codes_file.as_path()).unwrap();
+        let reader = BufReader::new(file);
+        let stream = Deserializer::from_reader(reader).into_iter::<Base64IrisCode>();
+        let stream = limited_iterator(stream, args.database_size.map(|x| 2 * x));
 
-        counter += 1;
-        if counter % 1000 == 0 {
-            info!("Processed {} plaintext entries", counter);
+        let processors: BothEyes<_> = [tx_l, tx_r];
+        for (idx, json_pt) in stream.enumerate() {
+            let raw_query = (&json_pt.unwrap()).into();
+
+            let side = idx % 2;
+            processors[side].blocking_send(raw_query).unwrap();
         }
+    });
 
-        if args.checkpoints.contains(&counter) {
-            info!(
-                "Persisting graph checkpoint at {} iris codes to database",
-                counter
-            );
+    for (side, mut rx) in STORE_IDS.iter().copied().zip(receivers.into_iter()) {
+        let params = params.clone();
+        let mut hnsw_rng = AesRng::seed_from_u64(hnsw_rng.next_u64());
 
-            // Postgres backup -- eg_backup, pg_restore, call from shell
-            // Call backups from CLI
-            persist_graph_db(&graph);
+        jobs.spawn(async move {
+            let searcher = HnswSearcher { params };
+            let mut vector = PlaintextStore::new();
+            let mut graph: GraphMem<PlaintextStore> = GraphMem::new();
+            let mut counter = 0usize;
 
-            // existing graph is read from database
-            // index range for build -- from, to
-            // test: 0 to 100k
-        }
+            while let Some(raw_query) = rx.recv().await {
+                let query = vector.prepare_query(raw_query);
+                searcher
+                    .insert(&mut vector, &mut graph, &query, &mut hnsw_rng)
+                    .await;
+
+                counter += 1;
+                if counter % 1000 == 0 {
+                    info!("Processed {} plaintext entries for {} side", counter, side);
+                }
+            }
+
+            (side, Some(vector), Some(graph))
+        });
     }
 
-    info!(
-        "Persisting final graph at {} iris codes to database",
-        counter
-    );
+    info!("Building in-memory plaintext vector stores and HNSW graphs");
 
-    persist_graph_db(&graph);
+    let mut results = jobs.join_all().await;
+    results.sort_by_key(|x| x.0 as usize);
 
-    info!("Dropping HNSW graph to conserve system memory");
-    drop(graph);
+    let vectors: BothEyes<_> = [results[0].1.take().unwrap(), results[1].1.take().unwrap()];
+    let graphs: BothEyes<_> = [results[0].2.take().unwrap(), results[1].2.take().unwrap()];
+
+    for side in STORE_IDS {
+        info!(
+            "Persisting {} graph over {} iris codes to DB",
+            side,
+            &vectors[side as usize].points.len()
+        );
+        persist_graph_db(&graphs[side as usize]);
+    }
+
+    info!("Dropping HNSW graphs to conserve system memory");
+    drop(graphs);
 
     info!("Converting plaintext iris codes locally into secret shares");
 
     const N_PARTIES: usize = 3;
-    let database_size = vector.points.len();
-    let mut shared_irises: Vec<Vec<_>> = (0..N_PARTIES)
-        .map(|_| Vec::with_capacity(database_size))
-        .collect();
+    for (side, vector) in STORE_IDS.iter().copied().zip(vectors.into_iter()) {
+        let database_size = vector.points.len();
+        let mut shared_irises: Vec<Vec<_>> = (0..N_PARTIES)
+            .map(|_| Vec::with_capacity(database_size))
+            .collect();
 
-    for iris in vector.points.iter() {
-        let all_shares =
-            GaloisRingSharedIris::generate_shares_locally(&mut aby3_rng, iris.data.0.clone());
-        for (party_id, share) in all_shares.into_iter().enumerate() {
-            shared_irises[party_id].push(share);
+        info!("Generating {} shares", side);
+
+        for iris in vector.points.iter() {
+            let all_shares =
+                GaloisRingSharedIris::generate_shares_locally(&mut aby3_rng, iris.data.0.clone());
+            for (party_id, share) in all_shares.into_iter().enumerate() {
+                shared_irises[party_id].push(share);
+            }
         }
-    }
 
-    info!("Persisting secret shared iris codes to database");
-    for (party, iris_shares) in shared_irises.iter().enumerate() {
-        info!("Persisting party {} iris code secret shares", party);
-        persist_vector_shares(iris_shares);
+        for (party, iris_shares) in shared_irises.iter().enumerate() {
+            info!("Persisting {} shares for party {} to DB", side, party);
+            persist_vector_shares(iris_shares);
+        }
     }
 
     info!("Exited successfully! 🎉");
