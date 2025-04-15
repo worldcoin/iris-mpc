dd3420818722a3ae7ef4d6d92368f40cfd2bac0b
danielle-tfh <danielle.nagar@toolsforhumanity.com>
2025-04-15 17:17:09 +0200

    Remove only having mirror attacks for uniqueness

--- iris-mpc-gpu/src/server/actor.rs
+++ iris-mpc-gpu/src/server/actor.rs
@@ -1183,8 +1183,7 @@ impl ServerActor {
                 let attack_detected = (0..request_count)
                     .map(|i| {
                         // Here we check that the normal merged result is non-match while the mirrored merged result shows a match.
-                        batch.request_types[i] == UNIQUENESS_MESSAGE_TYPE
-                            && merged_results[i] == NON_MATCH_ID
+                        merged_results[i] == NON_MATCH_ID
                             && mirror_results.matches_with_skip_persistence[i]
                     })
                     .collect();
