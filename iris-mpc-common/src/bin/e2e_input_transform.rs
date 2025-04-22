8982cf1d626cea92be77e228cc9fda70b75515bc
kkowalski <chrisk@toolsforhumanity.com>
2025-04-22 08:24:39 +0200

    Add data to image

--- iris-mpc-common/src/bin/e2e_input_transform.rs
+++ iris-mpc-common/src/bin/e2e_input_transform.rs
@@ -7,34 +7,41 @@ use std::{
     fs::File,
     io::{Read, Write},
 };
+use uuid::Uuid;
 
 const RNG_SEED: u64 = 42; // Replace with your seed value
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct Signup {
     signup_id: String,
-    iris_code_left: String,
+    // iris_code_left: String,
     iris_code_shares_left: Option<[String; 3]>,
-    mask_code_left: String,
+    // mask_code_left: String,
     mask_code_shares_left: Option<[String; 3]>,
-    iris_code_right: String,
+    // iris_code_right: String,
     iris_code_shares_right: Option<[String; 3]>,
-    mask_code_right: String,
+    // mask_code_right: String,
     mask_code_shares_right: Option<[String; 3]>,
-    matched_with: Vec<Match>,
-    whitelisted: bool,
-    closest_signups: Vec<Match>,
-    zkp: String,
-    idcomm: String,
+    // matched_with: Vec<Match>,
+    // whitelisted: bool,
+    // closest_signups: Vec<Match>,
+    // zkp: String,
+    // idcomm: String,
+}
+
+#[derive(Debug, Serialize, Deserialize,Clone)]
+struct InversedSignup {
+    iris_codes: String,
+    mask_codes: String,
 }
 
 impl Signup {
-    fn add_shares(&mut self) {
+    fn add_shares(&mut self, signup_inversed: InversedSignup, signup_inversed_right: InversedSignup) {
         let mut rng = rand::rngs::StdRng::seed_from_u64(RNG_SEED);
-        let iris_code_left = IrisCodeArray::from_base64(&self.iris_code_left).unwrap();
-        let mask_code_left = IrisCodeArray::from_base64(&self.mask_code_left).unwrap();
-        let iris_code_right = IrisCodeArray::from_base64(&self.iris_code_right).unwrap();
-        let mask_code_right = IrisCodeArray::from_base64(&self.mask_code_right).unwrap();
+        let iris_code_left = IrisCodeArray::from_base64(&signup_inversed.iris_codes).unwrap();
+        let mask_code_left = IrisCodeArray::from_base64(&signup_inversed.mask_codes).unwrap();
+        let iris_code_right = IrisCodeArray::from_base64(&signup_inversed_right.iris_codes).unwrap();
+        let mask_code_right = IrisCodeArray::from_base64(&signup_inversed_right.mask_codes).unwrap();
 
         // Encode iris code shares left
         let shares_iris_code_left =
@@ -67,13 +74,13 @@ struct Match {
     normalization_used: bool,
 }
 
-fn read_json_file(file_path: &str) -> serde_json::Result<Vec<Signup>> {
+fn read_json_file(file_path: &str) -> serde_json::Result<Vec<InversedSignup>> {
     let mut file = File::open(file_path).expect("File not found");
     let mut data = String::new();
     file.read_to_string(&mut data)
         .expect("Unable to read string");
 
-    let signups: Vec<Signup> = serde_json::from_str(&data)?;
+    let signups: Vec<InversedSignup> = serde_json::from_str(&data)?;
     Ok(signups)
 }
 
@@ -85,16 +92,26 @@ fn write_json_file(file_path: &str, signups: &Vec<Signup>) -> std::io::Result<()
 }
 
 fn main() {
-    let input_file = "./iris-mpc-common/src/bin/data/ss_e2e.json";
-    let output_file = "./iris-mpc-common/src/bin/data/generated_ss_e2e_shares.json";
+    let input_file = "./iris-mpc-common/src/bin/data/1001lines.json";
+    let output_file = "./iris-mpc-common/src/bin/data/1001lines_shares.json";
     let mut signups = read_json_file(input_file).unwrap();
-    for signup in &mut signups {
-        println!("Calculating shares for signup {:?}", signup.signup_id);
-        signup.add_shares();
-    }
+
+    let mut output_signups = vec![];
+    let mut s = Signup {
+        signup_id: Uuid::new_v4().to_string(),
+        iris_code_shares_left: None,
+        mask_code_shares_left: None,
+        iris_code_shares_right: None,
+        mask_code_shares_right: None,
+    };
+
+    s.add_shares(signups[0].clone(),signups[1].clone());
+    output_signups.push(s.clone());
+
+
     write_json_file(
-        "./iris-mpc-common/src/bin/data/generated_ss_e2e_shares.json",
-        &signups,
+        output_file,
+        &output_signups,
     )
     .unwrap();
     println!(
--- iris-mpc-cpu/bin/init_test_dbs.rs
+++ iris-mpc-cpu/bin/init_test_dbs.rs
@@ -133,7 +133,7 @@ struct Args {
 
     /// PRNG seed for HNSW insertion, used to select the layer at which new
     /// elements are inserted into the hierarchical graph structure.
-    #[clap(default_value = "0")]
+    #[clap(long, default_value = "0")]
     hnsw_prng_seed: u64,
 
     /// PRNG seed for ABY3 MPC protocols, used for locally generating secret
