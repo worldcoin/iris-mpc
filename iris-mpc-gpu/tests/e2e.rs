1f12ffbe5bd56b433ceb298fdbf8497303643813
Daniel Kales <11509575+dkales@users.noreply.github.com>
2025-04-22 15:40:07 +0200

    give tests more stack

--- iris-mpc-gpu/tests/e2e.rs
+++ iris-mpc-gpu/tests/e2e.rs
@@ -41,8 +41,17 @@ mod e2e_test {
             .init();
     }
 
-    #[tokio::test]
-    async fn e2e_test() -> Result<()> {
+    #[test]
+    fn e2e_test() -> Result<()> {
+        let runtime = tokio::runtime::Builder::new_multi_thread()
+            .worker_threads(3)
+            .thread_stack_size(8 * 1024 * 1024)
+            .enable_all()
+            .build()?;
+        runtime.block_on(e2e_test_main())
+    }
+
+    async fn e2e_test_main() -> Result<()> {
         install_tracing();
         env::set_var("NCCL_P2P_LEVEL", "LOC");
         env::set_var("NCCL_NET", "Socket");
