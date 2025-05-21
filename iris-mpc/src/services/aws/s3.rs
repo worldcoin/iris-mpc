92083051af3586320f3764082db9b61491abcde2
Ertugrul Aypek <ertugrul.aypek@toolsforhumanity.com>
2025-05-21 16:21:01 +0200

    Update s3 importer to include version id (#1380)
    * WIP: update s3 importer to include version id
    
    * update s3 importer to include version id and unify iris loader
    
    * trigger image build
    
    * remove unnecessary dependencies
    
    * include version id in vector id
    
    * deploy image to stage
    
    * deploy hnsw image to stage
    
    * disable import temporarily
    
    * re-enable importer
    
    * testing completed, disable importer and use temp db with 100k items

--- iris-mpc/src/services/aws/s3.rs
+++ iris-mpc/src/services/aws/s3.rs
@@ -1,9 +1,5 @@
-use aws_config::{retry::RetryConfig, timeout::TimeoutConfig};
-use aws_sdk_s3::{
-    config::{Builder as S3ConfigBuilder, StalledStreamProtectionConfig},
-    Client as S3Client,
-};
-use std::time::Duration;
+use aws_config::retry::RetryConfig;
+use aws_sdk_s3::{config::Builder as S3ConfigBuilder, Client as S3Client};
 
 /// Creates an S3 client with retry configuration
 pub fn create_s3_client(shared_config: &aws_config::SdkConfig, force_path_style: bool) -> S3Client {
@@ -15,28 +11,4 @@ pub fn create_s3_client(shared_config: &aws_config::SdkConfig, force_path_style:
         .build();
 
     S3Client::from_conf(s3_config)
-}
-
-/// Creates an S3 client specifically for database chunks with additional
-/// configuration
-pub fn create_db_chunks_s3_client(
-    shared_config: &aws_config::SdkConfig,
-    force_path_style: bool,
-) -> S3Client {
-    let retry_config = RetryConfig::standard().with_max_attempts(5);
-
-    // Increase S3 connect timeouts to 10s
-    let timeout_config = TimeoutConfig::builder()
-        .connect_timeout(Duration::from_secs(10))
-        .build();
-
-    let db_chunks_s3_config = S3ConfigBuilder::from(shared_config)
-        // disable stalled stream protection to avoid panics during s3 import
-        .stalled_stream_protection(StalledStreamProtectionConfig::disabled())
-        .retry_config(retry_config)
-        .timeout_config(timeout_config)
-        .force_path_style(force_path_style)
-        .build();
-
-    S3Client::from_conf(db_chunks_s3_config)
 }
