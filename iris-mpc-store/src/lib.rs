0cf4ac269eee1c2147107e3d95968a172b90acc5
leonanos8 <nanomail4@gmail.com>
2025-05-27 11:50:56 +0200

    fetch partial results and rotations and store them in ServerJobResult, part1

--- iris-mpc-store/src/lib.rs
+++ iris-mpc-store/src/lib.rs
@@ -914,6 +914,8 @@ pub mod tests {
             None,
             None,
             None,
+            None,
+            None,
             false,
         ))?;
         let result_events = vec![result_event; count];
