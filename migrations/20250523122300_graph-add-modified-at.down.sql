db43c8e509ecb17f2d84bdd019000c0a32f524d4
Wojciech Sromek <157375010+wojciechsromek@users.noreply.github.com>
2025-05-30 12:00:04 +0200

    Update 20250523122300_graph-add-modified-at.down.sql

--- migrations/20250523122300_graph-add-modified-at.down.sql
+++ migrations/20250523122300_graph-add-modified-at.down.sql
@@ -1,5 +1,3 @@
 DROP TRIGGER IF EXISTS graph_links_set_last_modified_at ON hawk_graph_links;
-DROP TRIGGER IF EXISTS graph_entry_set_last_modified_at ON hawk_graph_entry;
 
 ALTER TABLE hawk_graph_links DROP COLUMN last_modified_at;
-ALTER TABLE hawk_graph_entry DROP COLUMN last_modified_at;
