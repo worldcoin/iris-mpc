9f12010769789a8c8f7c9580349af60d2211b367
Bryan Gillespie <bryan@inversed.tech>
2025-05-31 07:19:24 -0600

    Fix off-by-one error on serial id range check bound

--- migrations/20250530201832_unify_id_repr.up.sql
+++ migrations/20250530201832_unify_id_repr.up.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS hawk_graph_entry;
 CREATE TABLE hawk_graph_links
 (
     graph_id smallint NOT NULL CHECK (graph_id = 0 OR graph_id = 1),
-    serial_id bigint NOT NULL CHECK (serial_id BETWEEN 1 AND 4294967296),
+    serial_id bigint NOT NULL CHECK (serial_id BETWEEN 1 AND 4294967295),
     version_id smallint NOT NULL CHECK (version_id >= 0),
     layer smallint NOT NULL CHECK (layer >= 0),
     links bytea NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE hawk_graph_links
 CREATE TABLE hawk_graph_entry
 (
     graph_id smallint NOT NULL CHECK (graph_id = 0 OR graph_id = 1),
-    serial_id bigint NOT NULL CHECK (serial_id BETWEEN 1 AND 4294967296),
+    serial_id bigint NOT NULL CHECK (serial_id BETWEEN 1 AND 4294967295),
     version_id smallint NOT NULL CHECK (version_id >= 0),
     layer smallint NOT NULL CHECK (layer >= 0),
     CONSTRAINT hawk_graph_entry_pkey PRIMARY KEY (graph_id)
