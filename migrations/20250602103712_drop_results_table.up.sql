fc3d09575778fbc9aca9820e0e11bda12f733b39
leonanos8 <nanomail4@gmail.com>
2025-06-11 13:10:23 +0200

    remove migrations that did not exist in v0.17.0

--- migrations/20250602103712_drop_results_table.up.sql
+++
@@ -1,5 +0,0 @@
--- all results (including uniqueness) will be replayed by modifications table --
-DROP TABLE IF EXISTS results;
-
--- allow in progress uniqueness modifications to be inserted. they are only assigned a serial id if result is unique --
-ALTER TABLE modifications ALTER COLUMN serial_id DROP NOT NULL;
