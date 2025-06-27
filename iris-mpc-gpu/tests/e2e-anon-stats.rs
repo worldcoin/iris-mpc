a5ba16d978e702a2305d5fe1dc6d4b36f1b14cb5
Daniel Kales <11509575+dkales@users.noreply.github.com>
2025-06-27 14:50:35 +0200

    use power two bucket

--- iris-mpc-gpu/tests/e2e-anon-stats.rs
+++ iris-mpc-gpu/tests/e2e-anon-stats.rs
@@ -17,7 +17,7 @@ mod e2e_anon_stats_test {
     const DB_BUFFER: usize = 8 * 1000;
     const NUM_BATCHES: usize = 300;
     const MAX_BATCH_SIZE: usize = 64;
-    const N_BUCKETS: usize = 10;
+    const N_BUCKETS: usize = 8;
     const MATCH_DISTANCES_BUFFER_SIZE: usize = 1 << 6;
     const MATCH_DISTANCES_BUFFER_SIZE_EXTRA_PERCENT: usize = 5000;
 
