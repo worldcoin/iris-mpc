da34d5dfd6119f97cb6f9322f3e6128843ec1183
Stuart Woodbury <stuart@inversed.tech>
2025-07-04 10:12:37 -0400

    improve logging

--- iris-mpc-cpu/bin/network_test_cli.rs
+++ iris-mpc-cpu/bin/network_test_cli.rs
@@ -5,16 +5,12 @@ mod health;
 use clap::Parser;
 use eyre::{eyre, Result};
 use futures::future::try_join_all;
-use health::{get_tcp_info, tcp_info};
-use std::{
-    net::SocketAddr,
-    os::fd::AsRawFd,
-    time::{Duration, Instant},
-};
+use health::get_tcp_info;
+use std::{net::SocketAddr, os::fd::AsRawFd, time::Duration};
 use tokio::io::{AsyncReadExt, AsyncWriteExt, BufReader};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::mpsc::{self, UnboundedReceiver, UnboundedSender};
-use tokio::time::{interval, sleep};
+use tokio::time::interval;
 
 /// start the program in one of two modes: client or server
 #[derive(Parser)]
@@ -30,18 +26,27 @@ enum Cmd {
 #[derive(clap::Args)]
 struct ClientArgs {
     /// <ip address>:<socket>
+    #[arg(long)]
     server: String,
+
     /// number of connections to establish
+    #[arg(short, long)]
     connections: usize,
 
     /// duration in seconds for each step of the test
+    #[arg(short, long)]
     step_sec: Option<u64>,
+
     /// the maximum number of tests to run
+    #[arg(short, long)]
     num_steps: Option<usize>,
 
     /// MB/s to start the test at
+    #[arg(short, long)]
     throughput_start: Option<usize>,
+
     /// if set, the throughput will be incremented by X MB every step. otherwise it doubles.
+    #[arg(short, long)]
     increment: Option<usize>,
 }
 
@@ -180,65 +185,12 @@ async fn run_client(args: ClientArgs) -> Result<()> {
             };
         }
 
+        log_to_terminal(idx, num_steps, &cmd, &rsp);
+
         mb_sec = match args.increment {
             Some(mb) => mb_sec + mb,
             None => mb_sec * 2,
         };
-
-        // todo: add a way to log to csv instead of the terminal
-
-        println!(
-            "stats for step {}, duration {}, throughput {}",
-            idx, step_sec, mb_sec
-        );
-        println!("---------------------------------");
-
-        println!("avg_delivery_rate (MB/s):");
-        for m in &rsp {
-            print!("{:.2} ", m.avg_delivery_rate);
-        }
-        println!();
-
-        println!("bytes_sent (MB):");
-        for m in &rsp {
-            print!("{:.2} ", m.bytes_sent);
-        }
-        println!();
-
-        println!("busy_time (ms)");
-        for m in &rsp {
-            print!("{:.3} ", m.busy_time);
-        }
-        println!();
-
-        println!("rwnd_limited (ms)");
-        for m in &rsp {
-            print!("{:.3} ", m.rwnd_limited);
-        }
-        println!();
-
-        println!("sndbuf_limited (ms)");
-        for m in &rsp {
-            print!("{:.3} ", m.sndbuf_limited);
-        }
-        println!();
-
-        println!("delivered");
-        for m in &rsp {
-            print!("{} ", m.delivered);
-        }
-        println!();
-
-        println!("delivered_ce");
-        for m in &rsp {
-            print!("{} ", m.delivered_ce);
-        }
-        println!();
-
-        println!("");
-        println!("---------------------------------");
-        println!("");
-        println!("");
     }
 
     println!("test finished");
@@ -251,64 +203,115 @@ async fn client_task(
     metrics_tx: UnboundedSender<Result<MetricsRsp>>,
 ) -> Result<()> {
     let fd = stream.as_raw_fd();
-    let tick_ms = 500;
     while let Some(cmd) = cmd_rx.recv().await {
-        let mut last_info: Option<tcp_info> = None;
-        let mut samples = 0;
-        let mut delivery_rate_sum = 0;
+        // MB/s to B/s, divided over MS_PER_SEC/TICK_MS intervals
+        // tried sending every 10ms but it used up all the ram and swap, even when data size was adjusted appropriately.
+        const TICK_MS: u64 = 100;
+        let mut data = vec![0_u8; (cmd.throughput * 1_000_000 / 10) + 4]; // +4 for the length
+        let len = data.len() - 4;
+        data[..4].copy_from_slice(&(len as u32).to_le_bytes());
 
-        let start = Instant::now();
-        let mut snd_ticker = interval(Duration::from_millis(tick_ms));
+        // want to only show metrics per step, not since the socket was open
+        let s_ti = get_tcp_info(fd)?;
 
-        let data = vec![0_u8; cmd.throughput * 1_000_000 / 100];
+        let mut snd_ticker = interval(Duration::from_millis(TICK_MS));
+
+        // warm up - want to sample socket statistics when sending is already in progress
         for _ in 0..6 {
             snd_ticker.tick().await;
-            stream.write_all(&(data.len() as u32).to_le_bytes()).await?;
             stream.write_all(&data).await?;
         }
 
-        // want the stats to be offset from the sending
-        sleep(Duration::from_millis(tick_ms / 2)).await;
-        let mut stats_ticker = interval(Duration::from_millis(tick_ms));
+        let mut samples = 0;
+        let mut delivery_rate_sum = 0;
+        for _ in 0..(cmd.duration_sec * 1000) / TICK_MS {
+            snd_ticker.tick().await;
 
-        while start.elapsed().as_secs() < cmd.duration_sec {
-            tokio::select! {
-                _ = snd_ticker.tick() => {
-                    stream.write_all(&(data.len() as u32).to_le_bytes()).await?;
-                    stream.write_all(&data).await?;
-                },
-                _ = stats_ticker.tick() => {
-                    let mut tcp_info = get_tcp_info(fd)?;
-                    delivery_rate_sum += tcp_info.tcpi_delivery_rate;
-                    samples += 1;
+            let tcp_info = get_tcp_info(fd)?;
+            delivery_rate_sum += tcp_info.tcpi_delivery_rate;
+            samples += 1;
 
-                    // for ease of use, only log the diff.
-                    if let Some(prev) = last_info.as_ref() {
-                        tcp_info.tcpi_bytes_sent -= prev.tcpi_bytes_sent;
-                        tcp_info.tcpi_busy_time -= prev.tcpi_busy_time;
-                        tcp_info.tcpi_rwnd_limited -= prev.tcpi_rwnd_limited;
-                        tcp_info.tcpi_sndbuf_limited -= prev.tcpi_sndbuf_limited;
-                        tcp_info.tcpi_delivered -= prev.tcpi_delivered;
-                        tcp_info.tcpi_delivered_ce -= prev.tcpi_delivered_ce;
-                    }
-                    last_info.replace(tcp_info);
-                }
-            }
+            stream.write_all(&data).await?;
         }
 
-        let ti = last_info.unwrap();
+        // wait for final send to finish
+        snd_ticker.tick().await;
+
+        let ti = get_tcp_info(fd)?;
+        delivery_rate_sum += ti.tcpi_delivery_rate;
+        samples += 1;
+
         let rsp = MetricsRsp {
+            // calculate avg bytes/sec and convert to MB/s
             avg_delivery_rate: (delivery_rate_sum / samples) as f64 / 1_000_000.0,
-            bytes_sent: ti.tcpi_bytes_sent as f64 / 1_000_000.0,
-            busy_time: ti.tcpi_busy_time as f64 / 1000.0,
-            rwnd_limited: ti.tcpi_rwnd_limited as f64 / 1000.0,
-            sndbuf_limited: ti.tcpi_sndbuf_limited as f64 / 1000.0,
-            delivered: ti.tcpi_delivered,
-            delivered_ce: ti.tcpi_delivered_ce,
+            bytes_sent: (ti.tcpi_bytes_sent - s_ti.tcpi_bytes_sent) as f64 / 1_000_000.0,
+            busy_time: (ti.tcpi_busy_time - s_ti.tcpi_busy_time) as f64 / 1000.0,
+            rwnd_limited: (ti.tcpi_rwnd_limited - s_ti.tcpi_rwnd_limited) as f64 / 1000.0,
+            sndbuf_limited: (ti.tcpi_sndbuf_limited - s_ti.tcpi_sndbuf_limited) as f64 / 1000.0,
+            delivered: ti.tcpi_delivered - s_ti.tcpi_delivered,
+            delivered_ce: ti.tcpi_delivered_ce - s_ti.tcpi_delivered_ce,
         };
 
         metrics_tx.send(Ok(rsp))?;
     }
 
     Ok(())
+}
+
+fn log_to_terminal(idx: usize, num_steps: usize, cmd: &ClientCmd, rsp: &[MetricsRsp]) {
+    println!(
+        "stats for step {}/{}, duration(s) {}, throughput(MB/s) {}",
+        idx + 1,
+        num_steps,
+        cmd.duration_sec,
+        cmd.throughput
+    );
+    println!("---------------------------------");
+
+    println!("avg_delivery_rate (MB/s):");
+    for m in rsp {
+        print!("{:.2} ", m.avg_delivery_rate);
+    }
+    println!();
+
+    println!("bytes_sent (MB):");
+    for m in rsp {
+        print!("{:.2} ", m.bytes_sent);
+    }
+    println!();
+
+    println!("busy_time (ms)");
+    for m in rsp {
+        print!("{:.3} ", m.busy_time);
+    }
+    println!();
+
+    println!("rwnd_limited (ms)");
+    for m in rsp {
+        print!("{:.3} ", m.rwnd_limited);
+    }
+    println!();
+
+    println!("sndbuf_limited (ms)");
+    for m in rsp {
+        print!("{:.3} ", m.sndbuf_limited);
+    }
+    println!();
+
+    println!("delivered");
+    for m in rsp {
+        print!("{} ", m.delivered);
+    }
+    println!();
+
+    println!("delivered_ce");
+    for m in rsp {
+        print!("{} ", m.delivered_ce);
+    }
+    println!();
+
+    println!("");
+    println!("---------------------------------");
+    println!("");
+    println!("");
 }
