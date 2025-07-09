e5352268d87d7d5aa56a396bce29452dce2c7bfc
danielle-tfh <danielle.nagar@toolsforhumanity.com>
2025-07-09 17:17:20 +0200

    Fix clippy

--- iris-mpc-cpu/tests/e2e.rs
+++ iris-mpc-cpu/tests/e2e.rs
@@ -1,7 +1,7 @@
 use eyre::Result;
 use iris_mpc_common::{
-    iris_db::db::IrisDB,
-    test::{generate_full_test_db, TestCase, TestCaseGenerator},
+    iris_db::{db::IrisDB, iris::IrisCode},
+    test::{generate_full_test_db, TestCaseGenerator},
 };
 use iris_mpc_cpu::{
     execution::hawk_main::{HawkActor, HawkArgs, HawkHandle, VectorId},
