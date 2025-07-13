743dd9eabae91d841b8b5c023fa90000a283b28f
Mark A. Greenslade <siajasl-0@pm.me>
2025-07-13 17:54:10 +0200

    HNSW-E2E: 1. Reverted to temporarily using docker init-db.

--- scripts/run-tests-hnsw-e2e.sh
+++ scripts/run-tests-hnsw-e2e.sh
@@ -6,22 +6,9 @@ set -e
 # ----------------------------------------------------------------
 function _main()
 {
-    local path_to_monorepo
-
-    path_to_monorepo="$(_get_path_to_monorepo)"
-
-    _log "Initialising system state"
-    _init_system_state "${path_to_monorepo}"
-
     _log "Executing end to end tests"
-    _exec_tests "${path_to_monorepo}"
-}
 
-function _exec_tests()
-{
-    local path_to_monorepo="${1}"
-
-    pushd "${path_to_monorepo}/iris-mpc-upgrade-hawk" || exit
+    pushd "$(_get_path_to_monorepo)/iris-mpc-upgrade-hawk" || exit
     cargo test --test e2e_genesis
     popd || exit
 }
@@ -43,45 +30,6 @@ function _get_path_to_monorepo()
     root=$( dirname "$here" )
 
     echo "${root}"
-}
-
-function _init_system_state()
-{
-    # NOTE: This function is temporary.  In next iteration of e2e testing framework
-    # the initialisation of system state will also be performed by the e2e tests.
-
-    local path_to_monorepo="${1}"
-
-    _log "Initialising postgres dBs"
-    _init_system_state_pgres "${path_to_monorepo}"
-
-    _log "Initialising AWS services"
-    _init_system_state_aws "${path_to_monorepo}"
-}
-
-function _init_system_state_pgres()
-{
-    local path_to_monorepo="${1}"
-    local path_to_iris_shares
-
-    path_to_iris_shares="${path_to_monorepo}/iris-mpc-upgrade-hawk/tests/resources/iris-shares-plaintext/20250710-synthetic-irises-1k.ndjson"
-
-    set -a
-    SMPC_INIT_SKIP_HNSW_GRAPH=true
-    SMPC_INIT_TARGET_DB_SIZE=100
-    SMPC_INIT_PATH_TO_IRIS_PLAINTEXT="${path_to_iris_shares}"
-    export SMPC_INIT_SKIP_HNSW_GRAPH
-    export SMPC_INIT_TARGET_DB_SIZE
-    export SMPC_INIT_PATH_TO_IRIS_PLAINTEXT
-    source "${path_to_monorepo}/scripts/tools/init_db_from_plaintext_iris_file.sh"
-    set +a
-}
-
-function _init_system_state_aws()
-{
-    local path_to_monorepo="${1}"
-
-    _log "TODO: initialise local stack S3 bucket for Iris deletions"
 }
 
 function _log ()
