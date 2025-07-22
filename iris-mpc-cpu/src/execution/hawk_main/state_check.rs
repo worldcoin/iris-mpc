ed8de90e4e119a0fb367b183a93b01890d43e9ad
Stuart Woodbury <stuart@inversed.tech>
2025-07-22 13:50:22 -0400

    add check to verify that the PRF didnt get out of sync

--- iris-mpc-cpu/src/execution/hawk_main/state_check.rs
+++ iris-mpc-cpu/src/execution/hawk_main/state_check.rs
@@ -5,7 +5,7 @@ use siphasher::sip::SipHasher13;
 use std::hash::{Hash, Hasher};
 
 use crate::{
-    execution::hawk_main::{LEFT, RIGHT},
+    execution::hawk_main::{BothOrient, LEFT, RIGHT},
     network::value::{NetworkValue, StateChecksum},
 };
 
@@ -37,6 +37,45 @@ impl SetHash {
 }
 
 impl HawkSession {
+    pub async fn prf_check(sessions: &BothOrient<BothEyes<Vec<HawkSessionRef>>>) -> Result<()> {
+        // make a function because the borrow checker can't track the lifetimes properly if this was a closure
+        async fn squeeze_rng(session: &HawkSessionRef) -> Result<()> {
+            let mut session = session.write().await;
+            let prf = &mut session.aby3_store.session.prf;
+
+            let my_share = prf.gen_zero_share::<u128>();
+            let my_msg = || NetworkValue::PrfCheck(my_share);
+
+            let net = &mut session.aby3_store.session.network_session;
+            net.send_prev(my_msg()).await?;
+            net.send_next(my_msg()).await?;
+
+            let decode = |msg| match msg {
+                Ok(NetworkValue::PrfCheck(c)) => Ok(c),
+                other => {
+                    tracing::error!("Unexpected message format: {:?}", other);
+                    Err(eyre!("Could not deserialize PrfCheck"))
+                }
+            };
+            let prev_share = decode(net.receive_prev().await)?;
+            let next_share = decode(net.receive_next().await)?;
+
+            if (prev_share + my_share + next_share).convert() != 0_u128 {
+                bail!("PRFs are out of sync");
+            }
+            Ok(())
+        }
+
+        let _ = futures::future::try_join_all(
+            sessions
+                .iter()
+                .flat_map(|orient| orient.iter().flat_map(|eyes| eyes.iter()))
+                .map(squeeze_rng),
+        )
+        .await?;
+        Ok(())
+    }
+
     pub async fn state_check(sessions: BothEyes<&HawkSessionRef>) -> Result<()> {
         let (left_state, right_state) = join!(
             HawkSession::state_check_side(sessions[LEFT]),
