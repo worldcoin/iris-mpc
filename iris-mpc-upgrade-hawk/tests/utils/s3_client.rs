d22f992be4a98a03cfe115cc35961646b7c0b13b
Stuart Woodbury <stuart@inversed.tech>
2025-08-04 18:40:04 -0400

    try using to_vec instead of to_string() in attempt to fix problem with s3

--- iris-mpc-upgrade-hawk/tests/utils/s3_client.rs
+++ iris-mpc-upgrade-hawk/tests/utils/s3_client.rs
@@ -1,52 +1,34 @@
-use crate::utils::{errors::TestError, logger};
-use aws_config;
+use crate::utils::errors::TestError;
 use aws_sdk_s3::{primitives::ByteStream as S3_ByteStream, Client as S3_Client};
 use eyre::Result;
 use iris_mpc::services::aws::clients::AwsClients;
 use iris_mpc_common::{config::Config, IrisSerialId};
 use iris_mpc_cpu::genesis::state_accessor::S3Object;
 
-/// Component name for logging purposes.
-const COMPONENT: &str = "SystemState-Aws";
-
 /// Uploads to an AWS S3 bucket a set of serial identifiers marked as deleted.
 pub async fn upload_iris_deletions(
     data: &Vec<IrisSerialId>,
-    s3: &S3_Client,
+    s3_client: &S3_Client,
     config: &Config,
 ) -> Result<(), TestError> {
     // Set bucket/key based on environment.
     let s3_bucket = get_s3_bucket_for_iris_deletions(&config.environment);
     let s3_key = get_s3_key_for_iris_deletions(&config.environment);
-    logger::log_info(
-        COMPONENT,
-        format!(
-            "Inserting deleted serial ids into S3 bucket: {}, key: {}",
-            s3_bucket, s3_key
-        )
-        .as_str(),
-    );
 
-    // Set body of payload to be persisted.
-    let body = S3_ByteStream::from(
-        serde_json::to_string(&S3Object {
-            deleted_serial_ids: data.to_owned(),
-        })
-        .unwrap()
-        .into_bytes(),
-    );
+    let obj = S3Object {
+        deleted_serial_ids: data.to_owned(),
+    };
+    let obj_bytes = serde_json::to_vec(&obj).unwrap();
+    let body = S3_ByteStream::from(obj_bytes);
 
-    // Upload payload.
-    s3.put_object()
+    s3_client
+        .put_object()
         .bucket(&s3_bucket)
         .key(&s3_key)
         .body(body)
         .send()
         .await
-        .map_err(|err| {
-            logger::log_error(COMPONENT, format!("Failed to upload file to S3: {}", err));
-            TestError::SetupError("Failed to upload Iris deletions to S3".to_string())
-        })?;
+        .unwrap();
 
     Ok(())
 }
