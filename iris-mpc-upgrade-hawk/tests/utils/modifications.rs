d7b7ef2b8139733211a5d8fed43a6d7775799248
Stuart Woodbury <stuart@inversed.tech>
2025-08-06 11:22:16 -0400

    wip: modifications

---
+++ iris-mpc-upgrade-hawk/tests/utils/modifications.rs
@@ -0,0 +1,29 @@
+use std::{fs::File, io::BufReader, path::PathBuf};
+
+use serde::{Deserialize, Serialize};
+
+/// used as inputs to iris-mpc-store > insert_modification()
+#[derive(Clone, Serialize, Deserialize, Default)]
+pub struct ModificationInput {
+    pub serial_id: Option<i64>,
+    pub request_type: String,
+    #[serde(default = "default_modification_status")]
+    pub status: String,
+    #[serde(default = "default_false")]
+    pub persisted: bool,
+}
+
+fn default_modification_status() -> String {
+    "IN_PROGRESS".into()
+}
+
+fn default_false() -> bool {
+    false
+}
+
+pub fn read_modifications(resource_path: PathBuf) -> Result<Vec<ModificationInput>, Error> {
+    let file = File::open(resource_path.as_path())?;
+    let reader = BufReader::new(file);
+    let modifications: Vec<ModificationInput> = serde_json::from_reader(reader)?;
+    Ok(modifications)
+}
