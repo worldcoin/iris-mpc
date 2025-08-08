b290ea4cb8ae1a027c220c3b955bbf615392d332
Stuart Woodbury <stuart@inversed.tech>
2025-08-08 14:24:05 -0400

    make prf check owrk on all sessions again

--- iris-mpc-cpu/src/execution/hawk_main/state_check.rs
+++ iris-mpc-cpu/src/execution/hawk_main/state_check.rs
@@ -5,7 +5,7 @@ use siphasher::sip::SipHasher13;
 use std::hash::{Hash, Hasher};
 
 use crate::{
-    execution::hawk_main::{LEFT, RIGHT},
+    execution::hawk_main::{BothOrient, LEFT, RIGHT},
     network::value::{NetworkValue, StateChecksum},
 };
 
@@ -37,7 +37,7 @@ impl SetHash {
 }
 
 impl HawkSession {
-    pub async fn prf_check(sessions: BothEyes<&HawkSession>) -> Result<()> {
+    pub async fn prf_check(sessions: &BothOrient<BothEyes<Vec<HawkSession>>>) -> Result<()> {
         // make a function because the borrow checker can't track the lifetimes properly if this was a closure
         async fn squeeze_rng(session: &HawkSession) -> Result<()> {
             let mut store = session.aby3_store.write().await;
@@ -66,10 +66,13 @@ impl HawkSession {
             Ok(())
         }
 
-        let (res_left, res_right) =
-            join!(squeeze_rng(sessions[LEFT]), squeeze_rng(sessions[RIGHT]));
-        let _ = res_left?;
-        let _ = res_right?;
+        let _ = futures::future::try_join_all(
+            sessions
+                .iter()
+                .flat_map(|orient| orient.iter().flat_map(|eyes| eyes.iter()))
+                .map(squeeze_rng),
+        )
+        .await?;
         Ok(())
     }
 
