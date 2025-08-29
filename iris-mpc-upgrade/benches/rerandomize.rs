12e0e22979d77bee9df5ceb4a06449f411fcc6e0
Daniel Kales <11509575+dkales@users.noreply.github.com>
2025-08-29 15:39:38 +0200

    bench: add MT bench

--- iris-mpc-upgrade/benches/rerandomize.rs
+++ iris-mpc-upgrade/benches/rerandomize.rs
@@ -1,5 +1,6 @@
 use criterion::{criterion_group, criterion_main, BatchSize};
 use iris_mpc_common::{IRIS_CODE_LENGTH, MASK_CODE_LENGTH};
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 fn iris_rerandomize(c: &mut criterion::Criterion) {
     use iris_mpc_store::DbStoredIris;
@@ -21,6 +22,56 @@ fn iris_rerandomize(c: &mut criterion::Criterion) {
             },
             |iris| randomize_iris(iris, &master_seed, 0),
             BatchSize::SmallInput,
+        );
+    });
+    c.bench_function("iris_rerandomize 1024", |b| {
+        b.iter_batched(
+            || {
+                (0..1024)
+                    .map(|_| {
+                        DbStoredIris::new(
+                            42,
+                            1,
+                            vec![1u8; IRIS_CODE_LENGTH * std::mem::size_of::<u16>()],
+                            vec![2u8; MASK_CODE_LENGTH * std::mem::size_of::<u16>()],
+                            vec![3u8; IRIS_CODE_LENGTH * std::mem::size_of::<u16>()],
+                            vec![4u8; MASK_CODE_LENGTH * std::mem::size_of::<u16>()],
+                        )
+                    })
+                    .collect::<Vec<_>>()
+            },
+            |irises| {
+                irises
+                    .into_iter()
+                    .map(|iris| randomize_iris(iris, &master_seed, 0))
+                    .collect::<Vec<_>>()
+            },
+            BatchSize::LargeInput,
+        );
+    });
+    c.bench_function("iris_rerandomize par_iter 1024", |b| {
+        b.iter_batched(
+            || {
+                (0..1024)
+                    .map(|_| {
+                        DbStoredIris::new(
+                            42,
+                            1,
+                            vec![1u8; IRIS_CODE_LENGTH * std::mem::size_of::<u16>()],
+                            vec![2u8; MASK_CODE_LENGTH * std::mem::size_of::<u16>()],
+                            vec![3u8; IRIS_CODE_LENGTH * std::mem::size_of::<u16>()],
+                            vec![4u8; MASK_CODE_LENGTH * std::mem::size_of::<u16>()],
+                        )
+                    })
+                    .collect::<Vec<_>>()
+            },
+            |irises| {
+                irises
+                    .into_par_iter()
+                    .map(|iris| randomize_iris(iris, &master_seed, 0))
+                    .collect::<Vec<_>>()
+            },
+            BatchSize::LargeInput,
         );
     });
 }
