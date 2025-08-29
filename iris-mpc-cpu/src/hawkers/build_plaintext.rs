ead38d0495626859079726a9cb41013f856d0bcb
Mihai <mihai.calancea@gmail.com>
2025-08-29 17:32:35 +0300

    parallelize inserts in init_test_dbs

--- iris-mpc-cpu/src/hawkers/build_plaintext.rs
+++ iris-mpc-cpu/src/hawkers/build_plaintext.rs
@@ -6,75 +6,62 @@ use itertools::Itertools;
 use tokio::task::JoinSet;
 
 use crate::{
-    execution::hawk_main::{
-        insert::{self, InsertPlanV},
-        BothEyes,
-    },
+    execution::hawk_main::insert::{self, InsertPlanV},
     hawkers::plaintext_store::SharedPlaintextStore,
     hnsw::{GraphMem, HnswSearcher},
 };
 
-pub type SharedPlaintextGraphs = BothEyes<GraphMem<SharedPlaintextStore>>;
-pub type SharedPlaintextStores = BothEyes<SharedPlaintextStore>;
-
 pub async fn plaintext_parallel_batch_insert(
     graph: Option<GraphMem<SharedPlaintextStore>>,
     store: Option<SharedPlaintextStore>,
-    irises: Vec<(IrisVectorId, IrisCode)>,
-    params: crate::hnsw::HnswParams,
-    batch_size: usize,
-    prf_seed: &[u8; 16],
+    irises: Vec<(IrisVectorId, Arc<IrisCode>)>,
+    searcher: HnswSearcher,
+    prf: impl Fn(IrisVectorId) -> Result<usize> + Clone + Send + 'static,
 ) -> Result<(GraphMem<SharedPlaintextStore>, SharedPlaintextStore)> {
     assert!(graph.is_none() == store.is_none());
     let mut graph = Arc::new(graph.unwrap_or_default());
     let mut store = store.unwrap_or_default();
 
-    let searcher = HnswSearcher { params };
+    let mut jobs: JoinSet<Result<_>> = JoinSet::new();
 
-    for batch in &irises.into_iter().enumerate().chunks(batch_size) {
-        let mut jobs: JoinSet<Result<_>> = JoinSet::new();
+    for (vector_id, iris) in irises {
+        let mut store = store.clone();
+        let graph = graph.clone();
+        let searcher = searcher.clone();
+        let prf = prf.clone();
 
-        for (_, iris) in batch {
-            let query = Arc::new(iris.1);
-            let vector_id = iris.0;
-            let prf_seed = *prf_seed;
-            let mut store = store.clone();
-            let graph = graph.clone();
-            let searcher = searcher.clone();
+        jobs.spawn(async move {
+            let insertion_layer = prf(vector_id)?;
 
-            jobs.spawn(async move {
-                let insertion_layer = searcher.select_layer_prf(&prf_seed, &(vector_id))?;
+            let (links, set_ep) = searcher
+                .search_to_insert(&mut store, &graph, &iris, insertion_layer)
+                .await?;
 
-                let (links, set_ep) = searcher
-                    .search_to_insert(&mut store, &graph, &query, insertion_layer)
-                    .await?;
-
-                let insert_plan: InsertPlanV<SharedPlaintextStore> = InsertPlanV {
-                    query,
-                    links,
-                    set_ep,
-                };
-                Ok((vector_id, insert_plan))
-            });
-        }
-
-        let mut results: Vec<_> = jobs
-            .join_all()
-            .await
-            .into_iter()
-            .collect::<Result<_, _>>()?;
-
-        results.sort_by_key(|(vector_id, _)| (vector_id.serial_id()));
-
-        let (ids, plans): (Vec<_>, Vec<_>) = results.into_iter().unzip();
-        let ids = ids.into_iter().map(Some).collect_vec();
-        let plans = plans.into_iter().map(Some).collect_vec();
-
-        // Unwrap Arc while inserting, then wrap again for the next batch
-        let mut graph_temp = Arc::try_unwrap(graph).unwrap();
-        insert::insert(&mut store, &mut graph_temp, &searcher, plans, &ids).await?;
-        graph = Arc::new(graph_temp);
+            let insert_plan: InsertPlanV<SharedPlaintextStore> = InsertPlanV {
+                query: iris,
+                links,
+                set_ep,
+            };
+            Ok((vector_id, insert_plan))
+        });
     }
+
+    let mut results: Vec<_> = jobs
+        .join_all()
+        .await
+        .into_iter()
+        .collect::<Result<_, _>>()?;
+
+    results.sort_by_key(|(vector_id, _)| (vector_id.serial_id()));
+
+    let (ids, plans): (Vec<_>, Vec<_>) = results.into_iter().unzip();
+    let ids = ids.into_iter().map(Some).collect_vec();
+    let plans = plans.into_iter().map(Some).collect_vec();
+
+    // Unwrap Arc while inserting, then wrap again for the next batch
+    let mut graph_temp = Arc::try_unwrap(graph).unwrap();
+    insert::insert(&mut store, &mut graph_temp, &searcher, plans, &ids).await?;
+    graph = Arc::new(graph_temp);
 
     Ok((Arc::try_unwrap(graph).unwrap(), store))
 }
@@ -84,7 +71,7 @@ mod tests {
     use super::*;
     use crate::{
         hawkers::plaintext_store::PlaintextStore,
-        hnsw::{graph::layered_graph::migrate, HnswParams, HnswSearcher},
+        hnsw::{graph::layered_graph::migrate, HnswSearcher},
     };
     use aes_prng::AesRng;
     use iris_mpc_common::iris_db::db::IrisDB;
@@ -97,7 +84,7 @@ mod tests {
         HnswSearcher,
         GraphMem<SharedPlaintextStore>,
         SharedPlaintextStore,
-        Vec<(IrisVectorId, IrisCode)>,
+        Vec<(IrisVectorId, Arc<IrisCode>)>,
         [u8; 16],
     )> {
         let mut rng = AesRng::seed_from_u64(0_u64);
@@ -114,13 +101,13 @@ mod tests {
 
         let irises_to_insert = IrisDB::new_random_rng(to_insert, &mut rng).db;
 
-        let irises: Vec<(IrisVectorId, IrisCode)> = irises_to_insert
+        let irises: Vec<(IrisVectorId, Arc<IrisCode>)> = irises_to_insert
             .into_iter()
             .enumerate()
             .map(|(id, iris_code)| {
                 (
                     IrisVectorId::from_serial_id((id + database_size + 1).try_into().unwrap()),
-                    iris_code,
+                    Arc::new(iris_code),
                 )
             })
             .collect();
@@ -131,7 +118,7 @@ mod tests {
     async fn check_results(
         mut store: SharedPlaintextStore,
         graph: GraphMem<SharedPlaintextStore>,
-        irises: Vec<(IrisVectorId, IrisCode)>,
+        irises: Vec<(IrisVectorId, Arc<IrisCode>)>,
         searcher: &HnswSearcher,
         expected_total_size: usize,
     ) -> Result<()> {
@@ -142,8 +129,7 @@ mod tests {
         );
 
         // Check if each inserted iris can be found and matched correctly
-        for (_vector_id, iris_code) in irises {
-            let query = Arc::new(iris_code);
+        for (_vector_id, query) in irises {
             let result = searcher.search(&mut store, &graph, &query, 1).await?;
             assert!(
                 searcher.is_match(&mut store, &[result]).await?,
@@ -158,17 +144,16 @@ mod tests {
     async fn test_insert_with_small_batches() -> Result<()> {
         let database_size = 256;
         let to_insert = 512;
-        let batch_size = 32;
-        let (searcher, graph, store, irises, prf_seed) =
-            setup_test_data(database_size, to_insert).await?;
+        let (searcher, graph, store, irises, _) = setup_test_data(database_size, to_insert).await?;
+        let searcher_ = searcher.clone();
+        let prf = move |vector_id| searcher_.select_layer_prf(&[0u8; 16], &vector_id);
 
         let (final_graph, final_store) = plaintext_parallel_batch_insert(
             Some(graph),
             Some(store),
             irises.clone(),
-            HnswParams::new(64, 32, 32),
-            batch_size,
-            &prf_seed,
+            searcher.clone(),
+            prf,
         )
         .await?;
 
@@ -186,17 +171,16 @@ mod tests {
     async fn test_insert_with_large_batches() -> Result<()> {
         let database_size = 256;
         let to_insert = 512;
-        let batch_size = 100;
-        let (searcher, graph, store, irises, prf_seed) =
-            setup_test_data(database_size, to_insert).await?;
+        let (searcher, graph, store, irises, _) = setup_test_data(database_size, to_insert).await?;
+        let searcher_ = searcher.clone();
+        let prf = move |vector_id| searcher_.select_layer_prf(&[0u8; 16], &vector_id);
 
         let (final_graph, final_store) = plaintext_parallel_batch_insert(
             Some(graph),
             Some(store),
             irises.clone(),
-            HnswParams::new(64, 32, 32),
-            batch_size,
-            &prf_seed,
+            searcher.clone(),
+            prf,
         )
         .await?;
 
