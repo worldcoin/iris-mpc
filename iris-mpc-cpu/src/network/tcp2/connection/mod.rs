2fa7330088452efab18467a4b25d345f24762a2f
Stuart Woodbury <stuart@inversed.tech>
2025-10-01 10:55:04 -0400

    wip: accept_loop

--- iris-mpc-cpu/src/network/tcp2/connection/mod.rs
+++ iris-mpc-cpu/src/network/tcp2/connection/mod.rs
@@ -1,14 +1,38 @@
+mod client; // trait for initiating a connection. hides details of TCP vs TLS
 mod connection_state;
+mod handshake;
+mod listener; // accept inbound connections
+mod server; // trait for accepting connections. hides details of TCP vs TLS // used to determine the peer id and connection id
+
 pub use connection_state::ConnectionState;
 
 use crate::{
-    execution::{player::Identity, session::StreamId},
-    network::tcp2::NetworkConnection,
+    execution::player::Identity,
+    network::tcp2::{data::ConnectionId, NetworkConnection},
 };
+use eyre::Result;
+use socket2::{SockRef, TcpKeepalive};
+use std::time::Duration;
+use tokio::net::TcpStream;
 
 pub struct Connection<T: NetworkConnection> {
     stream: T,
-    stream_id: StreamId,
+    connection_id: ConnectionId,
     own_id: Identity,
     peer_id: Identity,
+}
+
+/// set no_delay and keepalive
+fn configure_tcp_stream(stream: &TcpStream) -> Result<()> {
+    let params = TcpKeepalive::new()
+        // idle time before keepalives get sent. NGINX default is 60 seconds. want to be less than that.
+        .with_time(Duration::from_secs(30))
+        // how often to send keepalives
+        .with_interval(Duration::from_secs(30))
+        // how many unanswered probes before the connection is closed
+        .with_retries(4);
+    let socket_ref = SockRef::from(&stream);
+    socket_ref.set_tcp_nodelay(true)?;
+    socket_ref.set_tcp_keepalive(&params)?;
+    Ok(())
 }
