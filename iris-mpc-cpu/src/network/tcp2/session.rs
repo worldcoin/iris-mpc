95803a5fc830bb0995c98a66a98e8ddf185f890b
Stuart Woodbury <stuart@inversed.tech>
2025-10-01 09:21:59 -0400

    saving progress

---
+++ iris-mpc-cpu/src/network/tcp2/session.rs
@@ -0,0 +1,33 @@
+use crate::{
+    execution::{player::Identity, session::SessionId},
+    network::{value::NetworkValue, Networking},
+};
+use async_trait::async_trait;
+use eyre::Result;
+
+#[derive(Debug)]
+pub struct TcpSession {
+    session_id: SessionId,
+    config: TcpConfig,
+}
+
+impl TcpSession {
+    pub fn new(session_id: SessionId, config: TcpConfig) -> Self {
+        Self { session_id, config }
+    }
+
+    pub fn id(&self) -> SessionId {
+        self.session_id
+    }
+}
+
+#[async_trait]
+impl Networking for TcpSession {
+    async fn send(&mut self, value: NetworkValue, receiver: &Identity) -> Result<()> {
+        todo!()
+    }
+
+    async fn receive(&mut self, sender: &Identity) -> Result<NetworkValue> {
+        todo!()
+    }
+}
