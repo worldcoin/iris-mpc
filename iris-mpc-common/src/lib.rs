67db38ecfd360a38382b67e1ee21aa901ecaecfa
sdwoodbury <stuart@inversed.tech>
2025-10-08 09:48:13 -0400

    [POP-2956] rotation aware trick dot (#1707)
    * add new variants of trick dot
    
    * add unit tests and a benchmark

--- iris-mpc-common/src/lib.rs
+++ iris-mpc-common/src/lib.rs
@@ -20,6 +20,13 @@ pub const IRIS_CODE_LENGTH: usize = 12_800;
 pub const MASK_CODE_LENGTH: usize = 6_400;
 pub const ROTATIONS: usize = 31;
 
+pub const PRE_PROC_ROW_PADDING: usize = 120;
+pub const IRIS_CODE_ROWS: usize = 16;
+// 16 = 12800 / 800 = (IRIS_CODE_LENGTH) / (CODE_COLS * 4)
+pub const PRE_PROC_IRIS_CODE_LENGTH: usize =
+    IRIS_CODE_LENGTH + (IRIS_CODE_ROWS * PRE_PROC_ROW_PADDING);
+pub const PRE_PROC_MASK_CODE_LENGTH: usize = MASK_CODE_LENGTH + (8 * PRE_PROC_ROW_PADDING);
+
 /// Iris code database type; .0 = iris code, .1 = mask
 pub type IrisCodeDb = (Vec<u16>, Vec<u16>);
 /// Borrowed version of iris database; .0 = iris code, .1 = mask
