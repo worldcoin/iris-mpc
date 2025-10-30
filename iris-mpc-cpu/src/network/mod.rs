744dc2e8566b4965c51bd10f0c8cd282ad5a9b8c
fu <stuart@inversed.tech>
2025-10-30 12:31:16 -0400

    delete grpc module

--- iris-mpc-cpu/src/network/mod.rs
+++ iris-mpc-cpu/src/network/mod.rs
@@ -1,10 +1,4 @@
-use crate::{
-    execution::{
-        player::Identity,
-        session::{SessionId, StreamId},
-    },
-    network::value::NetworkValue,
-};
+use crate::{execution::player::Identity, network::value::NetworkValue};
 use async_trait::async_trait;
 use eyre::Result;
 
@@ -19,12 +13,6 @@ pub trait Networking {
 #[derive(Clone)]
 pub enum NetworkType {
     Local,
-    #[cfg(test)]
-    Grpc {
-        connection_parallelism: usize,
-        stream_parallelism: usize,
-        request_parallelism: usize,
-    },
     Tcp {
         connection_parallelism: usize,
         request_parallelism: usize,
@@ -36,21 +24,8 @@ impl NetworkType {
         1
     }
 
-    pub fn default_stream_parallelism() -> usize {
-        1
-    }
-
     pub fn default_request_parallelism() -> usize {
         1
-    }
-
-    #[cfg(test)]
-    pub fn default_grpc() -> Self {
-        Self::Grpc {
-            connection_parallelism: Self::default_connection_parallelism(),
-            stream_parallelism: Self::default_stream_parallelism(),
-            request_parallelism: Self::default_request_parallelism(),
-        }
     }
 
     pub fn default_tcp() -> Self {
@@ -61,8 +36,6 @@ impl NetworkType {
     }
 }
 
-#[cfg(test)]
-pub mod grpc;
 pub mod local;
 pub mod tcp;
 pub mod value;
