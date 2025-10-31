f146b10b5f971bffe4f09baee3fab7994c4450af
Mark A. Greenslade <siajasl-0@pm.me>
2025-10-31 14:28:55 +0100

    1. Moved utils binaries to bin crate.

--- iris-mpc-utils/bin/service_client/requests/mod.rs
+++
@@ -1,9 +0,0 @@
-mod batch_generator;
-mod dispatcher;
-mod factory;
-mod types;
-
-pub use batch_generator::Generator;
-pub use dispatcher::Dispatcher;
-pub use factory::Factory;
-pub use types::{BatchDispatcher, BatchIterator, BatchProfile, BatchSize, MessageFactory};
