ea8bee9881ab633122e7ce47cbac897567141849
sdwoodbury <stuart@inversed.tech>
2025-11-04 18:03:05 -0500

    refactor tcp stack and add graceful shutdown (#1729)
    * squash changes from new_tcp_stack2 rebased onto dev
    
    * code review
    
    ---------
    
    Co-authored-by: naure <naure@users.noreply.github.com>

--- iris-mpc-cpu/src/network/tcp/mod.rs
+++ iris-mpc-cpu/src/network/tcp/mod.rs
@@ -1,50 +1,45 @@
-use crate::{
-    execution::{
-        hawk_main::HawkArgs,
-        local::generate_local_identities,
-        player::{Role, RoleAssignment},
-        session::{NetworkSession, Session},
-    },
-    network::tcp::{
-        config::TcpConfig,
-        handle::TcpNetworkHandle,
-        networking::{
-            client::{TcpClient, TlsClient},
-            connection_builder::PeerConnectionBuilder,
-            server::TcpServer,
-        },
-    },
-};
+mod config;
+mod connection;
+mod data;
+mod handle;
+mod session;
+
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
+use std::sync::{Arc, Once};
+use std::time::Duration;
+
+use crate::execution::hawk_main::HawkArgs;
+use crate::execution::local::generate_local_identities;
+use crate::execution::player::{Role, RoleAssignment};
+use crate::execution::session::{NetworkSession, Session};
+use crate::network::tcp::config::TcpConfig;
+use crate::network::tcp::connection::client::{BoxTcpClient, TcpClient, TlsClient};
+use crate::network::tcp::connection::server::{BoxTcpServer, TcpServer, TlsServer};
+use crate::network::tcp::handle::TcpNetworkHandle;
 use async_trait::async_trait;
 use eyre::Result;
 use iris_mpc_common::config::TlsConfig;
 use itertools::izip;
-use std::sync::{Arc, Once};
-use std::{
-    net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr},
-    time::Duration,
-};
-use tokio::io::{AsyncRead, AsyncWrite};
+use tokio::io::{AsyncRead, AsyncWrite, AsyncWriteExt};
+use tokio::net::TcpStream;
+use tokio_rustls::TlsStream;
 use tokio_util::sync::CancellationToken;
-
-pub mod config;
-mod data;
-pub mod handle;
-pub mod networking;
-pub mod session;
-
-use crate::network::tcp::networking::client::BoxTcpClient;
-use crate::network::tcp::networking::server::{BoxTcpServer, TlsServer};
-use data::*;
 
 #[async_trait]
 pub trait NetworkHandle: Send + Sync {
-    async fn make_network_sessions(&mut self) -> Result<Vec<NetworkSession>>;
-    async fn make_sessions(&mut self) -> Result<Vec<Session>>;
+    // warning: dropping sessions may close the underlying connection. If other MPC
+    // parties are still working, closing the connection may cause the other party to
+    // raise an error due to the connection being closed.
+    async fn make_network_sessions(&mut self) -> Result<(Vec<NetworkSession>, CancellationToken)>;
+    async fn make_sessions(&mut self) -> Result<(Vec<Session>, CancellationToken)>;
+    // allows unit and integration tests to wait for MPC instances to finish working before sessions are dropped.
+    async fn sync_peers(&mut self) -> Result<()>;
 }
 
-pub trait NetworkConnection: AsyncRead + AsyncWrite + Send + Sync + Unpin {}
-impl<T: AsyncRead + AsyncWrite + Unpin + Send + ?Sized + Sync> NetworkConnection for T {}
+#[async_trait]
+pub trait NetworkConnection: AsyncRead + AsyncWrite + Send + Sync + Unpin {
+    async fn close(&mut self);
+}
 
 // used to establish an outbound connection
 #[async_trait]
@@ -84,7 +79,7 @@ impl NetworkHandleArgs {
 
 pub async fn build_network_handle(
     args: NetworkHandleArgs,
-    ct: CancellationToken,
+    shutdown_ct: CancellationToken,
 ) -> Result<Box<dyn NetworkHandle>> {
     static INSTALL_CRYPTO_PROVIDER: Once = Once::new();
     INSTALL_CRYPTO_PROVIDER.call_once(|| {
@@ -115,38 +110,27 @@ pub async fn build_network_handle(
         args.request_parallelism * args.sessions_per_request,
     );
 
-    // PeerConnectionBuilder is generic over listener and connector and don't want to use a boxed trait to
-    // reduce code duplication. instead, use a macro which makes use of local variables.
+    let peers = izip!(identities, &args.addresses)
+        .enumerate()
+        .filter(|(idx, _)| *idx != my_index)
+        .map(|(_, (id, url))| (id.clone(), url.to_string()));
+
     macro_rules! build_network_handle {
-        ($listener:expr, $connector:expr) => {{
-            let connection_builder = PeerConnectionBuilder::new(
-                my_identity,
-                tcp_config.clone(),
-                $listener,
-                $connector,
-                ct.clone(),
-            )
-            .await?;
-
-            for (identity, url) in
-                izip!(identities, &args.addresses).filter(|(_, address)| address != &my_address)
-            {
-                connection_builder
-                    .include_peer(identity.clone(), url.clone())
-                    .await?;
-            }
-
-            let (reconnector, connections) = connection_builder.build().await?;
-            let networking = TcpNetworkHandle::new(
-                reconnector,
-                connections,
-                tcp_config,
-                ct,
-                my_index,
-                role_assignments,
-            );
-            Ok(Box::new(networking))
-        }};
+        ($listener:expr, $connector:expr) => {
+            Ok(Box::new(
+                TcpNetworkHandle::new(
+                    my_identity,
+                    peers,
+                    $connector,
+                    $listener,
+                    tcp_config,
+                    shutdown_ct,
+                    my_index,
+                    role_assignments,
+                )
+                .await?,
+            ))
+        };
     }
 
     if let Some(tls) = args.tls.as_ref() {
@@ -179,8 +163,9 @@ pub async fn build_network_handle(
         build_network_handle!(listener, connector)
     } else {
         tracing::info!(
-            "Building NetworkHandle, without TLS, from config: {:?}",
-            tcp_config
+            "Building NetworkHandle, without TLS, from config: {:?}, listen_addr: {:?}",
+            tcp_config,
+            my_addr
         );
         let listener = BoxTcpServer(TcpServer::new(my_addr).await?);
         let connector = BoxTcpClient(TcpClient::default());
@@ -197,26 +182,115 @@ fn to_inaddr_any(mut socket: SocketAddr) -> SocketAddr {
     socket
 }
 
+//
+// below are mostly AI generated trait implementations allowing the connections to be closable.
+//
+
+pub struct TcpStreamConn(pub TcpStream);
+pub struct TlsStreamConn(pub TlsStream<TcpStream>);
+
+// allow mixing TLS client and TCP server by boxing connections
+/// Dynamic stream type for mixed connectors and listeners
+pub type DynStreamConn = Box<dyn NetworkConnection>;
+
+#[async_trait]
+impl NetworkConnection for DynStreamConn {
+    async fn close(&mut self) {
+        (**self).close().await;
+    }
+}
+
+impl AsyncRead for TcpStreamConn {
+    fn poll_read(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &mut tokio::io::ReadBuf<'_>,
+    ) -> std::task::Poll<std::io::Result<()>> {
+        std::pin::Pin::new(&mut self.get_mut().0).poll_read(cx, buf)
+    }
+}
+
+impl AsyncWrite for TcpStreamConn {
+    fn poll_write(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &[u8],
+    ) -> std::task::Poll<std::io::Result<usize>> {
+        std::pin::Pin::new(&mut self.get_mut().0).poll_write(cx, buf)
+    }
+
+    fn poll_flush(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<std::io::Result<()>> {
+        std::pin::Pin::new(&mut self.get_mut().0).poll_flush(cx)
+    }
+
+    fn poll_shutdown(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<std::io::Result<()>> {
+        std::pin::Pin::new(&mut self.get_mut().0).poll_shutdown(cx)
+    }
+}
+
+#[async_trait]
+impl NetworkConnection for TcpStreamConn {
+    async fn close(&mut self) {
+        let _ = self.0.shutdown().await;
+    }
+}
+
+impl AsyncRead for TlsStreamConn {
+    fn poll_read(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &mut tokio::io::ReadBuf<'_>,
+    ) -> std::task::Poll<std::io::Result<()>> {
+        std::pin::Pin::new(&mut self.get_mut().0).poll_read(cx, buf)
+    }
+}
+
+impl AsyncWrite for TlsStreamConn {
+    fn poll_write(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &[u8],
+    ) -> std::task::Poll<std::io::Result<usize>> {
+        std::pin::Pin::new(&mut self.get_mut().0).poll_write(cx, buf)
+    }
+
+    fn poll_flush(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<std::io::Result<()>> {
+        std::pin::Pin::new(&mut self.get_mut().0).poll_flush(cx)
+    }
+
+    fn poll_shutdown(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<std::io::Result<()>> {
+        std::pin::Pin::new(&mut self.get_mut().0).poll_shutdown(cx)
+    }
+}
+
+#[async_trait]
+impl NetworkConnection for TlsStreamConn {
+    async fn close(&mut self) {
+        let _ = self.0.shutdown().await;
+    }
+}
+
 pub mod testing {
     use super::*;
+    use crate::execution::player::Identity;
     use eyre::Result;
-
+    use futures::future::join_all;
     use itertools::izip;
     use std::{collections::HashSet, net::SocketAddr, sync::LazyLock, time::Duration};
-    use tokio::{net::TcpStream, sync::Mutex, time::sleep};
+    use tokio::sync::Mutex;
     use tokio_util::sync::CancellationToken;
-
-    use crate::{
-        execution::player::Identity,
-        network::tcp::{
-            config::TcpConfig,
-            handle::{self, TcpNetworkHandle},
-            networking::{
-                client::TcpClient, connection_builder::PeerConnectionBuilder, server::TcpServer,
-            },
-            NetworkHandle,
-        },
-    };
 
     static USED_PORTS: LazyLock<Mutex<HashSet<SocketAddr>>> =
         LazyLock::new(|| Mutex::new(HashSet::new()));
@@ -241,58 +315,34 @@ pub mod testing {
         connection_parallelism: usize,
         request_parallelism: usize,
     ) -> Result<(
-        Vec<handle::TcpNetworkHandle<TcpStream>>,
+        Vec<TcpNetworkHandle<TcpStreamConn, TcpClient>>,
         Vec<Vec<NetworkSession>>,
     )> {
+        let mut handles =
+            get_local_tcp_handles(parties, connection_parallelism, request_parallelism).await?;
+
+        let results =
+            futures::future::join_all(handles.iter_mut().map(|h| h.make_network_sessions())).await;
+        let mut sessions = results.into_iter().collect::<Result<Vec<_>, _>>()?;
+        let no_ct = sessions.drain(..).map(|(s, _ct)| s).collect::<Vec<_>>();
+
+        Ok((handles, no_ct))
+    }
+
+    pub async fn get_local_tcp_handles(
+        parties: Vec<Identity>,
+        connection_parallelism: usize,
+        request_parallelism: usize,
+    ) -> Result<Vec<TcpNetworkHandle<TcpStreamConn, TcpClient>>> {
         assert_eq!(parties.len(), 3);
 
         let config = TcpConfig::new(
-            Duration::from_secs(5),
+            Duration::from_secs(30),
             connection_parallelism,
             request_parallelism,
         );
-
         let addresses = get_free_local_addresses(parties.len()).await?;
-        // Create NetworkHandles for each party
-        let mut builders = Vec::with_capacity(parties.len());
-        let connector = TcpClient::default();
-        let ct = CancellationToken::new();
-        for (party, addr) in izip!(parties.iter(), addresses.iter()) {
-            let listener = TcpServer::new(*addr).await?;
-            builders.push(
-                PeerConnectionBuilder::new(
-                    party.clone(),
-                    config.clone(),
-                    listener,
-                    connector.clone(),
-                    ct.clone(),
-                )
-                .await?,
-            );
-        }
-
-        sleep(Duration::from_secs(1)).await;
-
-        tracing::debug!("initiating connections");
-        // Connect each handle to every other handle
-        for i in 0..builders.len() {
-            for j in 0..builders.len() {
-                if i != j {
-                    builders[i]
-                        .include_peer(parties[j].clone(), addresses[j].to_string())
-                        .await?;
-                }
-            }
-        }
-
-        tracing::debug!("waiting for connections to complete");
-        let mut connections = vec![];
-        for b in builders {
-            let x = b.build().await?;
-            tracing::debug!("connections completed for player");
-            connections.push(x);
-        }
-        tracing::debug!("Players connected to each other");
+        let shutdown_ct = CancellationToken::new();
 
         let identities = generate_local_identities();
         let role_assignments: RoleAssignment = identities
@@ -301,26 +351,46 @@ pub mod testing {
             .map(|(index, id)| (Role::new(index), id.clone()))
             .collect();
         let role_assignments = Arc::new(role_assignments);
-        let ct = CancellationToken::new();
-        let mut handles = vec![];
-        for (idx, (r, c)) in connections.into_iter().enumerate() {
-            handles.push(TcpNetworkHandle::new(
-                r,
-                c,
-                config.clone(),
-                ct.clone(),
-                idx,
-                role_assignments.clone(),
-            ));
-        }
 
-        tracing::debug!("waiting for make_sessions to complete");
-        let mut sessions = vec![];
-        for h in handles.iter_mut() {
-            sessions.push(h.make_network_sessions().await?);
-        }
+        let handles_fut = izip!(&parties, &addresses)
+            .enumerate()
+            .map(|(peer_idx, (id, addr))| {
+                let config = config.clone();
+                let shutdown_ct = shutdown_ct.clone();
+                let parties = parties.clone();
+                let addresses = addresses.clone();
+                let role_assignments = role_assignments.clone();
+                async move {
+                    let connector = TcpClient::default();
+                    let listener = TcpServer::new(*addr).await?;
 
-        Ok((handles, sessions))
+                    let peers = izip!(&parties, &addresses)
+                        .enumerate()
+                        .filter(|(idx, _)| *idx != peer_idx)
+                        .map(|(_, (id, url))| (id.clone(), url.to_string()))
+                        .collect::<Vec<_>>();
+
+                    let handle: TcpNetworkHandle<TcpStreamConn, TcpClient> = TcpNetworkHandle::new(
+                        id.clone(),
+                        peers.into_iter(),
+                        connector,
+                        listener,
+                        config,
+                        shutdown_ct,
+                        peer_idx,
+                        role_assignments,
+                    )
+                    .await?;
+                    Ok(handle)
+                }
+            });
+
+        let handles: Vec<TcpNetworkHandle<TcpStreamConn, TcpClient>> = join_all(handles_fut)
+            .await
+            .into_iter()
+            .collect::<Result<Vec<_>, eyre::Report>>()?;
+
+        Ok(handles)
     }
 
     /// Interleaves a Vec of Vecs into a single Vec by taking one element from each inner Vec in turn.
@@ -346,21 +416,21 @@ pub mod testing {
 
 #[cfg(test)]
 mod tests {
-    use eyre::Result;
+    use super::testing::*;
 
+    use eyre::Result;
+    use rand::Rng;
     use std::time::Duration;
+
     use tokio::task::JoinSet;
     use tokio::time::sleep;
     use tracing_test::traced_test;
 
     use crate::execution::local::generate_local_identities;
     use crate::execution::player::{Identity, Role};
-    use crate::execution::session::NetworkSession;
-    use crate::network::tcp::data::StreamId;
-    use crate::network::value::NetworkValue;
-    use rand::Rng;
 
-    use super::testing::*;
+    use crate::network::tcp::NetworkSession;
+    use crate::network::value::NetworkValue;
 
     // can only send NetworkValue over the network. PrfKey is easy to make so this is used here.
     fn get_prf() -> NetworkValue {
@@ -488,46 +558,6 @@ mod tests {
 
         jobs.join_all().await;
 
-        Ok(())
-    }
-
-    #[tokio::test(flavor = "multi_thread")]
-    #[traced_test]
-    async fn test_tcp_comms_reconnect() -> Result<()> {
-        let identities = generate_local_identities();
-        let (managers, mut sessions) = setup_local_tcp_networking(identities.clone(), 1, 2).await?;
-        sleep(Duration::from_millis(500)).await;
-
-        assert_eq!(sessions.len(), 3);
-        assert_eq!(sessions[0].len(), 2);
-
-        let mut iters = vec![];
-        for session in sessions.iter_mut() {
-            iters.push(session.drain(..));
-        }
-
-        let mut session_list = vec![];
-        for _ in 0..2 {
-            let mut s = vec![];
-            for x in iters.iter_mut() {
-                s.push(x.next().unwrap());
-            }
-            session_list.push(s);
-        }
-        let mut session_list = session_list.drain(..);
-
-        all_parties_talk(identities.clone(), session_list.next().unwrap()).await;
-        tracing::debug!("all_parties_talk works. testing reconnect");
-
-        // this will disconnect from the other party. the other party will reconnect without
-        // exercising any test code.
-        managers[0]
-            .test_reconnect(identities[1].clone(), StreamId::from(0))
-            .await
-            .unwrap();
-
-        tracing::debug!("reconnect successful. testing all_parties_talk again");
-        all_parties_talk(identities, session_list.next().unwrap()).await;
         Ok(())
     }
 }
