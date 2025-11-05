f2d268642d59e92b16744843d799a48cbd51c3c7
Mark A. Greenslade <siajasl-0@pm.me>
2025-11-05 15:11:24 +0100

    1. Flattening utils crate structure.

--- iris-mpc-bins/bin/iris-mpc-utils/node_service_client.rs
+++ iris-mpc-bins/bin/iris-mpc-utils/node_service_client.rs
@@ -13,11 +13,11 @@ use iris_mpc_utils::{
         download_net_encryption_public_keys, NodeAwsClient as NodeServiceClient,
         NodeAwsConfig as NodeServiceConfig,
     },
+    client,
     constants::NODE_CONFIG_KIND_MAIN,
     fsys::{
         local::get_path_to_node_config as get_path_to_local_node_config, reader::read_node_config,
     },
-    service_client as client,
     types::{NetEncryptionPublicKeys, NetNodeConfig, NetServiceClients, NetServiceConfig},
 };
 
--- iris-mpc-utils/src/lib.rs
+++ iris-mpc-utils/src/lib.rs
@@ -1,8 +1,8 @@
 pub mod aws;
+pub mod client;
 pub mod constants;
 pub mod fsys;
 pub mod irises;
 pub mod misc;
 pub mod pgres;
-pub mod service_client;
 pub mod types;
