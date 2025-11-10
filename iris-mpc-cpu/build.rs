8d29618f920774a9d4465fc0b8f3c94f1f0e8458
Stuart Woodbury <stuart@inversed.tech>
2025-11-10 15:29:56 -0500

    remove grpc protos

--- iris-mpc-cpu/build.rs
+++ iris-mpc-cpu/build.rs
@@ -1,8 +1,1 @@
-fn main() {
-    println!("cargo:rerun-if-changed=src/proto/party_node.proto");
-    tonic_build::configure()
-        .out_dir("src/proto_generated")
-        .emit_rerun_if_changed(false) // https://github.com/hyperium/tonic/issues/1070#issuecomment-1729075588
-        .compile_protos(&["src/proto/party_node.proto"], &["src/proto"])
-        .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
-}
+fn main() {}
