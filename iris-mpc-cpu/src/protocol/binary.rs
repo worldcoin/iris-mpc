401d701a4b5a05664c301d8ccffd70ca4d0fdc7c
iliailia <iliailiashenko@gmail.com>
2025-11-13 16:03:02 +0100

    [POP-3049] Compute minimal distance via round-robin tournament (#1754)
    Co-authored-by: mcalancea <mihai.calancea@gmail.com>

--- iris-mpc-cpu/src/protocol/binary.rs
+++ iris-mpc-cpu/src/protocol/binary.rs
@@ -164,7 +164,7 @@ async fn and_many_receive<T: IntRing2k + NetworkInt>(
 }
 
 /// Low-level SMPC protocol to compute the AND of two vectors of bit-sliced shares.
-async fn and_many<T: IntRing2k + NetworkInt>(
+pub(crate) async fn and_many<T: IntRing2k + NetworkInt>(
     session: &mut Session,
     a: SliceShare<'_, T>,
     b: SliceShare<'_, T>,
@@ -176,6 +176,46 @@ where
     let shares_b = and_many_receive(session).await?;
     let complete_shares = VecShare::from_ab(shares_a, shares_b);
     Ok(complete_shares)
+}
+
+/// Reduce the given vector of bit-vector shares by computing their element-wise AND.
+///
+/// Each vector in `v` is expected to have `len` bits.
+pub(crate) async fn and_product(
+    session: &mut Session,
+    v: Vec<VecShare<Bit>>,
+    len: usize,
+) -> Result<VecShare<Bit>, Error> {
+    if v.is_empty() {
+        bail!("Input vector is empty");
+    }
+    for vec_share in &v {
+        if vec_share.len() != len {
+            bail!("Input vector shares have different lengths");
+        }
+    }
+
+    let mut res = v;
+    while res.len() > 1 {
+        // if the length is odd, we save the last column to add it back later
+        let maybe_last_column = if res.len() % 2 == 1 { res.pop() } else { None };
+        let half_len = res.len() / 2;
+        let left_bits: VecShare<u64> =
+            VecShare::new_vec(res.drain(..half_len).flatten().collect_vec()).pack();
+        let right_bits: VecShare<u64> =
+            VecShare::new_vec(res.drain(..).flatten().collect_vec()).pack();
+        let and_bits = and_many(session, left_bits.as_slice(), right_bits.as_slice()).await?;
+        let mut and_bits = and_bits.convert_to_bits();
+        let num_and_bits = half_len * len;
+        and_bits.truncate(num_and_bits);
+        res = and_bits
+            .inner()
+            .chunks(len)
+            .map(|chunk| VecShare::new_vec(chunk.to_vec()))
+            .collect_vec();
+        res.extend(maybe_last_column);
+    }
+    res.pop().ok_or(eyre!("Not enough elements"))
 }
 
 /// Computes binary AND of two vectors of bit-sliced shares.
