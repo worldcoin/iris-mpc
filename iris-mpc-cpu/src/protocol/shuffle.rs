d6fd4ee60a6f9332b4c2656b71179f1e7e338acb
ilia <iliailiashenko@gmail.com>
2025-11-17 11:27:29 +0100

    Add batching

--- iris-mpc-cpu/src/protocol/shuffle.rs
+++ iris-mpc-cpu/src/protocol/shuffle.rs
@@ -1,4 +1,5 @@
 use eyre::Result;
+use itertools::Itertools;
 use rand::Rng;
 
 use crate::{
@@ -17,21 +18,29 @@ use crate::{
 pub type Permutation = (Vec<u32>, Vec<u32>);
 
 fn batch_shuffle(
+    perm: &[u32],
+    data: &[RingElement<u32>],
+    batch_size: usize,
+) -> Vec<RingElement<u32>> {
+    let mut res = Vec::with_capacity(data.len());
+    for i in perm {
+        let start = (*i as usize) * batch_size;
+        let end = start + batch_size;
+        res.extend_from_slice(&data[start..end]);
+    }
+    res
+}
+
+fn shuffle_triplets(
     perm: Vec<u32>,
     data: VecRingElement<u32>,
     batch_size: usize,
 ) -> VecRingElement<u32> {
-    let mut res = Vec::with_capacity(data.len());
-    for i in perm {
-        let start = (i as usize) * batch_size;
-        let end = start + batch_size;
-        res.extend_from_slice(&data.0[start..end]);
+    let mut res = VecRingElement::with_capacity(data.len());
+    for batch in data.0.chunks(batch_size * 3) {
+        res.extend(batch_shuffle(&perm, batch, 3));
     }
-    VecRingElement(res)
-}
-
-fn shuffle_triplets(perm: Vec<u32>, data: VecRingElement<u32>) -> VecRingElement<u32> {
-    batch_shuffle(perm, data, 3)
+    res
 }
 
 /// Perform a random shuffle of the input distances using the 3-party shuffle protocol
@@ -76,29 +85,51 @@ fn shuffle_triplets(perm: Vec<u32>, data: VecRingElement<u32>) -> VecRingElement
 /// = pi(A + B + C)
 /// = pi(distances)
 #[allow(dead_code)]
-pub(crate) async fn random_shuffle(
+pub(crate) async fn random_shuffle_batch(
     session: &mut Session,
-    distances: Vec<(Share<u32>, DistanceShare<u32>)>,
-) -> Result<Vec<(Share<u32>, DistanceShare<u32>)>> {
+    distances: Vec<Vec<(Share<u32>, DistanceShare<u32>)>>,
+) -> Result<Vec<Vec<(Share<u32>, DistanceShare<u32>)>>> {
+    // check that distances is not empty
+    if distances.is_empty() {
+        eyre::bail!("Input distances cannot be empty");
+    }
+
+    // check that all batches have the same length
+    let batch_size = distances[0].len();
+    if !distances.iter().all(|batch| batch.len() == batch_size) {
+        eyre::bail!("All batches must have the same length");
+    }
+
+    let flattened_distances = distances.into_iter().flatten().collect_vec();
+
     let shuffle_role =
         (session.session_id().0 + session.own_role().index() as u32) % N_PARTIES as u32;
 
-    match shuffle_role {
-        0 => shuffle_party_0(session, distances).await,
-        1 => shuffle_party_1(session, distances).await,
-        2 => shuffle_party_2(session, distances).await,
+    let flattened_results = match shuffle_role {
+        0 => shuffle_party_0(session, flattened_distances, batch_size).await?,
+        1 => shuffle_party_1(session, flattened_distances, batch_size).await?,
+        2 => shuffle_party_2(session, flattened_distances, batch_size).await?,
         _ => eyre::bail!("Invalid shuffle role: {}", shuffle_role),
-    }
+    };
+
+    // Reshape flattened_results back into batches
+    let results = flattened_results
+        .chunks(batch_size)
+        .map(|chunk| chunk.to_vec())
+        .collect();
+
+    Ok(results)
 }
 
 async fn shuffle_party_0(
     session: &mut Session,
     distances: Vec<(Share<u32>, DistanceShare<u32>)>,
+    batch_size: usize,
 ) -> Result<Vec<(Share<u32>, DistanceShare<u32>)>> {
     let n = distances.len();
     let prf = &mut session.prf;
     // Generate shares of a random permutation
-    let (pi_01, pi_20) = prf.gen_permutation(n as u32)?;
+    let (pi_01, pi_20) = prf.gen_permutation((batch_size) as u32)?;
     let (a, b): (Vec<Vec<_>>, Vec<Vec<_>>) = distances
         .into_iter()
         .map(|(id_share, dist_share)| {
@@ -115,10 +146,9 @@ async fn shuffle_party_0(
         .unzip();
 
     // X0 = pi_01(A + B + Z_01)
-    let x0 = shuffle_triplets(pi_01, ((a + b)? + z01)?);
+    let x0 = shuffle_triplets(pi_01, ((a + b)? + z01)?, batch_size);
     // X1 = pi_20(X0 + Z_20)
-    let x1 = shuffle_triplets(pi_20, (x0 + z20)?);
-
+    let x1 = shuffle_triplets(pi_20, (x0 + z20)?, batch_size);
     let network = &mut session.network_session;
     network.send_ring_vec_next(&x1).await?;
 
@@ -132,12 +162,12 @@ async fn shuffle_party_0(
 async fn shuffle_party_1(
     session: &mut Session,
     distances: Vec<(Share<u32>, DistanceShare<u32>)>,
+    batch_size: usize,
 ) -> Result<Vec<(Share<u32>, DistanceShare<u32>)>> {
-    let n = distances.len();
     let prf = &mut session.prf;
     // Generate shares of a random permutation
     // pi_12 and pi_01
-    let (pi_12, pi_01) = prf.gen_permutation(n as u32)?;
+    let (pi_12, pi_01) = prf.gen_permutation((batch_size) as u32)?;
     // extract C
     let c: VecRingElement<u32> = distances
         .into_iter()
@@ -151,18 +181,17 @@ async fn shuffle_party_1(
         .unzip();
 
     // Y0 = pi_01(C - Z_01)
-    let y0 = shuffle_triplets(pi_01, (c - z01)?);
+    let y0 = shuffle_triplets(pi_01, (c - z01)?, batch_size);
 
     // ROUND 1
-    // Send Y1 to party 2
+    // Send Y0 to party 2
     let network = &mut session.network_session;
     network.send_ring_vec_next(&y0).await?;
 
     // Receive X1 from party 0
     let x1 = network.receive_ring_vec_prev().await?;
-
     // X2 = pi_12(X1 + Z_12)
-    let x2 = shuffle_triplets(pi_12, (x1 + z12)?);
+    let x2 = shuffle_triplets(pi_12, (x1 + z12)?, batch_size);
 
     // tilde(B) shared with party 0
     let tilde_b: VecRingElement<u32> = (0..x2.len())
@@ -188,11 +217,12 @@ async fn shuffle_party_1(
 async fn shuffle_party_2(
     session: &mut Session,
     distances: Vec<(Share<u32>, DistanceShare<u32>)>,
+    batch_size: usize,
 ) -> Result<Vec<(Share<u32>, DistanceShare<u32>)>> {
     let n = distances.len();
     let prf = &mut session.prf;
     // Generate shares of a random permutation
-    let (pi_20, pi_12) = prf.gen_permutation(n as u32)?;
+    let (pi_20, pi_12) = prf.gen_permutation((batch_size) as u32)?;
 
     let (z20, z12): (VecRingElement<u32>, VecRingElement<u32>) = (0..3 * n)
         .map(|_| prf.gen_rands::<RingElement<u32>>())
@@ -205,9 +235,9 @@ async fn shuffle_party_2(
     let y0 = network.receive_ring_vec_prev().await?;
 
     // Y1 = pi_20(Y0 - Z_20)
-    let y1 = shuffle_triplets(pi_20, (y0 - z20)?);
+    let y1 = shuffle_triplets(pi_20, (y0 - z20)?, batch_size);
     // Y2 = pi_12(Y1 - Z_12)
-    let y2 = shuffle_triplets(pi_12, (y1 - z12)?);
+    let y2 = shuffle_triplets(pi_12, (y1 - z12)?, batch_size);
     // tilde(A) shared with party 0
     let tilde_a: VecRingElement<u32> = (0..y2.len())
         .map(|_| prf.get_my_prf().gen::<RingElement<u32>>())
@@ -243,10 +273,19 @@ mod tests {
     #[tokio::test(flavor = "multi_thread")]
     #[traced_test]
     async fn test_shuffle() -> Result<()> {
-        let list_len = 6_u32;
+        let num_batches = 3;
+        let batch_size = 6_u32;
         // Create a sorted plain list of distances:
-        // (0,(0,1)), (1,(1,1)), ..., (5,(5,1))
-        let plain_list = (0..list_len).map(|i| (i, (i, 1))).collect_vec();
+        // (0,(0,1)), (1,(1,1)), ..., (5,(5,1)) 1st batch
+        // (6,(6,1)), (7,(7,1)), ..., (11,(11,1)) 2nd batch
+        // (12,(12,1)), (13,(13,1)), ..., (17,(17,1)) 3rd batch
+        let plain_list = (0..num_batches)
+            .map(|batch_i| {
+                (0..batch_size)
+                    .map(|i| (batch_size * batch_i + i, (batch_size * batch_i + i, 1)))
+                    .collect_vec()
+            })
+            .collect_vec();
 
         // PRF seeds are fixed in test setup, so the shuffle is deterministic
         let mut local_stores = setup_local_store_aby3_players(NetworkType::Local).await?;
@@ -259,17 +298,22 @@ mod tests {
                 let role = store_lock.session.own_role();
                 let distances = plain_list
                     .iter()
-                    .map(|(id, (code_dist, mask_dist))| {
-                        (
-                            Share::from_const(*id, role),
-                            DistanceShare::new(
-                                Share::from_const(*code_dist, role),
-                                Share::from_const(*mask_dist, role),
-                            ),
-                        )
+                    .map(|batch| {
+                        batch
+                            .iter()
+                            .map(|(id, (code_dist, mask_dist))| {
+                                (
+                                    Share::from_const(*id, role),
+                                    DistanceShare::new(
+                                        Share::from_const(*code_dist, role),
+                                        Share::from_const(*mask_dist, role),
+                                    ),
+                                )
+                            })
+                            .collect_vec()
                     })
                     .collect_vec();
-                random_shuffle(&mut store_lock.session, distances).await
+                random_shuffle_batch(&mut store_lock.session, distances).await
             });
         }
         let res = jobs
@@ -279,34 +323,41 @@ mod tests {
             .collect::<Result<Vec<_>>>()?;
 
         assert_eq!(res.len(), N_PARTIES);
-        assert_eq!(res[0].len(), list_len as usize);
-        assert_eq!(res[1].len(), list_len as usize);
-        assert_eq!(res[2].len(), list_len as usize);
+        assert_eq!(res[0].len(), num_batches as usize);
+        assert_eq!(res[1].len(), num_batches as usize);
+        assert_eq!(res[2].len(), num_batches as usize);
 
-        let expected = vec![
-            (2, (2, 1)),
-            (4, (4, 1)),
-            (3, (3, 1)),
-            (1, (1, 1)),
-            (0, (0, 1)),
-            (5, (5, 1)),
-        ];
+        let perm = [2, 4, 3, 1, 0, 5];
+        let expected = (0..num_batches)
+            .map(|batch_i| {
+                (0..batch_size)
+                    .map(|i| {
+                        (
+                            batch_size * batch_i + perm[i as usize],
+                            (batch_size * batch_i + perm[i as usize], 1),
+                        )
+                    })
+                    .collect_vec()
+            })
+            .collect_vec();
 
-        for (i, expected_i) in expected.into_iter().enumerate() {
-            let distance_i = {
-                let mut id = Share::zero();
-                let mut dist = DistanceShare::new(Share::zero(), Share::zero());
-                for party_res in res.iter() {
-                    id += party_res[i].clone().0;
-                    dist += &party_res[i].clone().1;
-                }
-                let id = id.get_a().convert();
-                let code_dot = dist.code_dot.get_a().convert();
-                let mask_dot = dist.mask_dot.get_a().convert();
+        for (batch_i, expected_batch) in expected.into_iter().enumerate() {
+            for (i, expected_i) in expected_batch.into_iter().enumerate() {
+                let distance_i = {
+                    let mut id = Share::zero();
+                    let mut dist = DistanceShare::new(Share::zero(), Share::zero());
+                    for party_res in res.iter() {
+                        id += party_res[batch_i][i].clone().0;
+                        dist += &party_res[batch_i][i].clone().1;
+                    }
+                    let id = id.get_a().convert();
+                    let code_dot = dist.code_dot.get_a().convert();
+                    let mask_dot = dist.mask_dot.get_a().convert();
 
-                (id, (code_dot, mask_dot))
-            };
-            assert_eq!(distance_i, expected_i);
+                    (id, (code_dot, mask_dot))
+                };
+                assert_eq!(distance_i, expected_i);
+            }
         }
 
         Ok(())
