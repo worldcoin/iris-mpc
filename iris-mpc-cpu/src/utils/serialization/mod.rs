0226b72cda5319744945d717cc31ff1360013810
Mihai <mihai.calancea@gmail.com>
2025-11-20 10:28:40 +0200

    inline suppression

--- iris-mpc-cpu/src/utils/serialization/mod.rs
+++ iris-mpc-cpu/src/utils/serialization/mod.rs
@@ -8,6 +8,7 @@ pub mod iris_ndjson;
 pub mod types;
 
 pub fn write_bin<T: Serialize>(data: &T, filename: &str) -> Result<()> {
+    // nosemgrep: tainted-path
     let file = File::create(filename)?;
     let writer = BufWriter::new(file);
     bincode::serialize_into(writer, data)?;
@@ -15,6 +16,7 @@ pub fn write_bin<T: Serialize>(data: &T, filename: &str) -> Result<()> {
 }
 
 pub fn read_bin<T: DeserializeOwned>(filename: &str) -> Result<T> {
+    // nosemgrep: tainted-path
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
     let data: T = bincode::deserialize_from(reader)?;
@@ -22,6 +24,7 @@ pub fn read_bin<T: DeserializeOwned>(filename: &str) -> Result<T> {
 }
 
 pub fn write_json<T: Serialize>(data: &T, filename: &str) -> Result<()> {
+    // nosemgrep: tainted-path
     let file = File::create(filename)?;
     let writer = BufWriter::new(file);
     serde_json::to_writer(writer, &data)?;
@@ -29,6 +32,7 @@ pub fn write_json<T: Serialize>(data: &T, filename: &str) -> Result<()> {
 }
 
 pub fn read_json<T: DeserializeOwned>(filename: &str) -> Result<T> {
+    // nosemgrep: tainted-path
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
     let data: T = serde_json::from_reader(reader)?;
