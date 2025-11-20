c8f4c6a8b941af4f8fdeaf87dd857872b691621f
Stuart Woodbury <stuart@inversed.tech>
2025-11-20 13:32:20 -0500

    squash sw/3173-3

--- iris-mpc-cpu/src/execution/hawk_main.rs
+++ iris-mpc-cpu/src/execution/hawk_main.rs
@@ -542,19 +542,10 @@ impl HawkActor {
             .map(|id_option| id_option.map(|original_id| original_id.next_version()))
             .collect_vec();
 
-        // Parallel insertions are not supported, so only one session is needed.
-        let session = &sessions[0];
-        let mut store = session.aby3_store.write().await;
-        let mut graph = session.graph_store.write().await;
+        let connect_plans =
+            insert::insert_hawk(sessions, &self.searcher, plans, &insertion_ids).await?;
 
-        insert::insert(
-            &mut *store,
-            &mut *graph,
-            &self.searcher,
-            plans,
-            &insertion_ids,
-        )
-        .await
+        Ok(connect_plans)
     }
 
     async fn update_anon_stats(
