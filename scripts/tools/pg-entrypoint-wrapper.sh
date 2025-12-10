32bdc3c22d9ab03d3db61e8ae60d5f3261d91f4b
sdwoodbury <stuart@inversed.tech>
2025-12-09 19:45:36 -0500

    fix filename and fix postgres init

--- scripts/tools/pg-entrypoint-wrapper.sh
+++ scripts/tools/pg-entrypoint-wrapper.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 set -e
 
-# Start Postgres in the background. docker-entrypoint.sh is built into the postgres image
-docker-entrypoint.sh postgres &
-
-# Wait for readiness
-until pg_isready -U "$POSTGRES_USER"; do
-  sleep 1
-done
 
 # Cleanup schemas
 
@@ -16,16 +9,24 @@ if [ ! -s "$PGDATA/PG_VERSION" ]; then
     # Standard postgres entrypoint will handle initialization including init-db-custom.sh
     exec docker-entrypoint.sh postgres "$@"
 else
+    # Start Postgres in the background. docker-entrypoint.sh is built into the postgres image
+    docker-entrypoint.sh postgres &
+
+    # Wait for readiness
+    until pg_isready -U "$POSTGRES_USER"; do
+      sleep 1
+    done
+
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "SMPC0" <<EOF
-    DROP SCHEMA IF EXISTS "genesis_cpu_0" CASCADE;
+    DROP SCHEMA IF EXISTS "genesis_cpu_dev_0" CASCADE;
     EOF
 
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "SMPC1" <<EOF
-    DROP SCHEMA IF EXISTS "genesis_cpu_1" CASCADE;
+    DROP SCHEMA IF EXISTS "genesis_cpu_dev_1" CASCADE;
     EOF
 
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "SMPC2" <<EOF
-    DROP SCHEMA IF EXISTS "genesis_cpu_2" CASCADE;
+    DROP SCHEMA IF EXISTS "genesis_cpu_dev_2" CASCADE;
     EOF
 
     wait -n
