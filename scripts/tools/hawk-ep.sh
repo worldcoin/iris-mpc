ba451410b71aa1a1b9eb125b9a482c678d764c0f
sdwoodbury <stuart@inversed.tech>
2025-12-11 15:39:53 -0500

    cap bandwidth

--- scripts/tools/hawk-ep.sh
+++ scripts/tools/hawk-ep.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Add 10ms delay to outbound traffic
 INTERFACE=$(ip route | grep default | awk '{print $5}' | head -1)
-tc qdisc add dev $INTERFACE root netem delay 10ms
+tc qdisc add dev $INTERFACE root handle 1: netem delay 10ms
+
+tc qdisc add dev $INTERFACE parent 1: handle 10: tbf \
+    rate 25gbit burst 1mbit latency 50ms
 
 # Start the main application
 exec "$@"
