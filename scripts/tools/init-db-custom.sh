cf5a681638dc05a8d4c4319478533360426cf971
sdwoodbury <stuart@inversed.tech>
2025-12-11 12:57:38 -0500

    change schema name to match config files

--- scripts/tools/init-db-custom.sh
+++ scripts/tools/init-db-custom.sh
@@ -16,4 +16,4 @@ URL_ONE="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/SMPC_dev_0"
 URL_TWO="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/SMPC_dev_1"
 URL_THREE="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/SMPC_dev_2"
 
-/bin/init-test-dbs --path-to-iris-codes /opt/irises.dat --skip-hnsw-graph --target-db-size=10000 --db-schema-party1 "genesis_gpu_dev_0" --db-schema-party2 "genesis_gpu_dev_1" --db-schema-party3 "genesis_gpu_dev_2" --db-url-party1 $URL_ONE --db-url-party2 $URL_TWO --db-url-party3 $URL_THREE
+/bin/init-test-dbs --path-to-iris-codes /opt/irises.dat --skip-hnsw-graph --target-db-size=10000 --db-schema-party1 SMPC_gpu_dev_0" --db-schema-party2 SMPC_gpu_dev_1" --db-schema-party3 SMPC_gpu_dev_2" --db-url-party1 $URL_ONE --db-url-party2 $URL_TWO --db-url-party3 $URL_THREE
