54d392866a36c4e66b29054776cfb3e6366fdf51
sdwoodbury <stuart@inversed.tech>
2025-12-11 16:02:12 -0500

    bugfix: entry point wraper

--- scripts/tools/pg-entrypoint-wrapper.sh
+++ scripts/tools/pg-entrypoint-wrapper.sh
@@ -18,15 +18,15 @@ else
     done
 
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "SMPC_dev_0" <<EOF
-    DROP SCHEMA IF EXISTS "genesis_cpu_dev_0" CASCADE;
+    DROP SCHEMA IF EXISTS "SMPC_cpu_dev_0" CASCADE;
     EOF
 
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "SMPC_dev_1" <<EOF
-    DROP SCHEMA IF EXISTS "genesis_cpu_dev_1" CASCADE;
+    DROP SCHEMA IF EXISTS "SMPC_cpu_dev_1" CASCADE;
     EOF
 
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "SMPC_dev_2" <<EOF
-    DROP SCHEMA IF EXISTS "genesis_cpu_dev_2" CASCADE;
+    DROP SCHEMA IF EXISTS "SMPC_cpu_dev_2" CASCADE;
     EOF
 
     wait -n
