76b2bdaed77230b34f8b7fd30dc474499a224d07
sdwoodbury <stuart@inversed.tech>
2025-12-12 16:12:24 -0500

    rely on db being put there manually

--- scripts/tools/init-db-custom.sh
+++ scripts/tools/init-db-custom.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 set -e
 
-psql -U "$POSTGRES_USER" -d postgres <<EOF
-DROP DATABASE IF EXISTS "SMPC_dev_0";
-DROP DATABASE IF EXISTS "SMPC_dev_1";
-DROP DATABASE IF EXISTS "SMPC_dev_2";
-CREATE DATABASE "SMPC_dev_0";
-CREATE DATABASE "SMPC_dev_1";
-CREATE DATABASE "SMPC_dev_2";
-EOF
-
 wait_for_db() {
     DB="$1"
     echo "Waiting for database $DB to become ready..."
@@ -24,8 +15,6 @@ wait_for_db "SMPC_dev_2"
 
 echo "All databases are ready."
 
-URL_ONE="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/SMPC_dev_0"
-URL_TWO="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/SMPC_dev_1"
-URL_THREE="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/SMPC_dev_2"
-
-/bin/init-test-dbs --source /opt/irises.dat --skip-hnsw-graph --target-db-size=10000 --db-schema-party1 "SMPC_gpu_dev_0" --db-schema-party2 "SMPC_gpu_dev_1" --db-schema-party3 "SMPC_gpu_dev_2" --db-url-party1 $URL_ONE --db-url-party2 $URL_TWO --db-url-party3 $URL_THREE
+pg_restore -U postgres -d SMPC_dev_0 --clean --if-exists /opt/smpc_dev_0.sql
+pg_restore -U postgres -d SMPC_dev_1 --clean --if-exists /opt/smpc_dev_1.sql
+pg_restore -U postgres -d SMPC_dev_2 --clean --if-exists /opt/smpc_dev_2.sql
