019a60998c64d48eae9daa23d9e87a6e7f71c252
sdwoodbury <stuart@inversed.tech>
2025-12-11 20:12:34 -0500

    saving progress. need to just dump the db

--- scripts/tools/pg-entrypoint-wrapper.sh
+++ scripts/tools/pg-entrypoint-wrapper.sh
@@ -1,33 +1,19 @@
 #!/bin/sh
 set -e
 
-
-# Cleanup schemas
-
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
     echo "Fresh volume detected - running standard postgres initialization"
-    # Standard postgres entrypoint will handle initialization including init-db-custom.sh
     exec docker-entrypoint.sh postgres "$@"
-else
-    # Start Postgres in the background. docker-entrypoint.sh is built into the postgres image
-    docker-entrypoint.sh postgres &
-
-    # Wait for readiness
-    until pg_isready -U "$POSTGRES_USER"; do
-      sleep 1
-    done
-
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "SMPC_dev_0" <<EOF
-    DROP SCHEMA IF EXISTS "SMPC_cpu_dev_0" CASCADE;
-    EOF
-
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "SMPC_dev_1" <<EOF
-    DROP SCHEMA IF EXISTS "SMPC_cpu_dev_1" CASCADE;
-    EOF
-
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "SMPC_dev_2" <<EOF
-    DROP SCHEMA IF EXISTS "SMPC_cpu_dev_2" CASCADE;
-    EOF
-
-    wait -n
 fi
+
+# Existing volume => cleanup schemas after postgres starts
+docker-entrypoint.sh postgres &
+
+until pg_isready -U "$POSTGRES_USER"; do
+  sleep 1
+done
+
+# Call a separate SQL cleanup script
+/bin/cleanup-cpu-schemas.sh
+
+wait -n
