3189b7bb71b7d1e15ae85a0770cb90c78f6e31c2
sdwoodbury <stuart@inversed.tech>
2025-12-15 22:05:42 -0500

    try to fix buid and push

---
+++ scripts/tools/init-single-db.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+exec /bin/init-single-db \
+  --party-id="$SMPC__SERVER_COORDINATION_PARTY_ID" \
+  --source=/tmp/irises.ndjson \
+  --db-url="$SMPC__CPU_DATABASE__URL" \
+  --db-schema="SMPC_minfhd2_dev_$SMPC__SERVER_COORDINATION_PARTY_ID" \
+  --target-db-size=1048576
