113465769188c52ebe4a51479a570ad46c5cfdf6
sdwoodbury <stuart@inversed.tech>
2025-12-15 23:43:12 -0500

    fix file name

--- scripts/tools/init-single-db.sh
+++ scripts/tools/init-single-db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 exec /bin/init-single-db \
   --party-id $SMPC__SERVER_COORDINATION__PARTY_ID \
-  --source \"/tmp/irises.ndjson\" \
+  --source "/opt/irises.ndjson" \
   --db-url "$SMPC__CPU_DATABASE__URL" \
   --db-schema "SMPC_minfhd2_dev_$SMPC__SERVER_COORDINATION__PARTY_ID" \
   --target-db-size 1048576
