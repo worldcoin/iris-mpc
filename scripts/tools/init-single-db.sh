4ed5ea53dc822fe9b33f824caec3e3bbcbeb5c75
sdwoodbury <stuart@inversed.tech>
2025-12-19 17:29:26 -0500

    build genesis dbs

--- scripts/tools/init-single-db.sh
+++ scripts/tools/init-single-db.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-exec /bin/init-single-db \
+/bin/init-single-db \
   --party-id $SMPC__SERVER_COORDINATION__PARTY_ID \
   --source "/opt/irises.ndjson" \
   --db-url "$SMPC__CPU_DATABASE__URL" \
-  --db-schema "SMPC_minfhd2_dev_$SMPC__SERVER_COORDINATION__PARTY_ID" \
-  --target-db-size 1048576
+  --db-schema "genesis_cpu1M_dev_$SMPC__SERVER_COORDINATION__PARTY_ID" \
+  --target-db-size 1048576 && \
+
+/bin/init-single-db \
+    --party-id $SMPC__SERVER_COORDINATION__PARTY_ID \
+    --source "/opt/irises.ndjson" \
+    --db-url "$SMPC__CPU_DATABASE__URL" \
+    --db-schema "genesis_gpu1M_dev_$SMPC__SERVER_COORDINATION__PARTY_ID" \
+    --target-db-size 1048576
