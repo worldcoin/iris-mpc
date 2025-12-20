ddb534f4998694ebec96d32752f5778dd364a059
sdwoodbury <stuart@inversed.tech>
2025-12-20 10:18:03 -0500

    fix script

--- scripts/tools/init-single-db.sh
+++ scripts/tools/init-single-db.sh
@@ -20,22 +20,19 @@ echo "gpu1M init done"
     --source "/opt/irises-1k.ndjson" \
     --db-url "$SMPC__CPU_DATABASE__URL" \
     --db-schema "genesis_gpu1M_dev_$SMPC__SERVER_COORDINATION__PARTY_ID" \
-    --target-db-size 1049576
+    --target-db-size 1049576 \
     --skip 0
 echo "100K irises added to gpu"
-/bin/graph-mem-cli --db-url $SMPC__CPU_DATABASE__URL --schema genesis_gpu1M_dev_$SMPC__SERVER_COORDINATION__PARTY_ID --file /opt/graph.dat restore-db
+/bin/graph-mem-cli --db-url $SMPC__CPU_DATABASE__URL --schema genesis_cpu1M_dev_$SMPC__SERVER_COORDINATION__PARTY_ID --file /opt/graph.dat restore-db
 echo "restore graph done"
-
-shutdown_requested=false
 
 shutdown_handler() {
     echo "Shutdown requested"
-    shutdown_requested=true
+    exit 0
 }
 
-trap shutdown_handler SIGTERM SIGINT SIGHUP SIGQUIT
+trap shutdown_handler SIGTERM SIGINT
 
-while [ "$shutdown_requested" = false ]; do
-    sleep infinity &  # Run in background
-    wait $!           # Wait for the background process
+while true; do
+    sleep 1
 done
