d2849a93d07f3f43b09d1f6d3c3ef2c7a042382b
sdwoodbury <stuart@inversed.tech>
2026-01-12 15:59:18 -0500

    make height configurable

---
+++ scripts/run-genesis.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# this script is meant to be called by Dockerfile.genesis.hawk
+
+set -e
+
+# exec replaces the shell with the binary
+# $@ allows arguments to be forwarded from kubernetes
+exec /bin/iris-mpc-hawk-genesis --max-height=${GENESIS_MAX_HEIGHT} --perform-snapshot=false
