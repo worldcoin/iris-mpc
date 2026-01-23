da7447331cd40f135c240c146748645308cd77b6
Elena Gryaznova <elena.gryaznova@toolsforhumanity.com>
2026-01-23 18:01:28 +0100

    Make retry count configurable

--- iris-mpc/src/services/aws/sns.rs
+++ iris-mpc/src/services/aws/sns.rs
@@ -2,9 +2,9 @@ use aws_config::retry::RetryConfig;
 use aws_sdk_sns::config::Builder;
 use aws_sdk_sns::Client as SNSClient;
 
-pub fn create_sns_client(shared_config: &aws_config::SdkConfig) -> SNSClient {
+pub fn create_sns_client(shared_config: &aws_config::SdkConfig, max_attempts: u32) -> SNSClient {
     let sns_config = Builder::from(shared_config)
-        .retry_config(RetryConfig::standard().with_max_attempts(5))
+        .retry_config(RetryConfig::standard().with_max_attempts(max_attempts))
         .build();
     SNSClient::from_conf(sns_config)
 }
