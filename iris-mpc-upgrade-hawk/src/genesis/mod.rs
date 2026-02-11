99b2e1e0a7439bfd3677fb75c5eae64e7f14a3ea
sdwoodbury <stuart@inversed.tech>
2026-02-11 12:25:51 -0500

    comment

--- iris-mpc-upgrade-hawk/src/genesis/mod.rs
+++ iris-mpc-upgrade-hawk/src/genesis/mod.rs
@@ -694,7 +694,7 @@ async fn exec_indexation(
             ));
 
             // Final barrier: ensure all nodes are done before dropping the Handle
-            // (which closes TCP connections).
+            // Prevents the unit tests from failing the health_check after the Sync job.
             hawk_handle.sync_peers(false, None).await?;
 
             Ok(())
