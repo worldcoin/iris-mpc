9f9148f7077fa37c4e7368d25eac9c872b7412cf
sdwoodbury <stuart@inversed.tech>
2026-04-30 16:14:44 -0400

    wip: change graph_mem_cli

--- iris-mpc-bins/bin/iris-mpc-cpu/graph_mem_cli.rs
+++ iris-mpc-bins/bin/iris-mpc-cpu/graph_mem_cli.rs
@@ -30,19 +30,13 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Command {
-    /// Load a graph from a database to memory, then write it to a file.
-    /// serializes both the left and right eye together
-    BackupDb,
-    /// (testing only) creates random data, stores it in the database, and then runs backup-db.
-    StoreRandom,
-    /// Load a graph from a file to memory, then write it to a database
-    RestoreDb,
-    /// restore the left or right eye
-    RestoreSide {
-        /// the left or right eye
-        side: Side,
-    },
-    /// (testing only) verify that Load/Store works as expected
+    /// Obtain the most recent checkoint, apply all relevant graph mutations, and save it to a file
+    BackupGraph,
+    /// (testing only) creates random data and stores it as a checkpoint
+    RandomCheckpoint,
+    /// Load a graph from a file to memory, then upload it to s3 and add it to the checkponts table.
+    LoadCheckpoint,
+    /// (testing only) run BackupGraph and compare it against a file.
     VerifyBackup,
     /// Load a graph from a file and compare it to the graph stored in the database.
     CompareToDb {
@@ -50,21 +44,6 @@ enum Command {
         #[arg(long, value_enum, default_value_t = DiffMethod::DetailedJaccard)]
         diff_method: DiffMethod,
     },
-}
-
-#[derive(Clone, Copy, ValueEnum)]
-enum Side {
-    Left,
-    Right,
-}
-
-impl From<Side> for usize {
-    fn from(side: Side) -> usize {
-        match side {
-            Side::Left => LEFT,
-            Side::Right => RIGHT,
-        }
-    }
 }
 
 #[tokio::main]
@@ -79,21 +58,16 @@ async fn main() -> Result<()> {
     let db_context = DbContext::new(&db_url, &schema).await;
 
     match command {
-        Command::BackupDb => {
-            db_context.write_graph_to_file(&file, dbg).await?;
+        Command::BackupGraph => {
+            todo!()
         }
-        Command::RestoreDb => {
+        Command::LoadCheckpoint => {
             db_context.load_graph_from_file(&file, dbg).await?;
-        }
-        Command::RestoreSide { side } => {
-            db_context
-                .load_side_from_file(&file, side.into(), dbg)
-                .await?;
         }
         Command::VerifyBackup => {
             db_context.verify_backup(&file, dbg).await?;
         }
-        Command::StoreRandom => {
+        Command::RandomCheckpoint => {
             db_context.store_random_graph().await?;
             db_context.write_graph_to_file(&file, dbg).await?;
         }
