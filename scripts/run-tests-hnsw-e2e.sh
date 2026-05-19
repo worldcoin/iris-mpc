3abecdf345f41c978e9790e3280b3d3b7478e439
Stuart Woodbury <stuart@inversed.tech>
2026-05-18 21:00:42 -0400

    use aws_endpoint as cmdline parameter

--- scripts/run-tests-hnsw-e2e.sh
+++ scripts/run-tests-hnsw-e2e.sh
@@ -12,14 +12,14 @@ function _main()
     cargo test --release --test e2e_genesis -- --include-ignored
 
     # create the keys with both AWSCURRENT and AWSPREVIOUS states
-    export AWS_ENDPOINT_URL="http://127.0.0.1:4566"
-    cargo run --release -p iris-mpc-bins --bin key-manager --  --region us-east-1 --node-id 0 --env dev rotate --public-key-bucket-name wf-dev-public-keys
-    cargo run --release -p iris-mpc-bins --bin key-manager --  --region us-east-1 --node-id 1 --env dev rotate --public-key-bucket-name wf-dev-public-keys
-    cargo run --release -p iris-mpc-bins --bin key-manager --  --region us-east-1 --node-id 2 --env dev rotate --public-key-bucket-name wf-dev-public-keys
+    AWS_ENDPOINT_URL="http://127.0.0.1:4566"
+    cargo run --release -p iris-mpc-bins --bin key-manager --  --region us-east-1 --node-id 0 --env dev --endpont-url $AWS_ENDPOINT_URL rotate --public-key-bucket-name wf-dev-public-keys 
+    cargo run --release -p iris-mpc-bins --bin key-manager --  --region us-east-1 --node-id 1 --env dev --endpont-url $AWS_ENDPOINT_URL rotate --public-key-bucket-name wf-dev-public-keys 
+    cargo run --release -p iris-mpc-bins --bin key-manager --  --region us-east-1 --node-id 2 --env dev --endpont-url $AWS_ENDPOINT_URL rotate --public-key-bucket-name wf-dev-public-keys 
 
-    cargo run --release -p iris-mpc-bins --bin key-manager --  --region us-east-1 --node-id 0 --env dev rotate --public-key-bucket-name wf-dev-public-keys
-    cargo run --release -p iris-mpc-bins --bin key-manager --  --region us-east-1 --node-id 1 --env dev rotate --public-key-bucket-name wf-dev-public-keys
-    cargo run --release -p iris-mpc-bins --bin key-manager --  --region us-east-1 --node-id 2 --env dev rotate --public-key-bucket-name wf-dev-public-keys
+    cargo run --release -p iris-mpc-bins --bin key-manager --  --region us-east-1 --node-id 0 --env dev --endpont-url $AWS_ENDPOINT_URL rotate --public-key-bucket-name wf-dev-public-keys 
+    cargo run --release -p iris-mpc-bins --bin key-manager --  --region us-east-1 --node-id 1 --env dev --endpont-url $AWS_ENDPOINT_URL rotate --public-key-bucket-name wf-dev-public-keys 
+    cargo run --release -p iris-mpc-bins --bin key-manager --  --region us-east-1 --node-id 2 --env dev --endpont-url $AWS_ENDPOINT_URL rotate --public-key-bucket-name wf-dev-public-keys 
 
     cargo test --release --test e2e_hawk -- --include-ignored
     popd || exit
